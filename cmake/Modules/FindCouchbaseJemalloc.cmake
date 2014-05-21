782699b644b5eea5551b4423f8a5e06aa09a0fb8
Trond Norbye <trond.norbye@gmail.com>
2014-05-21 10:03:50 +0200

    Search and detect jemalloc
    See http://www.canonware.com/jemalloc/
    
    Change-Id: I99d8180f163d6018ce7a4a7af49b18163190375f
    Reviewed-on: http://review.couchbase.org/37440
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ cmake/Modules/FindCouchbaseJemalloc.cmake
@@ -0,0 +1,39 @@
+# Locate jemalloc library
+# This module defines
+#  JEMALLOC_FOUND, if false, do not try to link with jemalloc
+#  JEMALLOC_LIBRARIES, Library path and libs
+#  JEMALLOC_INCLUDE_DIR, where to find the ICU headers
+
+FIND_PATH(JEMALLOC_INCLUDE_DIR jemalloc/jemalloc.h
+          HINTS
+               ENV JEMALLOC_DIR
+          PATH_SUFFIXES include
+          PATHS
+               ~/Library/Frameworks
+               /Library/Frameworks
+               /opt/local
+               /opt/csw
+               /opt/jemalloc
+               /opt)
+
+FIND_LIBRARY(JEMALLOC_LIBRARIES
+             NAMES jemalloc libjemalloc
+             HINTS
+                 ENV JEMALLOC_DIR
+             PATHS
+                 ~/Library/Frameworks
+                 /Library/Frameworks
+                 /opt/local
+                 /opt/csw
+                 /opt/jemalloc
+                 /opt)
+
+IF (JEMALLOC_LIBRARIES)
+  SET(JEMALLOC_FOUND true)
+  MESSAGE(STATUS "Found jemalloc in ${JEMALLOC_INCLUDE_DIR} : ${JEMALLOC_LIBRARIES}")
+  ADD_DEFINITIONS(-DHAVE_JEMALLOC)
+ELSE (JEMALLOC_LIBRARIES)
+  SET(JEMALLOC_FOUND false)
+ENDIF (JEMALLOC_LIBRARIES)
+
+MARK_AS_ADVANCED(JEMALLOC_FOUND JEMALLOC_INCLUDE_DIR JEMALLOC_LIBRARIES)
