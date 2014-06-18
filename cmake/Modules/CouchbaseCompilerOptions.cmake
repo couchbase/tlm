280f9d683dd3064c33a2ef33ff775996fedbba26
Chris Hillery <ceej@couchbase.com>
2014-06-18 00:19:02 -0700

    MB-11442: Explicitly specify minimum deployment version for MacOS (defaulting to 10.7) Change-Id: Iffe7fd4f795506acd2aafcf24ab35918149b69da Reviewed-on: http://review.couchbase.org/38392 Reviewed-by: Chris Hillery <ceej@couchbase.com> Reviewed-by: Sriram Melkote <siri@couchbase.com> Tested-by: Chris Hillery <ceej@couchbase.com>

--- cmake/Modules/CouchbaseCompilerOptions.cmake
+++ cmake/Modules/CouchbaseCompilerOptions.cmake
@@ -1,4 +1,13 @@
 #
+# Choose deployment target on MacOS
+#
+IF (APPLE)
+  # See http://www.couchbase.com/issues/browse/MB-11442
+  SET (CMAKE_OSX_DEPLOYMENT_TARGET "10.7" CACHE STRING
+    "Minimum supported version of MacOS X")
+ENDIF (APPLE)
+
+#
 # Set flags for the C Compiler
 #
 IF ("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
