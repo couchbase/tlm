59836a8b420c6ef593867b4d387721eaf864f85e
Trond Norbye <trond.norbye@gmail.com>
2014-08-26 08:55:07 +0200

    Switch to GNU99 for clang/gcc
    MSVC supports parts of the C99 standard so we don't have to be
    that strict anymore:
    
    * C99 _Bool
    * C99 compound literals
    * C99 designated initializers
    * C99 variable declarations
    
    Change-Id: I597e1bb7e359dfb4bcdd32b293d7b76ee4a89355
    Reviewed-on: http://review.couchbase.org/40901
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- cmake/Modules/CouchbaseGccOptions.cmake
+++ cmake/Modules/CouchbaseGccOptions.cmake
@@ -2,10 +2,11 @@ SET(CB_GNU_DEBUG "-g")
 SET(CB_GNU_WARNINGS "-Wall -pedantic -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls -fno-strict-aliasing -Wno-overlength-strings")
 SET(CB_GNU_VISIBILITY "-fvisibility=hidden")
 SET(CB_GNU_THREAD "-pthread")
+SET(CB_GNU_LANG_VER "-std=gnu99")
 
 IF ("${ENABLE_WERROR}" STREQUAL "YES")
    SET(CB_GNU_WERROR "-Werror")
 ENDIF()
 
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CB_GNU_DEBUG} ${CB_GNU_WARNINGS} ${CB_GNU_VISIBILITY} ${CB_GNU_THREAD} ${CB_GNU_WERROR}")
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CB_GNU_DEBUG} ${CB_GNU_LANG_VER} ${CB_GNU_WARNINGS} ${CB_GNU_VISIBILITY} ${CB_GNU_THREAD} ${CB_GNU_WERROR}")
 SET(CMAKE_LINK_FLAGS "${CMAKE_LINK_FLAGS} ${CB_GNU_LDFLAGS}")
