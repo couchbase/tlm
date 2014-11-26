7e01dfd02544b9f3947b99f3030368a35cc09dab
Dave Rigby <daver@couchbase.com>
2014-11-26 15:50:26 +0000

    CBD-1463: Update erlang to R16B03-cb2 for OS X
    R16B03-cb2 runs Erlang's Install script after extracting, which sets
    the correct path for erlang's ROOTDIR.
    
    Change-Id: Ibb7b41af2f9171b86a6ffecd9d0b84ccfd1a9beb
    Reviewed-on: http://review.couchbase.org/43645
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -6,5 +6,10 @@ DECLARE_DEP (openssl VERSION 1.0.1h PLATFORMS windows_msvc)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc)
 DECLARE_DEP (v8 VERSION 3.23.6 PLATFORMS windows_msvc)
-DECLARE_DEP (erlang VERSION R16B03 PLATFORMS macosx windows_msvc)
+
+# Erlang - R16B03-cb2 contains OSX-specific packaging changes;
+#          Windows isn't affected hence still uses R16B03.
+DECLARE_DEP (erlang VERSION R16B03 PLATFORMS windows_msvc)
+DECLARE_DEP (erlang VERSION R16B03-cb2 PLATFORMS macosx)
+
 DECLARE_DEP (jemalloc VERSION 5d9732f-cb2 PLATFORMS macosx ubuntu12.04 ubuntu14.04)
