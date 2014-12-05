a53ba7dc352360ea87a61d6b7ee84372f47e49ee
Dave Rigby <daver@couchbase.com>
2014-12-05 16:30:18 +0000

    MB-12916: Deprecate pre-C++11 support
    In preparation for removing pre-C++11 support, check what language the
    compiler supports and warn if C++11 is not available.
    
    A follow-up patch in a few weeks will remove the warning and replace
    it with a hard error.
    
    Change-Id: I2cb4ca9da5ebaf3532dda5755db95f8bccff25b7
    Reviewed-on: http://review.couchbase.org/44019
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- cmake/Modules/CouchbaseMsvcxxOptions.cmake
+++ cmake/Modules/CouchbaseMsvcxxOptions.cmake
@@ -7,4 +7,13 @@ IF ("${ENABLE_WERROR}" STREQUAL "YES")
    SET(CB_MSVCXX_WERROR "")
 ENDIF()
 
+# C++11 support has gradually increased in MSVC starting with 2010 (v16), but
+# we declare that at least VS 2013 (v18) is needed for std::atomic / C99.
+IF (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER 17)
+  SET(COMPILER_SUPPORTS_CXX11 true)
+  SET(CB_CXX_LANG_VER "C++11")
+ENDIF()
+
+INCLUDE(CouchbaseCXXVersion)
+
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CB_MSVCXX_DEBUG} ${CB_MSVCXX_WARNINGS} ${CB_MSVCXX_VISIBILITY} ${CB_MSVCXX_THREAD} ${CB_MSVCXX_WERROR}")
