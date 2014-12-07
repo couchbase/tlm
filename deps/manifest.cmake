5a4ec0562852337fa94c66c34e7c5dabc70cc17c
Dave Rigby <daver@couchbase.com>
2014-12-07 22:23:39 +0000

    MB-10496: Add jemalloc as a cbdeps dependancy for CentOS 6
    This will cause jemalloc to be the preferred memory allocator for
    CentOS 6.
    
    Note that the previous version (cb1) of the jemalloc cbdeps package
    was incorrectly uploaded as cb2. As a consequence, Centos6 (only) uses
    version cb3. This is identical to cb2 for the other platforms, and is
    what cb2 /should/ have been for Centos 6 :(
    
    Change-Id: I0f2d5fd4323c268013a8a23f54c80c2a2c70639f
    Reviewed-on: http://review.couchbase.org/44111
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -12,5 +12,9 @@ DECLARE_DEP (v8 VERSION 3.23.6 PLATFORMS windows_msvc)
 DECLARE_DEP (erlang VERSION R16B03 PLATFORMS windows_msvc)
 DECLARE_DEP (erlang VERSION R16B03-cb2 PLATFORMS macosx)
 
+# Jemalloc - cb1 was incorrectly uploaded as cb2 for centos6; so cb3 created
+#            just for CentOS6; which contains the same changes as per cb2 for
+#            the other platforms (je_ prefix on symbols).
 DECLARE_DEP (jemalloc VERSION 5d9732f-cb2
                       PLATFORMS macosx ubuntu12.04 ubuntu14.04 debian7)
+DECLARE_DEP (jemalloc VERSION 5d9732f-cb3 PLATFORMS centos6)
