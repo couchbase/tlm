687981a250dd33d05408db3cc1ec918d914d34da
Dave Rigby <daver@couchbase.com>
2014-12-08 10:11:44 +0000

    Revert "MB-10496: Add jemalloc as a cbdeps dependancy for CentOS 6"
    At least some of the builders (e.g.
    http://builds.hq.northscale.net:8010/builders/centos-6-x64-master-builder/builds/1188)
    have a compiler which fails to compile some of the defragmenter code.
    Reverting this, to no longer use jemalloc and hence not try to compile
    the defragmenter code.
    
    This reverts commit 062f83926a9a3d3649b6272f3667eaecc3ef9b71.
    
    Change-Id: I5d4ad88e922770b62d5227e9a09b18a6758a848b
    Reviewed-on: http://review.couchbase.org/44097
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -13,4 +13,4 @@ DECLARE_DEP (erlang VERSION R16B03 PLATFORMS windows_msvc)
 DECLARE_DEP (erlang VERSION R16B03-cb2 PLATFORMS macosx)
 
 DECLARE_DEP (jemalloc VERSION 5d9732f-cb2
-                      PLATFORMS macosx ubuntu12.04 ubuntu14.04 centos6 debian7)
+                      PLATFORMS macosx ubuntu12.04 ubuntu14.04 debian7)
