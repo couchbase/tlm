b53eb279233e254c2c301e14405bcc83d16c0ab7
Trond Norbye <trond.norbye@gmail.com>
2014-12-08 11:04:55 +0100

    Treat headers found through cbdeps as system headers
    Some compilers supports an option to treat some directories
    as system headers and would _not_ emit warnings from them.
    gcc/g++/clang/clang++ supports this option.
    
    Change-Id: I5e4af6f20ae894dcb3b1056435da22089e15f571
    Reviewed-on: http://review.couchbase.org/44092
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

--- cmake/Modules/CouchbaseDefaultValues.cmake
+++ cmake/Modules/CouchbaseDefaultValues.cmake
@@ -5,3 +5,16 @@
 IF ("${INSTALL_HEADER_FILES}" STREQUAL "")
    SET(INSTALL_HEADER_FILES false)
 ENDIF ()
+
+# Create a list of all of the directories we would like
+# to be treated as system headers (and not report
+# compiler warnings from (if the compiler supports it)
+LIST(APPEND CB_SYSTEM_HEADER_DIRS "${CMAKE_INSTALL_PREFIX}/include")
+
+# Unfortunately google breakpad installs it's files in
+# a slightly different way, so it needs to be added to the
+# list (otherwise it'll be found through the -I statement
+# causing all of the warnings to appear.
+IF (IS_DIRECTORY "${CMAKE_INSTALL_PREFIX}/include/breakpad")
+   LIST(APPEND CB_SYSTEM_HEADER_DIRS "${CMAKE_INSTALL_PREFIX}/include/breakpad")
+ENDIF (IS_DIRECTORY "${CMAKE_INSTALL_PREFIX}/include/breakpad")
