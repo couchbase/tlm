7051ff11ab2c93c318e5e46796fc355ae5aff459
Trond Norbye <trond.norbye@gmail.com>
2014-12-20 22:22:53 +0100

    Add cbdeps for ubuntu14.04 and sunos
    Change-Id: I1e6b789ddd4efead4c8f952b2bf6a2a1056617fe
    Reviewed-on: http://review.couchbase.org/44564
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -1,13 +1,15 @@
 DECLARE_DEP (curl VERSION 7.35.0 PLATFORMS windows_msvc)
+DECLARE_DEP (curl VERSION 7.39.0-cb1 PLATFORMS sunos ubuntu14.04)
 DECLARE_DEP (gperftools VERSION 2.2 PLATFORMS windows_msvc)
 DECLARE_DEP (icu4c VERSION 53.1.0 PLATFORMS windows_msvc)
 DECLARE_DEP (libevent VERSION 2.1.4-alpha-dev PLATFORMS windows_msvc)
-DECLARE_DEP (libevent VERSION 2.0.21-cb1 PLATFORMS macosx)
+DECLARE_DEP (libevent VERSION 2.0.21-cb1 PLATFORMS macosx sunos ubuntu14.04)
 DECLARE_DEP (openssl VERSION 1.0.1h PLATFORMS windows_msvc)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc)
+DECLARE_DEP (snappy VERSION 1.1.1-cb1 PLATFORMS sunos ubuntu14.04)
 DECLARE_DEP (v8 VERSION 3.23.6 PLATFORMS windows_msvc)
-DECLARE_DEP (v8 VERSION e24973a-cb1 PLATFORMS macosx sunos)
+DECLARE_DEP (v8 VERSION e24973a-cb1 PLATFORMS macosx sunos ubuntu14.04)
 
 # Erlang - R16B03-cb2 contains OSX-specific packaging changes;
 #          Windows isn't affected hence still uses R16B03.
