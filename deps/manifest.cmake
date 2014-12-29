268deca5d1a92cc8365d683d3501087ee6ab4617
Trond Norbye <trond.norbye@gmail.com>
2014-12-29 17:07:30 +0100

    Added CentOS7 dependencies
    Change-Id: I4fd47ec81bdce82ab6e8ee9dea5cd56bf0583010
    Reviewed-on: http://review.couchbase.org/44709
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -1,15 +1,15 @@
 DECLARE_DEP (curl VERSION 7.35.0 PLATFORMS windows_msvc)
-DECLARE_DEP (curl VERSION 7.39.0-cb1 PLATFORMS sunos ubuntu14.04)
+DECLARE_DEP (curl VERSION 7.39.0-cb1 PLATFORMS centos7 sunos ubuntu14.04)
 DECLARE_DEP (gperftools VERSION 2.2 PLATFORMS windows_msvc)
 DECLARE_DEP (icu4c VERSION 53.1.0 PLATFORMS windows_msvc)
 DECLARE_DEP (libevent VERSION 2.1.4-alpha-dev PLATFORMS windows_msvc)
-DECLARE_DEP (libevent VERSION 2.0.21-cb1 PLATFORMS macosx sunos ubuntu14.04)
+DECLARE_DEP (libevent VERSION 2.0.21-cb1 PLATFORMS centos7 macosx sunos ubuntu14.04)
 DECLARE_DEP (openssl VERSION 1.0.1h PLATFORMS windows_msvc)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc)
-DECLARE_DEP (snappy VERSION 1.1.1-cb1 PLATFORMS sunos ubuntu14.04)
+DECLARE_DEP (snappy VERSION 1.1.1-cb1 PLATFORMS centos7 sunos ubuntu14.04)
 DECLARE_DEP (v8 VERSION 3.23.6 PLATFORMS windows_msvc)
-DECLARE_DEP (v8 VERSION e24973a-cb1 PLATFORMS macosx sunos ubuntu14.04)
+DECLARE_DEP (v8 VERSION e24973a-cb1 PLATFORMS centos7 macosx sunos ubuntu14.04)
 
 # Erlang - R16B03-cb2 contains OSX-specific packaging changes;
 #          Windows isn't affected hence still uses R16B03.
@@ -21,7 +21,7 @@ DECLARE_DEP (erlang VERSION R16B03-cb2 PLATFORMS macosx)
 #            the other platforms (je_ prefix on symbols).
 DECLARE_DEP (jemalloc VERSION 5d9732f-cb2
                       PLATFORMS macosx ubuntu12.04 ubuntu14.04 debian7 sunos)
-DECLARE_DEP (jemalloc VERSION 5d9732f-cb3 PLATFORMS centos6)
+DECLARE_DEP (jemalloc VERSION 5d9732f-cb3 PLATFORMS centos6 centos7)
 DECLARE_DEP (breakpad VERSION 6fa2f26-cb1
-                      PLATFORMS centos6 macosx ubuntu12.04 ubuntu14.04)
+                      PLATFORMS centos6 centos7 macosx ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (libstdcxx VERSION 4.9.2-cb1 PLATFORMS ubuntu12.04)
