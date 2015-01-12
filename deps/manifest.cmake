ef9dc853912148066b9c8752058e0604fd257199
Dave Rigby <daver@couchbase.com>
2015-01-12 08:50:38 +0000

    Breakpad: Update to cb4 (Windows support)
    Update to breakpad rev 1e455b5:
    
    * 7f06ece 2015-01-09 | MSVC: Ignore warning 4267 (triggered on x64 builds)
    * 1e455b5 2015-01-12 | Add external repo 'gyp'
    
    Change-Id: Ib62dab0b47215e24511a90be071415f20b2ab5f3
    Reviewed-on: http://review.couchbase.org/45206
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -5,8 +5,8 @@
 # DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
 #
 
-DECLARE_DEP (breakpad VERSION 369ec25-cb3
-                      PLATFORMS centos6 centos7 ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (breakpad VERSION 1e455b5-cb4
+                      PLATFORMS centos6 centos7 ubuntu12.04 ubuntu14.04 windows_msvc)
 DECLARE_DEP (curl VERSION 7.35.0 PLATFORMS windows_msvc)
 DECLARE_DEP (curl VERSION 7.39.0-cb1 PLATFORMS centos7 sunos ubuntu14.04)
 # Erlang - R16B03-cb2 contains OSX-specific packaging changes;
