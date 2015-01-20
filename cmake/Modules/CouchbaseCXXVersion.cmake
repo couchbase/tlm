caf215b09cf45f36b07cefbb9ac7078c42dcdd44
Dave Rigby <daver@couchbase.com>
2015-01-20 12:02:30 +0000

    MB-12916: Remove pre-C++11 support
    A C++11 compiler is now required to build Sherlock.
    
    Change-Id: Ifcc913f62a187491645f51084563b1ff259199f1
    Reviewed-on: http://review.couchbase.org/45616
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- cmake/Modules/CouchbaseCXXVersion.cmake
+++ cmake/Modules/CouchbaseCXXVersion.cmake
@@ -2,14 +2,10 @@ MESSAGE(STATUS "C++ compiler version: ${CMAKE_CXX_COMPILER_VERSION}")
 MESSAGE(STATUS "C++ language version: ${CB_CXX_LANG_VER}")
 
 IF (NOT COMPILER_SUPPORTS_CXX11)
-  MESSAGE(WARNING "*****************************************************************************
-The C++ compiler (${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}) is too old and don't support C++11.
-C++11 support will be required for Sherlock. Support for pre C++11 compilers will be *dropped* in the next few weeks. Please upgrade your compiler to continue being able to build. See the following links for information on some common platforms:
+  MESSAGE("The C++ compiler (${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}) is too old and don't support C++11.
+C++11 support is required for Sherlock - please upgrade your compiler. See the following links for information on some common platforms:
 - Ubuntu 12.04: http://goo.gl/HMOfwv (suggest g++ 4.9+)
-- CentOS 6: http://goo.gl/yaz0xb (suggest c++ 4.8+)
-*****************************************************************************
-(sleeping for 10 seconds...)
-")
-  EXECUTE_PROCESS(COMMAND sleep 10)
+- CentOS 6: http://goo.gl/yaz0xb (suggest c++ 4.8+)")
+  MESSAGE(FATAL_ERROR "A compiler supporting C++11 is required to build.")
 ENDIF()
 
