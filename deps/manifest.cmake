3793ce18dc24c248bd6f57c536e9a8e39521d9c2
Trond Norbye <trond.norbye@gmail.com>
2015-01-26 11:34:54 +0100

    MB-13210: Don't wrap lines in manifest.cmake
    Change-Id: I557e934382fbecbad449ce6065d7e7463fa3fc20
    Reviewed-on: http://review.couchbase.org/45859
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -7,13 +7,10 @@
 
 # Breakpad - cb4 & cb5 identical apart from windows-specific packaging bug
 #            which was fixed in cb5.
-DECLARE_DEP (breakpad VERSION 1e455b5-cb4
-                      PLATFORMS centos6 centos7 debian7 ubuntu12.04 ubuntu14.04)
-DECLARE_DEP (breakpad VERSION 1e455b5-cb5
-                      PLATFORMS windows_msvc)
+DECLARE_DEP (breakpad VERSION 1e455b5-cb4 PLATFORMS centos6 centos7 debian7 ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (breakpad VERSION 1e455b5-cb5 PLATFORMS windows_msvc)
 DECLARE_DEP (curl VERSION 7.35.0 PLATFORMS windows_msvc)
-DECLARE_DEP (curl VERSION 7.39.0-cb1
-                  PLATFORMS centos6 centos7 debian7 sunos ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (curl VERSION 7.39.0-cb1 PLATFORMS centos6 centos7 debian7 sunos ubuntu12.04 ubuntu14.04)
 # Erlang - R16B03-cb2 contains OSX-specific packaging changes;
 #          Windows isn't affected hence still uses R16B03.
 DECLARE_DEP (erlang VERSION R16B03 PLATFORMS windows_msvc)
@@ -27,19 +24,15 @@ DECLARE_DEP (icu4c VERSION 263593-cb3 PLATFORMS centos6 ubuntu12.04)
 #            the other platforms (je_ prefix on symbols).
 #            cb4 disables tls for sunos due to gcc using malloc functions
 #                to implement the tls stuff (causing a recursive death)
-DECLARE_DEP (jemalloc VERSION 5d9732f-cb2
-                      PLATFORMS macosx ubuntu12.04 ubuntu14.04 debian7)
+DECLARE_DEP (jemalloc VERSION 5d9732f-cb2 PLATFORMS macosx ubuntu12.04 ubuntu14.04 debian7)
 DECLARE_DEP (jemalloc VERSION 5d9732f-cb3 PLATFORMS centos6 centos7)
 DECLARE_DEP (jemalloc VERSION 5d9732f-cb4 PLATFORMS sunos)
 DECLARE_DEP (libevent VERSION 2.1.4-alpha-dev PLATFORMS windows_msvc)
-DECLARE_DEP (libevent VERSION 2.0.21-cb1
-                      PLATFORMS centos6 centos7 macosx sunos ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (libevent VERSION 2.0.21-cb1 PLATFORMS centos6 centos7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (libstdcxx VERSION 4.9.2-cb1 PLATFORMS ubuntu12.04)
 DECLARE_DEP (openssl VERSION 1.0.1h PLATFORMS windows_msvc)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc)
-DECLARE_DEP (snappy VERSION 1.1.1-cb1
-                    PLATFORMS centos6 centos7 sunos ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (snappy VERSION 1.1.1-cb1 PLATFORMS centos6 centos7 sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (v8 VERSION 3.23.6 PLATFORMS windows_msvc)
-DECLARE_DEP (v8 VERSION e24973a-cb1
-                PLATFORMS centos6 centos7 macosx sunos ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (v8 VERSION e24973a-cb1 PLATFORMS centos6 centos7 macosx sunos ubuntu12.04 ubuntu14.04)
