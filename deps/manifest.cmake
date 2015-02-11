b9e2b9565fcae91bb4d7e62fbe94306312c6adff
Chris Hillery <ceej@couchbase.com>
2015-02-11 01:12:12 -0800

    CBD-1199: Update dependencies on most platforms to latest builds
    Utilize the "tlm"-built versions of all dependencies
    
    Change-Id: I90dc9511cf2baff66c47dabfff0106ce2ef6996b
    Reviewed-on: http://review.couchbase.org/46673
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -10,40 +10,22 @@
 DECLARE_DEP (breakpad VERSION 1e455b5-cb4 PLATFORMS centos6 centos7 debian7 ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (breakpad VERSION 1e455b5-cb5 PLATFORMS windows_msvc)
 DECLARE_DEP (curl VERSION 7.35.0 PLATFORMS windows_msvc)
-DECLARE_DEP (curl VERSION 7.39.0-cb1 PLATFORMS centos6 centos7 debian7 ubuntu12.04 ubuntu14.04)
-# Erlang - R16B03-cb2 contains OSX-specific packaging changes;
-#          Windows isn't affected hence still uses R16B03.
+DECLARE_DEP (curl VERSION 7.40.0-cb1 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (erlang VERSION R16B03 PLATFORMS windows_msvc)
-DECLARE_DEP (erlang VERSION R16B03-cb2 PLATFORMS macosx)
-DECLARE_DEP (erlang VERSION mb11917-cb5 PLATFORMS centos6 ubuntu12.04)
+DECLARE_DEP (erlang VERSION R16B03-1-cb1 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (gperftools VERSION 2.2 PLATFORMS windows_msvc)
 DECLARE_DEP (icu4c VERSION 53.1.0 PLATFORMS windows_msvc)
-DECLARE_DEP (icu4c VERSION 263593-cb3 PLATFORMS centos6 ubuntu12.04)
-# Jemalloc - cb1 was incorrectly uploaded as cb2 for centos6; so cb3 created
-#            just for CentOS6; which contains the same changes as per cb2 for
-#            the other platforms (je_ prefix on symbols).
-DECLARE_DEP (jemalloc VERSION 5d9732f-cb2 PLATFORMS macosx ubuntu12.04 ubuntu14.04 debian7)
-DECLARE_DEP (jemalloc VERSION 5d9732f-cb3 PLATFORMS centos6 centos7)
+DECLARE_DEP (icu4c VERSION 263593-cb4 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (jemalloc VERSION 5d9732f-cb5 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (libevent VERSION 2.1.4-alpha-dev PLATFORMS windows_msvc)
-DECLARE_DEP (libevent VERSION 2.0.21-cb1 PLATFORMS centos6 centos7 macosx ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (libevent VERSION 2.0.22-cb1 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (libstdcxx VERSION 4.9.2-cb1 PLATFORMS ubuntu12.04)
 DECLARE_DEP (openssl VERSION 1.0.1h PLATFORMS windows_msvc)
+DECLARE_DEP (pysqlite2 VERSION 0ff6e32-cb1 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc)
-DECLARE_DEP (python-snappy VERSION c97d633-cb1 PLATFORMS centos6 centos7 macosx ubuntu12.04 ubuntu14.04 debian7)
+DECLARE_DEP (python-snappy VERSION c97d633-cb1 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc)
-DECLARE_DEP (snappy VERSION 1.1.1-cb2 PLATFORMS centos6 centos7 macosx ubuntu12.04 ubuntu14.04 debian7)
+DECLARE_DEP (snappy VERSION 1.1.1-cb2 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 DECLARE_DEP (v8 VERSION 3.23.6 PLATFORMS windows_msvc)
-DECLARE_DEP (v8 VERSION e24973a-cb1 PLATFORMS centos6 centos7 macosx ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (v8 VERSION e24973a-cb2 PLATFORMS centos6 centos7 debian7 macosx sunos ubuntu12.04 ubuntu14.04)
 
-
-#
-# This is the new packaging module
-#
-DECLARE_DEP(curl VERSION 7.40.0-cb1 PLATFORMS sunos)
-DECLARE_DEP(icu4c VERSION 263593-cb4 PLATFORMS sunos)
-DECLARE_DEP(jemalloc VERSION 5d9732f-cb5 PLATFORMS sunos)
-DECLARE_DEP(libevent VERSION 2.0.22-cb1 PLATFORMS sunos)
-DECLARE_DEP(otp VERSION R16B03-cb1 PLATFORMS sunos)
-DECLARE_DEP(python-snappy VERSION c97d633-cb1 PLATFORMS sunos)
-DECLARE_DEP(snappy VERSION 1.1.1-cb2 PLATFORMS sunos)
-DECLARE_DEP(v8 VERSION e24973a-cb2 PLATFORMS sunos)
