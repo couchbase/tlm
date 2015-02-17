2cfca977baac9e270965ebe897d534809596eaa6
Dave Rigby <daver@couchbase.com>
2015-02-17 22:29:01 +0000

    MB-13418: Add FindCouchbaseDbgHelp.cmake
    Add a CMake module to locate Windows' Dbghelp library. This is needed by
    the backtrace printing functionality in platform - specifically
    SymGetModuleInfo64() / SynGetAddr().
    
    Change-Id: Ibd5f5929ed63b24ffdfa254d9c17db67e64d2c8d
    Reviewed-on: http://review.couchbase.org/46974
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ cmake/Modules/FindCouchbaseDbgHelp.cmake
@@ -0,0 +1,30 @@
+# Locate Windows' DbgHelp.dll
+
+# This module defines
+#  DBGHELP_LIBRARY, Library path and libs
+#  DBGHELP_DLL, Path to .dll file
+
+INCLUDE(PlatformIntrospection)
+
+FIND_LIBRARY(DBGHELP_LIBRARY NAMES "DbgHelp")
+
+# Need the .dll file to copy into our install directory. Note we require
+# a matching architecture file.
+_DETERMINE_ARCH(HOST_ARCH)
+IF(HOST_ARCH STREQUAL "amd64")
+  FILE(GLOB dbghelp_hint "$ENV{PROGRAMFILES}/Microsoft Visual Studio */Common7/IDE/Remote Debugger/x64")
+ELSE(HOST_ARCH STREQUAL "amd64")
+  FILE(GLOB dbghelp_hint "$ENV{PROGRAMFILES}/Microsoft Visual Studio */Common7/IDE/Remote Debugger/x86")
+ENDIF(HOST_ARCH STREQUAL "amd64")
+MESSAGE(STATUS "ENV{PROGRAMW6432} : $ENV{PROGRAMW6432}")
+MESSAGE(STATUS "ENV{PROGRAMFILES} : $ENV{PROGRAMFILES}")
+MESSAGE(STATUS "DbgHelp arch: ${HOST_ARCH} paths: ${dbghelp_hint}")
+FIND_FILE(DBGHELP_DLL dbghelp.dll NO_SYSTEM_ENVIRONMENT_PATH HINTS "${dbghelp_hint}")
+
+IF (DBGHELP_LIBRARY AND DBGHELP_DLL)
+  MESSAGE(STATUS "Found Dbghelp in ${DBGHELP_LIBRARY} : ${DBGHELP_DLL}")
+ELSE (DBGHELP_LIBRARY AND DBGHELP_DLL)
+  MESSAGE(FATAL_ERROR "Can't build Couchbase without DbgHelp.dll")
+ENDIF (DBGHELP_LIBRARY AND DBGHELP_DLL)
+
+MARK_AS_ADVANCED(DBGHELP_LIBRARY DBGHELP_DLL)
