cac96fe64a47222119a1ba5eb0ac0dfe55c81f91
Trond Norbye <trond.norbye@gmail.com>
2015-03-02 11:39:11 +0100

    Detect google flatbuffers
    Change-Id: I33aadcb922fe8c7e90b057af14c6dcea78d13f5d
    Reviewed-on: http://review.couchbase.org/47549
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

---
+++ cmake/Modules/FindCouchbaseFlatbuffers.cmake
@@ -0,0 +1,17 @@
+# Locate Google flatbuffers
+# This module defines
+#  FLATBUFFERS_INCLUDE_DIR, where to find the flatbuffer headers
+#  FLATC, the flatc binary
+SET(google_flatbuffers_exploded ${CMAKE_BINARY_DIR}/tlm/deps/flatbuffers.exploded)
+
+FIND_PATH(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
+          PATHS ${google_flatbuffers_exploded}/include)
+FIND_PROGRAM(FLATC flatc PATHS PATHS ${google_flatbuffers_exploded}/bin)
+
+IF (FLATBUFFERS_INCLUDE_DIR AND FLATC)
+  MESSAGE(STATUS "Found Google Flatbuffers in ${FLATBUFFERS_INCLUDE_DIR} : ${FLATC}")
+ELSE (FLATBUFFERS_INCLUDE_DIR AND FLATC)
+   MESSAGE(FATAL_ERROR "Google flatbuffers is a requirement")
+ENDIF (FLATBUFFERS_INCLUDE_DIR AND FLATC)
+
+MARK_AS_ADVANCED(FLATBUFFERS_INCLUDE_DIR FLATC)
