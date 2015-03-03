3469447906a438c9c4b6e2ab5b740443ecc0c1d7
Chris Hillery <ceej@couchbase.com>
2015-03-03 02:08:55 -0800

    Build Windows cbdeps for amd64 and x86.
    Change-Id: I892136fdc7e843747bd767408edf54170591765a
    Reviewed-on: http://review.couchbase.org/47608
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/scripts/build-one-cbdep.bat
+++ deps/scripts/build-one-cbdep.bat
@@ -1,5 +1,30 @@
+rem Build amd64 first
+
+set target_arch=amd64
 cd deps/packages
 mkdir build
 cd build
-cmake ..
-cmake --build . --target %PACKAGE%
+cmake .. || goto error
+cmake --build . --target %PACKAGE% || goto error
+
+rem Clean up
+
+cd ..
+rmdir /s /q build
+
+rem Build again for x86
+
+set target_arch=x86
+cd deps/packages
+mkdir build
+cd build
+cmake .. || goto error
+cmake --build . --target %PACKAGE% || goto error
+
+goto eof
+
+:error
+echo Failed with error %ERRORLEVEL%.
+exit /b %ERRORLEVEL%
+
+:eof
