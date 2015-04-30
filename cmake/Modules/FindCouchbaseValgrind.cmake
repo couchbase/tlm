2b01b0e2b0b62512686c9da5e27b605f94f2a19f
Dave Rigby <daver@couchbase.com>
2015-04-30 21:49:41 +0100

    Valgrind: Show all leak kinds; permit partial loads
    Enable two additional Valgrind options:
    
    --show-leak-kinds=all: Shows all leaks (both definite and possible).
    
    --partial-loads-ok=yes: Permits multibyte loads to regions of memory
      where only some of the bytes in the 'word' are valid to succeed, as
      long as only the valid bytes are actually used. This is essentially
      necessary on x86 as optimizations typically do this.
    
    Change-Id: Ie46112cb84cf6e6731e6f0fe9252bb8074b03c73
    Reviewed-on: http://review.couchbase.org/50436
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- cmake/Modules/FindCouchbaseValgrind.cmake
+++ cmake/Modules/FindCouchbaseValgrind.cmake
@@ -1,4 +1,4 @@
 # Locate and configure Valgrind for memory checking.
 
 find_program( MEMORYCHECK_COMMAND valgrind )
-set( MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --xml=yes --xml-file=memcheck.%p.xml" )
+set( MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --show-leak-kinds=all --partial-loads-ok=yes --xml=yes --xml-file=memcheck.%p.xml" )
