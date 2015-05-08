0b3130602c8f23f712ce22be11f82f0132d81144
Trond Norbye <trond.norbye@gmail.com>
2015-05-08 17:33:44 +0000

    Disable DTrace for FreeBSD
    The dtrace binary don't like our dtrace descriptor
    
    Change-Id: I06c7d5e4755995dbef937e4d951a11b996d7a144
    Reviewed-on: http://review.couchbase.org/50770
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- cmake/Modules/FindCouchbaseDtrace.cmake
+++ cmake/Modules/FindCouchbaseDtrace.cmake
@@ -1,18 +1,19 @@
-# stupid systemtap use a binary named dtrace as well..
+# stupid systemtap use a binary named dtrace as well, but it's not dtrace
+IF (NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
+   IF (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+      MESSAGE(STATUS "We don't have support for DTrace on FreeBSD")
+   ELSE (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+      FIND_PROGRAM(DTRACE dtrace)
+      IF (DTRACE)
+         SET(ENABLE_DTRACE True CACHE BOOL "Whether DTrace has been found")
+         MESSAGE(STATUS "Found dtrace in ${DTRACE}")
 
-IF (NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+         IF (CMAKE_SYSTEM_NAME MATCHES "SunOS")
+            SET(DTRACE_NEED_INSTUMENT True CACHE BOOL
+                "Whether DTrace should instrument object files")
+         ENDIF (CMAKE_SYSTEM_NAME MATCHES "SunOS")
+      ENDIF (DTRACE)
 
-FIND_PROGRAM(DTRACE dtrace)
-IF (DTRACE)
-   SET(ENABLE_DTRACE True CACHE BOOL "Whether DTrace has been found")
-   MESSAGE(STATUS "Found dtrace in ${DTRACE}")
-
-   IF (CMAKE_SYSTEM_NAME MATCHES "SunOS")
-      SET(DTRACE_NEED_INSTUMENT True CACHE BOOL
-          "Whether DTrace should instrument object files")
-   ENDIF (CMAKE_SYSTEM_NAME MATCHES "SunOS")
-ENDIF (DTRACE)
-
-MARK_AS_ADVANCED(DTRACE_NEED_INSTUMENT ENABLE_DTRACE DTRACE)
-
-ENDIF (NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+      MARK_AS_ADVANCED(DTRACE_NEED_INSTUMENT ENABLE_DTRACE DTRACE)
+   ENDIF (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+ENDIF (NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
