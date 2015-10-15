7ea21c3abb81bcaaec0963d210457c758209918d
Dave Rigby <daver@couchbase.com>
2015-10-15 11:58:34 +0100

    MB-16560: Create script to tag releases
    Example usage:
    
        tlm/tag_release.py 4.0.0 memcached
    
    Example output:
    
        pushd memcached
        git tag -a -m "4.0.0 release (memcached)" v4.0.0 a7a7729d2d46854d2759814167722e0e5618d2fc
        git push review v4.0.0
        popd
    
    Change-Id: Ib9aca09623a629bacf4e4383fa652e6bf01a0763
    Reviewed-on: http://review.couchbase.org/56150
    Tested-by: buildbot <build@couchbase.com>
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ tag_release.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""Script to tag projects with the release from the manifest.
+
+Requires: Permission to push tags to Gerrit
+
+Usage: Run from the top of a repo checkout:
+    tag_release.py <release> <projects..>
+
+e.g.
+
+    tag_release.py 4.0.0 memcached platform
+
+This will print the commands needed to tag the release.
+Review, then copy/paste.
+"""
+
+import os
+import sys
+import xml.etree.ElementTree
+
+if len(sys.argv) < 3:
+    print >> sys.stderr, "Usage: {} <release> <projects...>".format(
+        sys.argv[0])
+    exit(1)
+
+release = sys.argv[1]
+manifest_path = ".repo/manifests/released/" + release + ".xml"
+if not os.path.exists(manifest_path):
+    print >> sys.stderr, "No such file '" + manifest_path + "'. "
+    print >> sys.stderr, "Check specified release and current working" \
+    "directory (should be run from top-level of repo checkout)."
+    exit(2)
+
+projects_to_tag = sys.argv[2:]
+
+e = xml.etree.ElementTree.parse(manifest_path).getroot()
+for p in e.findall('project'):
+    if p.attrib['name'] in projects_to_tag:
+        name = p.attrib['name']
+        sha = p.attrib['revision']
+        print "pushd " + name
+        print """git tag -a -m "{0} release ({1})" v{0} {2}""".format(
+            release, name, sha)
+        print "git push review v{0}".format(release)
+        print "popd"
