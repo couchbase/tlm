716635064c4f6bd5922cdacbebf949e5bbe35a5c
harsha <hhs.couchbase@gmail.com>
2015-12-18 16:49:30 +0530

    MB-17043 [Re-enable] Upgrade V8 to version 4.8
    V8 APIs have changed from earlier version of 3.23 to current
    version of 4.8, current stable v8 version used in Chrome.
    Corresponding API changes are incorporated in this fix.
    
    Change-Id: I86e2271546dbddd03250179b6fc5a35b7eff96af
    Reviewed-on: http://review.couchbase.org/60619
    Reviewed-by: Volker Mische <volker.mische@gmail.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Harsha H S <hhs.couchbase@gmail.com>
    Tested-by: Harsha H S <hhs.couchbase@gmail.com>

--- cmake/Modules/FindCouchbaseIcu.cmake
+++ cmake/Modules/FindCouchbaseIcu.cmake
@@ -3,61 +3,14 @@
 #  ICU_FOUND, if false, do not try to link with ICU
 #  ICU_LIBRARIES, Library path and libs
 #  ICU_INCLUDE_DIR, where to find the ICU headers
+SET(_icu_exploded ${CMAKE_BINARY_DIR}/tlm/deps/icu4c.exploded)
 
-FIND_PROGRAM(ICU_CONFIG_EXECUTABLE
-             NAMES icu-config
-             HINTS ${CMAKE_INSTALL_PREFIX}/bin
-             PATHS
-                /usr/bin
-                /usr/local/bin
-                ~/bin
-             DOC "icu-config executable")
-MARK_AS_ADVANCED(ICU_CONFIG_EXECUTABLE)
-
-IF (ICU_CONFIG_EXECUTABLE)
-  MESSAGE(STATUS "Found icu-config in ${ICU_CONFIG_EXECUTABLE}")
-  EXECUTE_PROCESS(COMMAND ${ICU_CONFIG_EXECUTABLE} --cppflags-searchpath
-                  OUTPUT_VARIABLE ICU_INCLUDE_DIR
-                  ERROR_QUIET)
-  STRING(REGEX REPLACE "^[-/]I" "" ICU_INCLUDE_DIR "${ICU_INCLUDE_DIR}")
-
-  EXECUTE_PROCESS(COMMAND ${ICU_CONFIG_EXECUTABLE} --ldflags-searchpath
-                  OUTPUT_VARIABLE ICU_LIB_SEARCHPATH
-                  ERROR_QUIET)
-  STRING(REGEX REPLACE "^[-/]L" "" ICU_LIB_HINT_DIR "${ICU_LIB_SEARCHPATH}")
-
-  IF (NOT WIN32)
-      EXECUTE_PROCESS(COMMAND ${ICU_CONFIG_EXECUTABLE} --ldflags-libsonly
-                      OUTPUT_VARIABLE _icu_libraries
-                      ERROR_QUIET)
-
-      IF (_icu_libraries)
-          STRING(STRIP ${_icu_libraries} icu_libraries)
-          STRING(REPLACE "-l" "" _icu_libraries ${icu_libraries})
-          STRING(REPLACE " " ";" _icu_libraries ${_icu_libraries})
-
-          FOREACH(_mylib ${_icu_libraries})
-             UNSET(_the_lib CACHE)
-             FIND_LIBRARY(_the_lib
-                          NAMES ${_mylib}
-                          HINTS ${ICU_LIB_HINT_DIR})
-             IF (_the_lib)
-                LIST(APPEND ICU_LIBRARIES ${_the_lib})
-             ELSE (_the_lib)
-                MESSAGE(FATAL_ERROR "Failed to locate ${_mylib}")
-             ENDIF (_the_lib)
-          ENDFOREACH(_mylib)
-      ENDIF(_icu_libraries)
-  ENDIF(NOT WIN32)
-ELSE (ICU_CONFIG_EXECUTABLE)
-  # Mostly for Windows, where icu-config is not common
-  FIND_PATH(ICU_INCLUDE_DIR
-            NAMES unicode/utypes.h utypes.h
-            PATH_SUFFIXES include
-            DOC "Include directories for ICU")
-  # Don't set ICU_LIB_HINT_DIR; depend on FIND_LIBRARY() calls below
-ENDIF(ICU_CONFIG_EXECUTABLE)
-
+FIND_PATH(ICU_INCLUDE_DIR unicode/utypes.h
+          HINTS
+              ENV ICU_DIR
+          PATH_SUFFIXES include
+          PATHS
+               ${_icu_exploded})
 
 IF (ICU_INCLUDE_DIR)
   STRING(STRIP ${ICU_INCLUDE_DIR} ICU_INCLUDE_DIR)
--- cmake/Modules/FindCouchbaseV8.cmake
+++ cmake/Modules/FindCouchbaseV8.cmake
@@ -2,33 +2,44 @@
 # This module defines
 #  V8_FOUND, if false, do not try to link with v8
 #  V8_LIBRARIES, Library path and libs
-#  V8_INCLUDE_DIR, where to find the ICU headers
+#  V8_INCLUDE_DIR, where to find V8 headers
 
-FIND_PATH(V8_INCLUDE_DIR v8.h
+SET(_v8_exploded ${CMAKE_BINARY_DIR}/tlm/deps/v8.exploded)
+
+# V8 inlcudes libplatform/libplatform.h which includes another header as
+# `#include <include/v8-platform.h>`. To allow this inclusion we are setting
+# V8_INCLUDE_DIR path up until include directory
+FIND_PATH(V8_INCLUDE_DIR include/v8.h
           HINTS
                ENV V8_DIR
-          PATH_SUFFIXES include
           PATHS
-               ~/Library/Frameworks
-               /Library/Frameworks
-               /opt/local
-               /opt/csw
-               /opt/v8
-               /opt/v8/include
-               /opt)
+               ${_v8_exploded})
 
-FIND_LIBRARY(V8_LIBRARIES
-             NAMES v8
-             HINTS
-                 ENV V8_DIR
-             PATHS
-                 ~/Library/Frameworks
-                 /Library/Frameworks
-                 /opt/local
-                 /opt/csw
-                 /opt/v8
-                 /opt/v8/lib
-                 /opt)
+IF (WIN32)
+  # RelWithDebInfo & MinSizeRel should use the Release libraries, otherwise use
+  # the same directory as the build type.
+  IF(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+    SET(_build_type "Release")
+  ELSE()
+    SET(_build_type ${CMAKE_BUILD_TYPE})
+  ENDIF()
+
+  FIND_LIBRARY(V8_SHAREDLIB
+               NAMES v8.dll
+               PATHS ${_v8_exploded}/lib/${_build_type})
+  FIND_LIBRARY(V8_PLATFORMLIB
+               NAMES v8_libplatform
+               PATHS ${_v8_exploded}/archive/${_build_type})
+  FIND_LIBRARY(V8_BASELIB
+               NAMES v8_libbase
+               PATHS ${_v8_exploded}/archive/${_build_type})
+  SET(V8_LIBRARIES ${V8_SHAREDLIB} ${V8_PLATFORMLIB} ${V8_BASELIB} winmm)
+ELSE (WIN32)
+  FIND_LIBRARY(V8_LIBRARIES
+               NAMES v8
+               HINTS
+                   ENV V8_DIR)
+ENDIF (WIN32)
 
 IF (V8_LIBRARIES)
   MESSAGE(STATUS "Found v8 in ${V8_INCLUDE_DIR} : ${V8_LIBRARIES}")
