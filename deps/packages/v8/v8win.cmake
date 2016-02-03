affd0c8e01aa853ae2c6e2bf27bbd9c58a979974
Volker Mische <vmx@couchbase.com>
2016-02-03 14:38:15 +0100

    MB-17043: Build V8 4.8 on Windows
    Add instructions to build the V8 dependency on Windows
    32-bit and 64-bit (release and debug build each).
    
    Change-Id: If6222c825cad688ff25c0302c76b48fa0abde7f0
    Reviewed-on: http://review.couchbase.org/59460
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Harsha H S <hhs.couchbase@gmail.com>

---
+++ deps/packages/v8/v8win.cmake
@@ -0,0 +1,7 @@
+SET (ENV{DEPOT_TOOLS_WIN_TOOLCHAIN} 0)
+SET (ENV{GYP_GENERATORS} "ninja")
+SET (GYP_FLAGS "-Dcomponent=shared_library" "-Dv8_enable_backtrace=1"
+               "-Dv8_use_snapshot='false'" "-Dv8_use_external_startup_data=0"
+               "-Dv8_enable_i18n_support=1" "-Dtest_isolation_mode=noop"
+               "-Dv8_target_arch=${MSBUILD_ARCH}")
+EXECUTE_PROCESS(COMMAND "python" "build\\gyp_v8" ${GYP_FLAGS})
