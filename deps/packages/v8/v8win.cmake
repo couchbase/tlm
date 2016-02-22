ae9ae208e088f726ff0349755033188b98cf578b
Volker Mische <vmx@couchbase.com>
2016-02-22 20:58:00 +0100

    MB-17043: Set correct target architecture for V8 in Windows
    To build the 32-bit version of V8 on Windows the v8_target_arch
    is `ia32`. For 64-bit it's `x64`.
    
    Change-Id: I00bd747a56a6447bb41faee47b8a52e438cab52a
    Reviewed-on: http://review.couchbase.org/60346
    Reviewed-by: Volker Mische <volker.mische@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- deps/packages/v8/v8win.cmake
+++ deps/packages/v8/v8win.cmake
@@ -3,5 +3,5 @@ SET (ENV{GYP_GENERATORS} "ninja")
 SET (GYP_FLAGS "-Dcomponent=shared_library" "-Dv8_enable_backtrace=1"
                "-Dv8_use_snapshot='false'" "-Dv8_use_external_startup_data=0"
                "-Dv8_enable_i18n_support=1" "-Dtest_isolation_mode=noop"
-               "-Dv8_target_arch=${MSBUILD_ARCH}")
+               "-Dv8_target_arch=${V8_TARGET_ARCH}")
 EXECUTE_PROCESS(COMMAND "python" "build\\gyp_v8" ${GYP_FLAGS})
