3ab46ea95f1f34b1d4986067a52607791b1af50f
Trond Norbye <trond.norbye@gmail.com>
2016-04-21 13:20:44 +0200

    Locate https://github.com/nlohmann/json
    Set up the compile environment so that users may use the
    library by adding:
    
        #include <nlohmann/json.hpp>
    
    Change-Id: Idaf536a40e7e7e0d0c45c9683fa5a4a72058d8a7
    Reviewed-on: http://review.couchbase.org/63178
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

---
+++ cmake/Modules/FindCouchbaseNlohmannJson.cmake
@@ -0,0 +1,18 @@
+# Locate nlohmann JSON library
+# This module defines
+#
+#  NLOHMANN_JSON_INCLUDE_DIR
+
+SET(_nhlomann_json_exploded ${CMAKE_BINARY_DIR}/tlm/deps/json.exploded)
+
+FIND_PATH(NLOHMANN_JSON_INCLUDE_DIR nlohmann/json.hpp
+          HINTS
+               ENV NLOHMANN_JSON_DIR
+          PATH_SUFFIXES include
+          PATHS ${_nhlomann_json_exploded})
+
+IF (NOT NLOHMANN_JSON_INCLUDE_DIR)
+  MESSAGE(ERROR "Can't build Couchbase without https://github.com/nlohmann/json")
+ENDIF (NOT NLOHMANN_JSON_INCLUDE_DIR)
+
+MARK_AS_ADVANCED(NLOHMANN_JSON_INCLUDE_DIR)
