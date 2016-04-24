b7934e40a41083607735ed71b7dab9d0e23db691
Trond Norbye <trond.norbye@gmail.com>
2016-04-24 10:55:15 +0200

    MB-19339: Search and locate LDAP
    Memcached is supposed to authenticate users defined in LDAP
    as part of the upcomming RBAC work
    
    Change-Id: I4126c99801c0f8b803d5eef94a744ab428acdc8d
    Reviewed-on: http://review.couchbase.org/63291
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

---
+++ cmake/Modules/FindCouchbaseLdap.cmake
@@ -0,0 +1,39 @@
+# Locate LDAP library (We've only tested this on systems
+# using OpenLDAP)
+#
+# This module defines
+#  LDAP_FOUND, if false, do not try to link with LDAP support
+#  LDAP_LIBRARIES
+#  LDAP_INCLUDE_DIR
+#
+# If found -DHAVE_LDAP=1 is added to the compile environment
+
+FIND_PATH(LDAP_INCLUDE_DIR ldap.h
+          HINTS
+               ENV LDAP_DIR
+          PATH_SUFFIXES include
+          PATHS
+               /opt/local
+               /opt/csw)
+
+FIND_LIBRARY(LDAP_LIBRARIES
+             NAMES ldap
+             HINTS
+                 ENV LDAP_DIR
+             PATH_SUFFIXES lib
+             PATHS
+                 /opt/local
+                 /opt/csw)
+
+IF (LDAP_INCLUDE_DIR AND LDAP_LIBRARIES)
+    MESSAGE(STATUS "Found ldap in ${LDAP_INCLUDE_DIR} : ${LDAP_LIBRARIES}")
+    ADD_DEFINITIONS(-DHAVE_LDAP=1)
+    SET(LDAP_FOUND True)
+ELSE (LDAP_INCLUDE_DIR AND LDAP_LIBRARIES)
+    SET(LDAP_FOUND False)
+    IF (CB_ENTERPRISE_EDITION AND NOT WIN32)
+        MESSAGE(FATAL_ERROR "Can't build Couchbase EE without LDAP")
+    ENDIF (CB_ENTERPRISE_EDITION AND NOT WIN32)
+ENDIF (LDAP_INCLUDE_DIR AND LDAP_LIBRARIES)
+
+MARK_AS_ADVANCED(LDAP_FOUND LDAP_INCLUDE_DIR LDAP_LIBRARIES)
