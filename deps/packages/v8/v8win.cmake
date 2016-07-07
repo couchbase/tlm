859c29b229732dd938ed921e572de43e5d712c8b
harsha <hhs.couchbase@gmail.com>
2016-07-07 19:17:14 +0530

    MB-20007 gyp files are moved out from build into gypfiles directory
    Change-Id: Iedf243a165be4fe83d14b4eef4a4b8cce497f259
    Reviewed-on: http://review.couchbase.org/65580
    Well-Formed: buildbot <build@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Harsha H S <hhs.couchbase@gmail.com>

--- deps/packages/v8/v8win.cmake
+++ deps/packages/v8/v8win.cmake
@@ -4,4 +4,4 @@ SET (GYP_FLAGS "-Dcomponent=shared_library" "-Dv8_enable_backtrace=1"
                "-Dv8_use_snapshot=true" "-Dv8_use_external_startup_data=0"
                "-Dv8_enable_i18n_support=1" "-Dtest_isolation_mode=noop"
                "-Dv8_target_arch=${V8_TARGET_ARCH}")
-EXECUTE_PROCESS(COMMAND "python" "build\\gyp_v8" ${GYP_FLAGS})
+EXECUTE_PROCESS(COMMAND "python" "gypfiles\\gyp_v8" ${GYP_FLAGS})
