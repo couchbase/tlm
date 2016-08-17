5dacd9e24a5722892a675a3b3df673b6a2329dfc
Sergey Avseyev <sergey.avseyev@gmail.com>
2016-08-17 20:46:35 +0300

    Enable dtrace for FreeBSD
    $ sudo dtrace -l -n :memcached::  -c ./install/bin/memcached
    ID    PROVIDER        MODULE    FUNCTION                                                        NAME
    67881 memcached41855  memcached _ZL15process_bin_getP14McbpConnection                           command-get
    67882 memcached41855  memcached conn_pipe_new                                                   conn-allocate
    67883 memcached41855  memcached conn_new                                                        conn-allocate
    67884 memcached41855  memcached conn_new_server                                                 conn-allocate
    67885 memcached41855  memcached _ZN10ConnectionC2EiP10event_base                                conn-create
    67886 memcached41855  memcached _ZN10ConnectionD1Ev                                             conn-destroy
    67887 memcached41855  memcached _Z17dispatch_conn_newii                                         conn-dispatch
    67888 memcached41855  memcached _ZN16McbpStateMachine14setCurrentTaskER14McbpConnectionPFbPS0_E process-command-end
    67889 memcached41855  memcached _ZL20dispatch_bin_commandP14McbpConnection                      process-command-start
    
    FreeBSD has better dtrace than Linux :)
    
    Change-Id: Ie7e339fdf535bc879ddb6ba01a39c9ec48e02821
    Reviewed-on: http://review.couchbase.org/66837
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- cmake/Modules/FindCouchbaseDtrace.cmake
+++ cmake/Modules/FindCouchbaseDtrace.cmake
@@ -1,19 +1,15 @@
 # stupid systemtap use a binary named dtrace as well, but it's not dtrace
 IF (NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
-   IF (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
-      MESSAGE(STATUS "We don't have support for DTrace on FreeBSD")
-   ELSE (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
       FIND_PROGRAM(DTRACE dtrace)
       IF (DTRACE)
          SET(ENABLE_DTRACE True CACHE BOOL "Whether DTrace has been found")
          MESSAGE(STATUS "Found dtrace in ${DTRACE}")
 
-         IF (CMAKE_SYSTEM_NAME MATCHES "SunOS")
+         IF (CMAKE_SYSTEM_NAME MATCHES "SunOS|FreeBSD")
             SET(DTRACE_NEED_INSTRUMENT True CACHE BOOL
                 "Whether DTrace should instrument object files")
-         ENDIF (CMAKE_SYSTEM_NAME MATCHES "SunOS")
+         ENDIF (CMAKE_SYSTEM_NAME MATCHES "SunOS|FreeBSD")
       ENDIF (DTRACE)
 
       MARK_AS_ADVANCED(DTRACE_NEED_INSTRUMENT ENABLE_DTRACE DTRACE)
-   ENDIF (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
 ENDIF (NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
