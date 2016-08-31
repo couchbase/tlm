fbcb6c5bb160bfb6fda3f0a1c62f9e9df8021d4c
Chris Hillery <ceej@couchbase.com>
2016-08-31 02:55:02 -0700

    MB-15796: Move windows_msvc2015 to v8 5.2.
    I had difficulty getting a working MSVC 2015 build with v8 4.8, even after
    rebuilding v8 with MSVC 2015. Since we intend to move to 5.2 soon anyway,
    I'm moving the windows_msvc2015 platform there now (it builds cleanly).
    
    Also fix typo in go-yacc output.
    
    Change-Id: I55c6d371095a14578707c05f4f2d3bc3bd35cae0
    Reviewed-on: http://review.couchbase.org/67202
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- cmake/Modules/go-yacc.cmake
+++ cmake/Modules/go-yacc.cmake
@@ -3,7 +3,7 @@ SET (ENV{GOROOT} "${GOROOT}")
 SET (GO_EXECUTABLE "${GOROOT}/bin/go")
 
 # Execute "go yacc"
-MESSAGE (STATUS "Executing: ${_goroot}/bin/go tool yacc ${YFILE}")
+MESSAGE (STATUS "Executing: ${GO_EXECUTABLE} tool yacc ${YFILE}")
 EXECUTE_PROCESS (RESULT_VARIABLE _failure
   COMMAND "${GO_EXECUTABLE}" tool yacc "${YFILE}")
 IF (_failure)
