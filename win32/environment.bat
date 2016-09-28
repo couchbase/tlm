b52d2f241f13c5d37c5f1213a3eae53d9fb4d2f0
Trond Norbye <trond.norbye@gmail.com>
2016-09-28 12:12:51 +0200

    Remove obsolete dirs and add phosphor
    This allows for running ctest in build\memcached
    
    Change-Id: I107f785bf6147ef1a0562473f0567505bcb210c3
    Reviewed-on: http://review.couchbase.org/68093
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- win32/environment.bat
+++ win32/environment.bat
@@ -50,11 +50,9 @@ rem test in a module..
 echo Setting compile environment for building Couchbase server
 set OBJDIR=\build
 set MODULEPATH=%SOURCE_ROOT%%OBJDIR%\platform
-set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\libvbucket
-set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\cbsasl
+set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\phosphor
 set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\memcached
 set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\couchstore
-set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\libmemcached
 set MODULEPATH=%MODULEPATH%;%SOURCE_ROOT%%OBJDIR%\sigar\build-src
 set PATH=%MODULEPATH%;%PATH%;%SOURCE_ROOT%\install\bin
 set OBJDIR=
