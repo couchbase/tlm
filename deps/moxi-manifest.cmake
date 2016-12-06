efd7991c357555e05f3cdf7b9d226d8bd01a920d
Chris Hillery <ceej@couchbase.com>
2016-12-06 02:15:40 -0800

    MB-21688: New CMake scripts for client-side moxi
    Change-Id: Ibf767d87dd0392852bb0e85c80556e9f5ae5fcc8
    Reviewed-on: http://review.couchbase.org/70674
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ deps/moxi-manifest.cmake
@@ -0,0 +1,24 @@
+#
+# Keep the list sorted alphabetically, and the platforms alphabetically.
+# The syntax is:
+#
+# DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
+#
+# Please note that this file contains entries for both supported and
+# unsupported platforms. The reason for putting unsupported platforms
+# in this file is that people may then build the dependencies from:
+# tlm/deps/packages and store the resulting dependency files in
+# ~/.cbdepscache and build the system without having to patch this
+# file (where people is people like Trond who wants to set up automated
+# builders on platforms like SmartOS/Solars, FreeBSD etc).
+#
+# The list of supported platforms may change between releases, but
+# you may use the cmake macro GET_SUPPORTED_PRODUCTION_PLATFORM to
+# check if this is a supported platform.
+#
+DECLARE_DEP (curl VERSION 7.35.0 PLATFORMS windows_msvc)
+DECLARE_DEP (curl VERSION 7.40.0-cb2 PLATFORMS centos6 centos7 debian7 debian8 freebsd sunos suse11.2 ubuntu12.04 ubuntu14.04)
+DECLARE_DEP (libevent VERSION 2.1.4-alpha-dev PLATFORMS windows_msvc)
+DECLARE_DEP (libevent VERSION 2.0.22-cb1 PLATFORMS macosx)
+DECLARE_DEP (libevent VERSION 2.0.22-cb2 PLATFORMS centos6 centos7 debian7 debian8 freebsd sunos suse11.2 ubuntu12.04 ubuntu14.04)
+
