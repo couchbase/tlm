92435185da7c008f9428aac623e90efbb7ce5e92
olivermd <oliver.downard@couchbase.com>
2017-01-12 12:42:08 +0000

    Search cb-deps for libevent before system path
    CMake would search everything else it can before using the
    suggestion made using PATH. This means that it will find system libevent
    headers and libraries before using the ones from cb dependencies.
    This can result in a mismatch between the headers used and the libraries
    linked and cause other issues relating to version differences.
    
    Use HINTS so that CMake searches CMake cache variables, CMake
    environment variables and then cb dependencies. Previously PATH also
    searched system paths and environment variables before cb
    dependencies.
    
    Change-Id: I1e475011306c52a18d8d93a723b2bfe03958a1fd
    Reviewed-on: http://review.couchbase.org/71913
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbaseLibevent.cmake
+++ cmake/Modules/FindCouchbaseLibevent.cmake
@@ -7,19 +7,19 @@
 SET(_libevent_exploded ${CMAKE_BINARY_DIR}/tlm/deps/libevent.exploded)
 
 FIND_PATH(LIBEVENT_INCLUDE_DIR event2/event.h
-          PATHS ${_libevent_exploded}/include)
+          HINTS ${_libevent_exploded}/include)
 
 FIND_LIBRARY(LIBEVENT_CORE_LIB
              NAMES event_core libevent_core
-             PATHS ${CMAKE_INSTALL_PATH}/lib)
+             HINTS ${CMAKE_INSTALL_PATH}/lib)
 
 FIND_LIBRARY(LIBEVENT_THREAD_LIB
              NAMES event_pthreads
-             PATHS ${CMAKE_INSTALL_PATH}/lib)
+             HINTS ${CMAKE_INSTALL_PATH}/lib)
 
 FIND_LIBRARY(LIBEVENT_EXTRA_LIB
              NAMES event_extra
-             PATHS ${CMAKE_INSTALL_PATH}/lib)
+             HINTS ${CMAKE_INSTALL_PATH}/lib)
 
 IF (LIBEVENT_INCLUDE_DIR AND LIBEVENT_CORE_LIB)
   MESSAGE(STATUS "Found libevent headers in: ${LIBEVENT_INCLUDE_DIR}")
