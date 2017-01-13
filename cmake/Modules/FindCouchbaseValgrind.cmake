c06e7792e7601089ab01e6f5976f9a40f6697d2b
Dave Rigby <daver@couchbase.com>
2017-01-13 16:04:31 +0000

    FindCouchbaseValgrind: sort options and wrap long lines
    No semantic change.
    
    Change-Id: I01933aebe32a852034fed6a494b2b3c4f3de8128
    Reviewed-on: http://review.couchbase.org/71990
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- cmake/Modules/FindCouchbaseValgrind.cmake
+++ cmake/Modules/FindCouchbaseValgrind.cmake
@@ -1,4 +1,14 @@
 # Locate and configure Valgrind for memory checking.
 
 find_program( MEMORYCHECK_COMMAND valgrind )
-set( MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --show-leak-kinds=all --partial-loads-ok=yes --num-callers=32 --track-origins=yes --gen-suppressions=all --suppressions=${CMAKE_SOURCE_DIR}/tlm/valgrind.supp --xml=yes --xml-file=memcheck.%p.xml" )
+set( _valgrind_options "--gen-suppressions=all"
+                       "--leak-check=full"
+                       "--num-callers=32"
+                       "--partial-loads-ok=yes"
+                       "--show-leak-kinds=all"
+                       "--suppressions=${CMAKE_SOURCE_DIR}/tlm/valgrind.supp"
+                       "--trace-children=yes"
+                       "--track-origins=yes"
+                       "--xml=yes --xml-file=memcheck.%p.xml" )
+
+string(REPLACE ";" " " MEMORYCHECK_COMMAND_OPTIONS "${_valgrind_options}")
