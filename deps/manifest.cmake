1525ca9f85e6ab465a5ad98729b4d5e455dbe6de
Chris Hillery <ceej@couchbase.com>
2017-03-29 04:07:14 -0700

    MB-20007: Move all platforms to v8 5.2
    Change-Id: Ibadc071cbad8b14f73c3acd4612231da89a41a98
    Reviewed-on: http://review.couchbase.org/75965
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -35,7 +35,7 @@ DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc windows_msvc20
 DECLARE_DEP (python-snappy VERSION c97d633-cb1 PLATFORMS centos6 centos7 debian7 debian8 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc windows_msvc2015)
 DECLARE_DEP (snappy VERSION 1.1.1-cb2 PLATFORMS centos6 centos7 debian7 debian8 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04)
-DECLARE_DEP (v8 VERSION 4.8-cb4 PLATFORMS centos6 centos7 debian7 debian8 macosx sunos suse11.2 ubuntu12.04 ubuntu14.04 windows_msvc)
-DECLARE_DEP (v8 VERSION 5.2-cb1 PLATFORMS suse12.2 ubuntu16.04 windows_msvc2015)
+DECLARE_DEP (v8 VERSION 5.2-cb1 PLATFORMS centos6 centos7 debian7 debian8 macosx suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 windows_msvc windows_msvc2015)
+DECLARE_DEP (v8 VERSION 4.8-cb4 PLATFORMS sunos)
 # 4.8-cb5 contains build fix only for FreeBSD; other platforms don't need it
 DECLARE_DEP (v8 VERSION 4.8-cb5 PLATFORMS freebsd)
