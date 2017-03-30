8acb7b9d8058e611a71ad6443e34d60a1b729e46
Chris Hillery <ceej@couchbase.com>
2017-03-30 00:37:27 -0700

    MB-22582: Remove dependency on pysqlite2
    Change-Id: I08fa319ce34305ae8cae56014fc3d0e95cd8df13
    Reviewed-on: http://review.couchbase.org/76028
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -30,7 +30,6 @@ DECLARE_DEP (json VERSION 1.1.0-cb1 PLATFORMS centos6 centos7 debian7 debian8 fr
 DECLARE_DEP (libevent VERSION 2.1.8-cb2 PLATFORMS centos6 centos7 debian7 debian8 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 windows_msvc windows_msvc2015)
 DECLARE_DEP (openssl VERSION 1.0.1h PLATFORMS windows_msvc windows_msvc2015)
 DECLARE_DEP (openssl_h VERSION 0.9.8za-cb1 PLATFORMS macosx)
-DECLARE_DEP (pysqlite2 VERSION 0ff6e32-cb1 PLATFORMS centos6 centos7 debian7 debian8 macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc windows_msvc2015)
 DECLARE_DEP (python-snappy VERSION c97d633-cb1 PLATFORMS centos6 centos7 debian7 debian8 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc windows_msvc2015)
