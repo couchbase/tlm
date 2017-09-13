29b329f6b796b3fd2b374c681d1495b739aa2dae
Chris Hillery <ceej@couchbase.com>
2017-09-13 02:09:17 -0700

    CBD-2082: Fix libuv RPATH for MacOS
    Change-Id: I913926b9ab1fdb87120eab36b70d5d1772933d85
    Reviewed-on: http://review.couchbase.org/83337
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- deps/packages/libuv/libuv_unix.sh
+++ deps/packages/libuv/libuv_unix.sh
@@ -6,3 +6,8 @@ INSTALL_DIR=$1
 ./autogen.sh   || exit 1
 ./configure --disable-silent-rules --prefix=${INSTALL_DIR} || exit 1
 make &&  make install
+
+# For MacOS, tweak install_name
+if [ $(uname -s) = "Darwin" ]; then
+    install_name_tool -id @rpath/libuv.1.dylib ${INSTALL_DIR}/lib/libuv.1.dylib
+fi
