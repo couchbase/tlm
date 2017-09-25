3bf3def4e24bcf7518a6e8a497f807b637528f28
Chris Hillery <ceej@couchbase.com>
2017-09-25 01:00:15 -0700

    CBD-2117: Fix packaging of libcouchbase .so files
    Change-Id: I28dc6d17f7b389401dd6dca055a17ff4f90e6a6c
    Reviewed-on: http://review.couchbase.org/83672
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -30,7 +30,7 @@ DECLARE_DEP (jemalloc VERSION 4.1.0-cb2 PLATFORMS windows_msvc windows_msvc2015 
 DECLARE_DEP (jemalloc VERSION 4.0.4-cb2 PLATFORMS freebsd sunos)
 DECLARE_DEP (jemalloc VERSION 4.3.1-cb2 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
 DECLARE_DEP (json VERSION 1.1.0-cb1 PLATFORMS centos6 centos7 debian7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04 windows_msvc windows_msvc2015 windows_msvc2017)
-DECLARE_DEP (libcouchbase VERSION 2.7.7-cb4 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04 windows_msvc2015)
+DECLARE_DEP (libcouchbase VERSION 2.7.7-cb5 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04 windows_msvc2015)
 DECLARE_DEP (libevent VERSION 2.1.8-cb2 PLATFORMS centos6 centos7 debian7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04 windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (libuv VERSION 1.13.1-cb4 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11.2 suse12.2 ubuntu14.04 ubuntu16.04 ubuntu17.04 windows_msvc2015)
 DECLARE_DEP (openssl VERSION 1.0.2k-cb1 PLATFORMS macosx windows_msvc windows_msvc2015 windows_msvc2017)
