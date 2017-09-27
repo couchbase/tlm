2a96c0d12375b70a5ae0ace50b5fe2638ecd8dc2
Paolo Cocchi <paolo.cocchi@couchbase.com>
2017-09-27 08:59:54 +0100

    MB-26158 [2/2] [RocksDB] Use the new portable shared library
    With this change we set the new portable RocksDB library as dependency
    for kv-engine.
    
    Change-Id: Iabc69a11b52209d132dd8aea54ec7eddc54decf5
    Reviewed-on: http://review.couchbase.org/83770
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -37,7 +37,7 @@ DECLARE_DEP (libuv VERSION 1.13.1-cb4 PLATFORMS centos6 centos7 debian7 debian8 
 DECLARE_DEP (openssl VERSION 1.0.2k-cb1 PLATFORMS macosx windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (python-snappy VERSION c97d633-cb1 PLATFORMS centos6 centos7 debian7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
-DECLARE_DEP (rocksdb VERSION 5.6.1-cb1 PLATFORMS centos6 centos7 debian7 debian8 macosx suse11.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
+DECLARE_DEP (rocksdb VERSION 5.6.1-cb2 PLATFORMS centos6 centos7 debian7 debian8 macosx suse11.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (snappy VERSION 1.1.1-cb2 PLATFORMS centos6 centos7 debian7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
 DECLARE_DEP (v8 VERSION 5.2-cb1 PLATFORMS windows_msvc2017)
