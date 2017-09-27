16046c24f05d53885338a8fbf7d661c9debc0938
Chris Hillery <ceej@couchbase.com>
2017-09-26 21:58:27 -0700

    CBD-2102: flex - fix @rpath on MacOS; add Windows build (gnuwin32)
    Change-Id: I7f4bfa3cc19437355d0f8bdd016847ce89b94244
    Reviewed-on: http://review.couchbase.org/83768
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/packages/flex/flex_unix.sh
+++ deps/packages/flex/flex_unix.sh
@@ -8,3 +8,7 @@ tar zxf flex-${VERSION}.tar.gz || exit 1
 cd flex-${VERSION} || exit 1
 ./configure --prefix=${INSTALL_DIR} || exit 1
 make && make install
+
+if [ $(uname -s) = "Darwin" ]; then
+    install_name_tool -id @rpath/libfl.2.dylib ${INSTALL_DIR}/lib/libfl.2.dylib
+fi
--- deps/packages/flex/flex_windows.bat
+++ deps/packages/flex/flex_windows.bat
@@ -1,1 +1,8 @@
-# TBD
+@echo on
+
+set INSTALL_DIR=%1
+
+mkdir tmp
+cd tmp
+cmake -E tar xf ..\flex-2.5.4a-1-bin.zip
+xcopy /I bin %INSTALL_DIR%\bin
