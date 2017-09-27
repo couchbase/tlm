d822fca6e0176589293fce6066f0cf9deb6bbc70
Kenneth Lareau <kenneth.lareau@couchbase.com>
2017-09-27 15:11:10 -0700

    CBD-2113: Ensure zlib include and lib paths are found by top-level CMakeLists.txt
    Change-Id: Ia610cfdb104067a3af9ded13038efd43abd1a254
    Reviewed-on: http://review.couchbase.org/83785
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -46,4 +46,4 @@ DECLARE_DEP (v8 VERSION 5.9-cb3 PLATFORMS centos6 centos7 debian8 debian9 macosx
 DECLARE_DEP (v8 VERSION 4.8-cb4 PLATFORMS sunos)
 # 4.8-cb5 contains build fix only for FreeBSD; other platforms don't need it
 DECLARE_DEP (v8 VERSION 4.8-cb5 PLATFORMS freebsd)
-DECLARE_DEP (zlib VERSION 1.2.11-cb1 PLATFORMS centos6 centos7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu14.04 ubuntu16.04)
+DECLARE_DEP (zlib VERSION 1.2.11-cb2 PLATFORMS centos6 centos7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu14.04 ubuntu16.04 windows_msvc2015)
