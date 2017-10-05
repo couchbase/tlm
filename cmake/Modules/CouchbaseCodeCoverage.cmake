f8a3e831284a8d0e583cb30d502605b68acb6d34
Eugen-Alexandru Virtan <eugen.virtan@couchbase.com>
2017-10-05 13:18:52 +0100

    Update documentation for running code coverage reports
    Update the documentation to include the correct function call and
    add a couple of useful notes.
    
    Change-Id: I8aa09da6044ed824f86b0875279ff01d29d08ee4
    Reviewed-on: http://review.couchbase.org/84058
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/CouchbaseCodeCoverage.cmake
+++ cmake/Modules/CouchbaseCodeCoverage.cmake
@@ -1,7 +1,7 @@
 # Support for running code coverage reporting.
 #
 # Usage:
-# 1). Add a call to ADD_COVERAGE_REPORT() to the module(s) you wish to
+# 1). Add a call to ENABLE_CODE_COVERAGE_REPORT() to the module(s) you wish to
 #     obtain code coverage reports on.
 # 2). Enable the option CB_CODE_COVERAGE (e.g. pass -DCB_CODE_COVERAGE=ON to cmake).
 # 3). Build as normal.
@@ -9,7 +9,11 @@
 # 5). Run `make coverage-report-html` and/or `coverage-report-xml`
 #     (from the selected module subdirectory) to generate the reports.
 # 6) (Optional) to zero coverage counters before a re-run run `make coverage-zero-counters`.
-
+#
+# Note 1: Do make sure you have gcov and gcovr installed!
+#
+# Note 2: If you followed all the steps above but the coverage report is empty,
+# try to use "touch CMakeCache.txt" in your build directory, then rebuild the project and rerun the coverage report
 
 OPTION(CB_CODE_COVERAGE "Enable code coverage testing."
        OFF)
