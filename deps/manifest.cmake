d8f7cfabbd7975a295b00bafab7e036b34bdcc59
Paolo Cocchi <paolo.cocchi@couchbase.com>
2017-10-09 12:39:59 +0100

    MB-26298 [2/2] [RocksDB] Use the new `Fast CRC32`-enabled library
    With this change we use the new RocksDB library that enables Fast CRC32.
    
    Change-Id: Ifc4dcc1df41c228c1ba3112f5dcae428e5c522ef
    Reviewed-on: http://review.couchbase.org/84141
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -38,7 +38,7 @@ DECLARE_DEP (numactl VERSION 2.0.11-cb1 PLATFORMS centos6 centos7 debian7 debian
 DECLARE_DEP (openssl VERSION 1.0.2k-cb1 PLATFORMS macosx windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (python-snappy VERSION c97d633 PLATFORMS windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (python-snappy VERSION c97d633-cb1 PLATFORMS centos6 centos7 debian7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
-DECLARE_DEP (rocksdb VERSION 5.6.1-cb3 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11.2 suse12.2 ubuntu14.04 ubuntu16.04)
+DECLARE_DEP (rocksdb VERSION 5.6.1-cb4 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11.2 suse12.2 ubuntu14.04 ubuntu16.04)
 DECLARE_DEP (snappy VERSION 1.1.1 PLATFORMS windows_msvc windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (snappy VERSION 1.1.1-cb2 PLATFORMS centos6 centos7 debian7 debian8 debian9 freebsd macosx sunos suse11.2 suse12.2 ubuntu12.04 ubuntu14.04 ubuntu16.04 ubuntu17.04)
 DECLARE_DEP (v8 VERSION 5.2-cb1 PLATFORMS windows_msvc2017)
