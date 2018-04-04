7c0f2a99fdd6d52491e0feb940043542d1b16e45
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-04-04 15:31:53 -0700

    CBD-2432: Need to add ActivePerl to PATH for Windows to build OpenSSL
    Change-Id: I2f6beee5a50fc930163bca1ab437252712d0811c
    Reviewed-on: http://review.couchbase.org/92143
    Reviewed-by: Kim Trang <kim.trang@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/packages/openssl/openssl_windows.bat
+++ deps/packages/openssl/openssl_windows.bat
@@ -1,5 +1,8 @@
 set INSTALL_DIR=%1
 
+rem Need to add ActivePerl to path
+set PATH=C:\Perl64\bin;%PATH%
+
 rem Build OpenSSL binary and libraries
 call perl Configure VC-WIN64A --prefix=./build || goto error
 call ms\do_win64a.bat || goto error
