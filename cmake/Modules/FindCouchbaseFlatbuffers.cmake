4ae1d8cdd3770538238a26da17995473c51540fa
Trond Norbye <trond.norbye@gmail.com>
2018-05-23 10:38:15 +0200

    MB-29799: Refactor FindCouchbaseFlatbuffers
    * Bypass the entire file if it is already included
    * On supported platforms we should only pick up the header
      and library from cbdeps and not search other locations
    * Follow the cmake style and use lowercase letters
    
    Change-Id: Ia8aee52912a48f513fd88f513f380ca62af93bbe
    Reviewed-on: http://review.couchbase.org/94570
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbaseFlatbuffers.cmake
+++ cmake/Modules/FindCouchbaseFlatbuffers.cmake
@@ -1,17 +1,50 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate Google flatbuffers
 # This module defines
+#  FLATBUFFERS_FOUND, if we located flatbuffers
 #  FLATBUFFERS_INCLUDE_DIR, where to find the flatbuffer headers
 #  FLATC, the flatc binary
-SET(google_flatbuffers_exploded ${CMAKE_BINARY_DIR}/tlm/deps/flatbuffers.exploded)
+if (NOT DEFINED FLATBUFFERS_FOUND)
+    include(PlatformIntrospection)
 
-FIND_PATH(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
-          HINTS ${google_flatbuffers_exploded}/include)
-FIND_PROGRAM(FLATC flatc HINTS ${google_flatbuffers_exploded}/bin)
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform)
+        # Supported platforms should only use the provided hints and pick it up
+        # from cbdeps
+        set(_flatbuffers_no_default_path NO_DEFAULT_PATH)
+    endif ()
 
-IF (FLATBUFFERS_INCLUDE_DIR AND FLATC)
-  MESSAGE(STATUS "Found Google Flatbuffers in ${FLATBUFFERS_INCLUDE_DIR} : ${FLATC}")
-ELSE (FLATBUFFERS_INCLUDE_DIR AND FLATC)
-   MESSAGE(FATAL_ERROR "Google flatbuffers is a requirement")
-ENDIF (FLATBUFFERS_INCLUDE_DIR AND FLATC)
+    set(google_flatbuffers_exploded ${CMAKE_BINARY_DIR}/tlm/deps/flatbuffers.exploded)
 
-MARK_AS_ADVANCED(FLATBUFFERS_INCLUDE_DIR FLATC)
+    find_path(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
+              HINTS ${google_flatbuffers_exploded}/include
+              ${_flatbuffers_no_default_path})
+    if (NOT FLATBUFFERS_INCLUDE_DIR)
+        message(FATAL_ERROR "Failed to locate flatbuffers/flatbuffers.h")
+    endif ()
+
+    find_program(FLATC flatc HINTS ${google_flatbuffers_exploded}/bin ${_flatbuffers_no_default_path})
+    if (NOT FLATC)
+        message(FATAL_ERROR "Failed to locate flatc")
+    endif ()
+
+    message(STATUS "Found Google Flatbuffers headers in: ${FLATBUFFERS_INCLUDE_DIR}")
+    message(STATUS "                           compiler: ${FLATC}")
+
+    set(FLATBUFFERS_FOUND true CACHE BOOL "Found Google flatbuffers" FORCE)
+    mark_as_advanced(FLATBUFFERS_FOUND FLATBUFFERS_INCLUDE_DIR FLATC)
+endif (NOT DEFINED FLATBUFFERS_FOUND)
