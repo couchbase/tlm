c571dce462ca3585b477e03ea57babadba5d529c
Trond Norbye <trond.norbye@gmail.com>
2018-05-23 11:18:08 +0200

    MB-29799: Refactor FindCouchbaseCurl
    * Bypass the entire file if it is already included
    * On supported platforms we should only pick up the header
      and library from cbdeps and not search other locations
    * Follow the cmake style and use lowercase letters
    
    Change-Id: I97b0ff5404131c862808865c4e48bd0fa62e6355
    Reviewed-on: http://review.couchbase.org/94573
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbaseCurl.cmake
+++ cmake/Modules/FindCouchbaseCurl.cmake
@@ -1,45 +1,59 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate cURL library
 # This module defines
 #  CURL_FOUND, if false, do not try to link with cURL
 #  CURL_LIBRARIES, Library path and libs
-#  CURL_INCLUDE_DIR, where to find the ICU headers
+#  CURL_INCLUDE_DIR, where to find the cURL headers
 #
-# I didn't use the one provided with CMake
-# because it didn't work with the curl I downloaded
-# for windows..
+if (NOT DEFINED CURL_FOUND)
+    include(PlatformIntrospection)
+    # Use include files directly from cbdeps exploded download
+    set(_curl_exploded "${CMAKE_BINARY_DIR}/tlm/deps/curl.exploded")
 
-# Use include files directly from cbdeps exploded download
-SET (_curl_exploded "${CMAKE_BINARY_DIR}/tlm/deps/curl.exploded")
-FIND_PATH(CURL_INCLUDE_DIR curl/curl.h
-          HINTS
-               "${_curl_exploded}/include"
-               ENV CURL_DIR
-          PATH_SUFFIXES include
-          PATHS
-               ~/Library/Frameworks
-               /Library/Frameworks
-               /opt/local
-               /opt/csw
-               /opt/curl
-               /opt)
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform)
+        if (EXISTS ${_curl_exploded} AND IS_DIRECTORY ${_curl_exploded})
+            # Supported platforms should only use the provided hints and
+            # pick it up from cbdeps (but we don't bundle this for all
+            # platforms)
+            set(_curl_no_default_path NO_DEFAULT_PATH)
+        endif ()
+    endif ()
 
-FIND_LIBRARY(CURL_LIBRARIES
-             NAMES curl libcurl libcurl_imp
-             HINTS
-                "${_curl_exploded}/lib"
-                 ENV CURL_DIR
-             PATHS
-                 ~/Library/Frameworks
-                 /Library/Frameworks
-                 /opt/local
-                 /opt/csw
-                 /opt/curl
-                 /opt)
+    find_path(CURL_INCLUDE_DIR curl/curl.h
+              HINTS ${_curl_exploded}/include
+              ${_curl_no_default_path})
 
-IF (CURL_LIBRARIES)
-  MESSAGE(STATUS "Found cURL in ${CURL_INCLUDE_DIR} : ${CURL_LIBRARIES}")
-ELSE (CURL_LIBRARIES)
-  MESSAGE(FATAL_ERROR "Can't build Couchbase without cURL")
-ENDIF (CURL_LIBRARIES)
+    if (NOT CURL_INCLUDE_DIR)
+        message(FATAL_ERROR "Failed to locate curl/curl.h")
+    endif ()
 
-MARK_AS_ADVANCED(CURL_INCLUDE_DIR CURL_LIBRARIES)
+    find_library(CURL_LIBRARIES
+                 NAMES curl libcurl libcurl_imp
+                 HINTS ${CMAKE_INSTALL_PREFIX}/lib
+                 ${_curl_no_default_path})
+
+    if (NOT CURL_LIBRARIES)
+        message(FATAL_ERROR "Failed to locate curl, libcurl or libcurl_imp")
+    endif ()
+
+    message(STATUS "Found cURL headers in: ${CURL_INCLUDE_DIR}")
+    message(STATUS "            libraries: ${CURL_LIBRARIES}")
+
+    set(CURL_FOUND true CACHE BOOL "Found cURL" FORCE)
+    mark_as_advanced(CURL_FOUND CURL_INCLUDE_DIR CURL_LIBRARIES)
+endif (NOT DEFINED CURL_FOUND)
