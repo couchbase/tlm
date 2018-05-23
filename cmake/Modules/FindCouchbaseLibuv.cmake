81b642756f151e07ee9e6ebcec7df1da5370433c
Trond Norbye <trond.norbye@gmail.com>
2018-05-23 10:53:05 +0200

    MB-29799: Refactor FindCouchbaseLibuv
    * Bypass the entire file if it is already included
    * On supported platforms we should only pick up the header
      and library from cbdeps and not search other locations
    * Follow the cmake style and use lowercase letters
    
    In addition it is being included in the toplevel
    CMakeFile.txt for EE builds to avoid it being
    run for each of the subprojects in eventing
    which resulted in the following to be reported:
    
    -- Found libuv headers in: /Users/trond/compile/trunk/cmake-build-debug/tlm/deps/libuv.exploded/include
    --              libraries: /Users/trond/compile/trunk/install/lib/libuv.dylib
    -- Found OpenSSL headers in /Users/trond/compile/trunk/cmake-build-debug/tlm/deps/openssl.exploded/include
    -- Using OpenSSL libraries: /Users/trond/compile/trunk/install/lib/libssl.dylib;/Users/trond/compile/trunk/install/lib/libcrypto.dylib;/Users/trond/compile/trunk/install/lib/libssl.dylib;/Users/trond/compile/trunk/install/lib/libcrypto.dylib
    -- Found libuv headers in: /Users/trond/compile/trunk/cmake-build-debug/tlm/deps/libuv.exploded/include
    --              libraries: /Users/trond/compile/trunk/install/lib/libuv.dylib
    
    Change-Id: Ide053aa3b1de93b36577063aaa48c93e776c2405
    Reviewed-on: http://review.couchbase.org/94571
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbaseLibuv.cmake
+++ cmake/Modules/FindCouchbaseLibuv.cmake
@@ -1,24 +1,53 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate libuv library
 # This module defines
-#  LIBUV_FOUND, if false, do not try to link with libuv
+#  LIBUV_FOUND, if libuv was found
 #  LIBUV_LIBRARIES, Library path and libs
-#  LIBUV_INCLUDE_DIR, where to find the ICU headers
-SET(_libuv_exploded ${CMAKE_BINARY_DIR}/tlm/deps/libuv.exploded)
+#  LIBUV_INCLUDE_DIR, where to find the libuv headers
+if (NOT DEFINED LIBUV_FOUND)
+    include(PlatformIntrospection)
 
-FIND_PATH(LIBUV_INCLUDE_DIR uv.h
-          HINTS "${_libuv_exploded}"
-          ENV LIBUV_DIR
-          PATH_SUFFIXES include)
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform)
+        # Supported platforms should only use the provided hints and pick it up
+        # from cbdeps
+        set(_libuv_no_default_path NO_DEFAULT_PATH)
+    endif ()
 
-FIND_LIBRARY(LIBUV_LIBRARIES
-          NAMES uv libuv
-          ENV LIBUV_DIR
-          PATH_SUFFIXES lib)
+    set(_libuv_exploded ${CMAKE_BINARY_DIR}/tlm/deps/libuv.exploded)
+    find_path(LIBUV_INCLUDE_DIR uv.h
+              HINTS ${_libuv_exploded}/include
+              ${_libuv_no_default_path})
 
-IF (LIBUV_LIBRARIES)
-  MESSAGE(STATUS "Found libuv in ${LIBUV_INCLUDE_DIR} : ${LIBUV_LIBRARIES}")
-ELSE (LIBUV_LIBRARIES)
-  MESSAGE(FATAL_ERROR "Can't build Couchbase without libuv")
-ENDIF (LIBUV_LIBRARIES)
+    if (NOT LIBUV_INCLUDE_DIR)
+        message(FATAL_ERROR "Failed to locate uv.h")
+    endif ()
 
-MARK_AS_ADVANCED(LIBUV_INCLUDE_DIR LIBUV_LIBRARIES)
+    find_library(LIBUV_LIBRARIES
+                 NAMES uv libuv
+                 HINTS ${CMAKE_INSTALL_PREFIX}/lib
+                 ${_libuv_no_default_path})
+    if (NOT LIBUV_LIBRARIES)
+        message(FATAL_ERROR "Failed to locate libuv")
+    endif ()
+
+    message(STATUS "Found libuv headers in: ${LIBUV_INCLUDE_DIR}")
+    message(STATUS "             libraries: ${LIBUV_LIBRARIES}")
+
+    set(LIBUV_FOUND true CACHE BOOL "Found libuv" FORCE)
+    mark_as_advanced(LIBUV_FOUND LIBUV_INCLUDE_DIR LIBUV_LIBRARIES)
+endif (NOT DEFINED LIBUV_FOUND)
