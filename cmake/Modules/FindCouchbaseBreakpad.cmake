ed033aeb1668aec2c555827876a350af8e0eeb06
Trond Norbye <trond.norbye@gmail.com>
2018-05-23 11:28:30 +0200

    MB-29799: Refactor FindCouchbaseBreakpad
    * Bypass the entire file if it is already included
    * On supported platforms we should only pick up the header
      and library from cbdeps and not search other locations
    * Follow the cmake style and use lowercase letters
    
    Change-Id: I4482b404fedca4037bd11ceff3a897f3fe23e4d9
    Reviewed-on: http://review.couchbase.org/94574
    Reviewed-by: Tim Bradgate <tim.bradgate@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbaseBreakpad.cmake
+++ cmake/Modules/FindCouchbaseBreakpad.cmake
@@ -1,72 +1,91 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate breakpad library
 # This module defines
 #  BREAKPAD_FOUND, if false, do not try to link with breakpad
 #  BREAKPAD_LIBRARIES, Library path and libs
-#  BREAKPAD_INCLUDE_DIR, where to find the ICU headers
+#  BREAKPAD_INCLUDE_DIR, where to find the breakpad headers
 #  MINIDUMP2CORE, program to generate a corefile from the minidump (UNIX only)
 
-INCLUDE(PlatformIntrospection)
+if (NOT DEFINED BREAKPAD_FOUND)
+    include(PlatformIntrospection)
 
-CB_GET_SUPPORTED_PLATFORM (_supported_platform)
-IF (_supported_platform AND NOT APPLE)
-  STRING(TOLOWER ${CMAKE_SYSTEM_NAME} LCASE_SYSTEM)
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform AND NOT APPLE)
+        string(TOLOWER ${CMAKE_SYSTEM_NAME} LCASE_SYSTEM)
 
-  SET(_breakpad_exploded ${CMAKE_BINARY_DIR}/tlm/deps/breakpad.exploded)
+        set(_breakpad_exploded ${CMAKE_BINARY_DIR}/tlm/deps/breakpad.exploded)
 
-  FIND_PATH(BREAKPAD_INCLUDE_DIR client/${LCASE_SYSTEM}/handler/exception_handler.h
-            HINTS ${_breakpad_exploded}/include
-            PATH_SUFFIXES breakpad)
-  IF (WIN32)
+        find_path(BREAKPAD_INCLUDE_DIR client/${LCASE_SYSTEM}/handler/exception_handler.h
+                  HINTS ${_breakpad_exploded}/include
+                  PATH_SUFFIXES breakpad)
 
-    # RelWithDebInfo & MinSizeRel should use the Release libraries, otherwise use
-    # the same directory as the build type.
-    IF (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
-      SET(_build_type "Release")
-    ELSE (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
-      SET(_build_type ${CMAKE_BUILD_TYPE})
-    ENDIF (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+        if (WIN32)
+            # RelWithDebInfo & MinSizeRel should use the Release libraries, otherwise use
+            # the same directory as the build type.
+            if (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+                set(_build_type "Release")
+            else (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+                set(_build_type ${CMAKE_BUILD_TYPE})
+            endif (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
 
-    FIND_LIBRARY(BREAKPAD_EXCEPTION_HANDLER_LIBRARY
-                 NAMES exception_handler
-                 PATHS ${_breakpad_exploded}/lib/${_build_type})
+            find_library(BREAKPAD_EXCEPTION_HANDLER_LIBRARY
+                         NAMES exception_handler
+                         PATHS ${_breakpad_exploded}/lib/${_build_type})
 
-    FIND_LIBRARY(BREAKPAD_CRASH_GENERATION_LIBRARY
-                 NAMES crash_generation_client
-                 PATHS ${_breakpad_exploded}/lib/${_build_type})
+            find_library(BREAKPAD_CRASH_GENERATION_LIBRARY
+                         NAMES crash_generation_client
+                         PATHS ${_breakpad_exploded}/lib/${_build_type})
 
-    FIND_LIBRARY(BREAKPAD_COMMON_LIBRARY
-                 NAMES common
-                 PATHS ${_breakpad_exploded}/lib/${_build_type})
+            find_library(BREAKPAD_COMMON_LIBRARY
+                         NAMES common
+                         PATHS ${_breakpad_exploded}/lib/${_build_type})
 
-    SET(BREAKPAD_LIBRARIES
-        ${BREAKPAD_EXCEPTION_HANDLER_LIBRARY}
-        ${BREAKPAD_CRASH_GENERATION_LIBRARY}
-        ${BREAKPAD_COMMON_LIBRARY})
+            set(BREAKPAD_LIBRARIES
+                ${BREAKPAD_EXCEPTION_HANDLER_LIBRARY}
+                ${BREAKPAD_CRASH_GENERATION_LIBRARY}
+                ${BREAKPAD_COMMON_LIBRARY})
 
-    # not used, just set to simplify the test below
-    SET(MINIDUMP2CORE true)
+            # not used, just set to simplify the test below
+            set(MINIDUMP2CORE true)
+        else (WIN32)
+            find_library(BREAKPAD_LIBRARIES
+                         NAMES breakpad_client
+                         HINTS ${_breakpad_exploded}/lib)
 
-  ELSE (WIN32)
-    FIND_LIBRARY(BREAKPAD_LIBRARIES
-                 NAMES breakpad_client
-                 HINTS ${_breakpad_exploded}/lib)
+            find_program(MINIDUMP2CORE minidump-2-core HINTS ${CMAKE_INSTALL_PREFIX}/bin)
+            if (MINIDUMP2CORE)
+                message(STATUS "Found minidump-2-core: ${MINIDUMP2CORE}")
+            endif (MINIDUMP2CORE)
+        endif (WIN32)
 
-    FIND_PROGRAM(MINIDUMP2CORE minidump-2-core HINTS ${_breakpad_exploded}/bin)
-    IF (MINIDUMP2CORE)
-      MESSAGE(STATUS "Found minidump-2-core: ${MINIDUMP2CORE}")
-    ENDIF (MINIDUMP2CORE)
-  ENDIF (WIN32)
+        if (BREAKPAD_LIBRARIES AND BREAKPAD_INCLUDE_DIR AND MINIDUMP2CORE)
+            set(BREAKPAD_FOUND True CACHE BOOL "Whether Google Breakpad has been found" FORCE)
+            message(STATUS "Found Google Breakpad:")
+            message(STATUS "   headers: ${BREAKPAD_INCLUDE_DIR}")
+            message(STATUS "   library: ${BREAKPAD_LIBRARIES}")
+        else (BREAKPAD_LIBRARIES AND BREAKPAD_INCLUDE_DIR AND MINIDUMP2CORE)
+            message(FATAL_ERROR "Google Breakpad not found (required on supported production platform '${_supported_platform}').")
+        endif (BREAKPAD_LIBRARIES AND BREAKPAD_INCLUDE_DIR AND MINIDUMP2CORE)
 
-  IF (BREAKPAD_LIBRARIES AND BREAKPAD_INCLUDE_DIR AND MINIDUMP2CORE)
-      SET(BREAKPAD_FOUND True CACHE BOOL "Whether Google Breakpad has been found" FORCE)
-      MESSAGE(STATUS "Found Google Breakpad:")
-      MESSAGE(STATUS "   headers: ${BREAKPAD_INCLUDE_DIR}")
-      MESSAGE(STATUS "   library: ${BREAKPAD_LIBRARIES}")
-  ELSE (BREAKPAD_LIBRARIES AND BREAKPAD_INCLUDE_DIR AND MINIDUMP2CORE)
-      MESSAGE(FATAL_ERROR "Google Breakpad not found (required on supported production platform '${_supported_platform}').")
-  ENDIF (BREAKPAD_LIBRARIES AND BREAKPAD_INCLUDE_DIR AND MINIDUMP2CORE)
-ELSE()
-    # Unsupported platforms
-    MESSAGE(STATUS "Google Breakpad is only used on supported platforms")
-    SET(BREAKPAD_FOUND False CACHE BOOL "Whether Google Breakpad has been found" FORCE)
-ENDIF ()
+        mark_as_advanced(BREAKPAD_FOUND BREAKPAD_LIBRARIES BREAKPAD_INCLUDE_DIR MINIDUMP2CORE)
+    else ()
+        # Unsupported platforms
+        message(STATUS "Google Breakpad is only used on supported platforms")
+        set(BREAKPAD_FOUND False CACHE BOOL "Whether Google Breakpad has been found" FORCE)
+        mark_as_advanced(BREAKPAD_FOUND)
+    endif ()
+endif (NOT DEFINED BREAKPAD_FOUND)
