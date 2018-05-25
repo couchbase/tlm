256bf3873a84605794827b023fa9165f54458f84
Trond Norbye <trond.norbye@gmail.com>
2018-05-25 08:30:37 +0200

    MB-29799: Refactor FindCouchbaseNlohmannJson
    * Bypass the entire file if it is already included
    * Follow the cmake style and use lowercase letters
    
    Change-Id: Id5320eb9f633f0c9175c0e3e2d3fff8c5201ace8
    Reviewed-on: http://review.couchbase.org/94745
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- cmake/Modules/FindCouchbaseNlohmannJson.cmake
+++ cmake/Modules/FindCouchbaseNlohmannJson.cmake
@@ -1,18 +1,38 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate nlohmann JSON library
 # This module defines
 #
+#  NLOHMANN_JSON_FOUND
 #  NLOHMANN_JSON_INCLUDE_DIR
 
-SET(_nhlomann_json_exploded ${CMAKE_BINARY_DIR}/tlm/deps/json.exploded)
+if (NOT DEFINED NLOHMANN_JSON_FOUND)
+    set(_nhlomann_json_exploded ${CMAKE_BINARY_DIR}/tlm/deps/json.exploded)
+    if (EXISTS ${_nhlomann_json_exploded} AND IS_DIRECTORY ${_nhlomann_json_exploded})
+        set(_nhlomann_json_no_default_path NO_DEFAULT_PATH)
+    endif ()
+    find_path(NLOHMANN_JSON_INCLUDE_DIR nlohmann/json.hpp
+              HINTS ${_nhlomann_json_exploded}/include
+              ${_nhlomann_json_no_default_path})
 
-FIND_PATH(NLOHMANN_JSON_INCLUDE_DIR nlohmann/json.hpp
-          HINTS
-               ${_nhlomann_json_exploded}
-               ENV NLOHMANN_JSON_DIR
-          PATH_SUFFIXES include)
+    if (NOT NLOHMANN_JSON_INCLUDE_DIR)
+        message(FATAL_ERROR "Failed to locate nlohmann/json.hpp")
+    endif (NOT NLOHMANN_JSON_INCLUDE_DIR)
 
-IF (NOT NLOHMANN_JSON_INCLUDE_DIR)
-  MESSAGE(ERROR "Can't build Couchbase without https://github.com/nlohmann/json")
-ENDIF (NOT NLOHMANN_JSON_INCLUDE_DIR)
-
-MARK_AS_ADVANCED(NLOHMANN_JSON_INCLUDE_DIR)
+    message(STATUS "Found nlohmann json in: ${NLOHMANN_JSON_INCLUDE_DIR}")
+    set(NLOHMANN_JSON_FOUND true CACHE BOOL "Found nlohmann json" FORCE)
+    mark_as_advanced(NLOHMANN_JSON_FOUND NLOHMANN_JSON_INCLUDE_DIR)
+endif (NOT DEFINED NLOHMANN_JSON_FOUND)
