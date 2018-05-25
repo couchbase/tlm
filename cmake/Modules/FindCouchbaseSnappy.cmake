4b1fc6f0e559f273f7eef0b3b63923ee6379876c
Trond Norbye <trond.norbye@gmail.com>
2018-05-25 08:49:43 +0200

    MB-29799: Refactor FindCouchbaseSnappy
    * Bypass the entire file if it is already included
    * On supported platforms we should only pick up the header
      and library from cbdeps and not search other locations
    * Follow the cmake style and use lowercase letters
    
    Change-Id: Ia30701a390a827fc6f7ca9b6b53133100a702ce6
    Reviewed-on: http://review.couchbase.org/94748
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- cmake/Modules/FindCouchbaseSnappy.cmake
+++ cmake/Modules/FindCouchbaseSnappy.cmake
@@ -1,41 +1,56 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate snappy library
 # This module defines
 #  SNAPPY_FOUND, if false, do not try to link with snappy
 #  SNAPPY_LIBRARIES, Library path and libs
 #  SNAPPY_INCLUDE_DIR, where to find the ICU headers
-SET(_snappy_exploded ${CMAKE_BINARY_DIR}/tlm/deps/snappy.exploded)
 
-FIND_PATH(SNAPPY_INCLUDE_DIR snappy.h
-          HINTS
-               "${_snappy_exploded}"
-               ENV SNAPPY_DIR
-          PATH_SUFFIXES include
-          PATHS
-               ~/Library/Frameworks
-               /Library/Frameworks
-               /opt/local
-               /opt/csw
-               /opt/snappy
-               /opt)
+if (NOT DEFINED SNAPPY_FOUND)
+    include(PlatformIntrospection)
 
-FIND_LIBRARY(SNAPPY_LIBRARIES
-             NAMES snappy
-             HINTS
-                 "${_snappy_exploded}"
-                 ENV SNAPPY_DIR
-             PATH_SUFFIXES lib
-             PATHS
-                 ~/Library/Frameworks
-                 /Library/Frameworks
-                 /opt/local
-                 /opt/csw
-                 /opt/snappy
-                 /opt)
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform)
+        # Supported platforms should only use the provided hints and pick it up
+        # from cbdeps
+        set(_snappy_no_default_path NO_DEFAULT_PATH)
+    endif ()
 
-IF (SNAPPY_LIBRARIES)
-  MESSAGE(STATUS "Found snappy in ${SNAPPY_INCLUDE_DIR} : ${SNAPPY_LIBRARIES}")
-ELSE (SNAPPY_LIBRARIES)
-  MESSAGE(FATAL_ERROR "Can't build Couchbase without Snappy")
-ENDIF (SNAPPY_LIBRARIES)
+    set(_snappy_exploded ${CMAKE_BINARY_DIR}/tlm/deps/snappy.exploded)
 
-MARK_AS_ADVANCED(SNAPPY_INCLUDE_DIR SNAPPY_LIBRARIES)
+    find_path(SNAPPY_INCLUDE_DIR snappy.h
+              HINTS ${_snappy_exploded}/include
+              ${_snappy_no_default_path})
+
+    if (NOT SNAPPY_INCLUDE_DIR)
+        message(FATAL_ERROR "Failed to locate snappy.h")
+    endif ()
+
+    find_library(SNAPPY_LIBRARIES
+                 NAMES snappy
+                 HINTS
+                 ${_snappy_exploded}/lib
+                 ${_snappy_no_default_path})
+
+    if (NOT SNAPPY_LIBRARIES)
+        message(FATAL_ERROR "Failed to locate snappy library")
+    endif ()
+
+    message(STATUS "Found snappy headers in: ${SNAPPY_INCLUDE_DIR}")
+    message(STATUS "              libraries: ${SNAPPY_LIBRARIES}")
+    set(SNAPPY_FOUND true CACHE BOOL "Found Google Snappy" FORCE)
+    mark_as_advanced(SNAPPY_FOUND SNAPPY_INCLUDE_DIR SNAPPY_LIBRARIES)
+endif (NOT DEFINED SNAPPY_FOUND)
