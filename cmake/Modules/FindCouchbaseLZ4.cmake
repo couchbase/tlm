a7dae9cdef77a943a4987a1cffd7b5820d494906
Trond Norbye <trond.norbye@gmail.com>
2018-05-25 08:21:21 +0200

    MB-29799: Refactor FindCouchbaseLZ4
    Bypass the entire file if it is already included
    
    Change-Id: I0ccb2dd7f77493e8b4897458b8260329339d35ca
    Reviewed-on: http://review.couchbase.org/94743
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbaseLZ4.cmake
+++ cmake/Modules/FindCouchbaseLZ4.cmake
@@ -1,37 +1,54 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate lz4 library
 # This module defines
 #  LZ4_FOUND, if false, do not try to link with lz4
 #  LZ4_LIBRARIES, Library path and libs
 #  LZ4_INCLUDE_DIR, where to find the ICU headers
-set(_lz4_exploded ${CMAKE_BINARY_DIR}/tlm/deps/lz4.exploded)
-set(_lz4_library_dir ${CMAKE_INSTALL_PREFIX})
 
-include(PlatformIntrospection)
+if (NOT DEFINED LZ4_FOUND)
+    set(_lz4_exploded ${CMAKE_BINARY_DIR}/tlm/deps/lz4.exploded)
+    set(_lz4_library_dir ${CMAKE_INSTALL_PREFIX})
 
-cb_get_supported_platform(_supported_platform)
-if (_supported_platform)
-  # Supported platforms should only use the provided hints and pick up
-  # LZ4 from cbdeps
-  set(NO_DEFAULT_PATH NO_DEFAULT_PATH)
-endif ()
+    include(PlatformIntrospection)
 
-find_path(LZ4_INCLUDE_DIR lz4.h
-          HINTS ${_lz4_exploded}/include
-          ${NO_DEFAULT_PATH})
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform)
+        # Supported platforms should only use the provided hints and pick up
+        # LZ4 from cbdeps
+        set(_lz4_no_default_path NO_DEFAULT_PATH)
+    endif ()
 
-find_library(LZ4_LIBRARIES
-             NAMES lz4
-             HINTS ${_lz4_library_dir}/lib
-             ${NO_DEFAULT_PATH})
+    find_path(LZ4_INCLUDE_DIR lz4.h
+              HINTS ${_lz4_exploded}/include
+              ${_lz4_no_default_path})
 
-if (LZ4_INCLUDE_DIR AND LZ4_LIBRARIES)
-  set(LZ4_FOUND True CACHE BOOL "Whether LZ4 has been found")
-  message(STATUS "Found LZ4:")
-  message(STATUS "   headers: ${LZ4_INCLUDE_DIR}")
-  message(STATUS "   library: ${LZ4_LIBRARIES}")
-else()
-  message(WARNING "LZ4 not found")
-  set(LZ4_FOUND False CACHE BOOL "Whether LZ4 has been found")
-endif()
+    find_library(LZ4_LIBRARIES
+                 NAMES lz4
+                 HINTS ${_lz4_library_dir}/lib
+                 ${_lz4_no_default_path})
 
-mark_as_advanced(LZ4_FOUND LZ4_INCLUDE_DIR LZ4_LIBRARIES)
+    if (LZ4_INCLUDE_DIR AND LZ4_LIBRARIES)
+        set(LZ4_FOUND True CACHE BOOL "Whether LZ4 has been found" FORCE)
+        message(STATUS "Found LZ4 headers in: ${LZ4_INCLUDE_DIR}")
+        message(STATUS "           libraries: ${LZ4_LIBRARIES}")
+    else ()
+        message(WARNING "LZ4 not found")
+        set(LZ4_FOUND False CACHE BOOL "Whether LZ4 has been found" FORCE)
+    endif ()
+
+    mark_as_advanced(LZ4_FOUND LZ4_INCLUDE_DIR LZ4_LIBRARIES)
+endif (NOT DEFINED LZ4_FOUND)
