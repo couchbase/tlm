dfa878ce9300be89630f12fd8c4f557c32e73190
Trond Norbye <trond.norbye@gmail.com>
2018-05-25 08:43:26 +0200

    MB-29799: Refactor FindCouchbaseRocksDB
    * Bypass the entire file if it is already included
    * On supported platforms we should only pick up the header
      and library from cbdeps and not search other locations
    * Follow the cmake style and use lowercase letters
    
    Change-Id: I98bcd3496eed4a34d24feff4a27722d8868f5cea
    Reviewed-on: http://review.couchbase.org/94747
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- cmake/Modules/FindCouchbaseRocksDB.cmake
+++ cmake/Modules/FindCouchbaseRocksDB.cmake
@@ -1,23 +1,53 @@
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Locate RocksDB library
 # This module defines
 #  ROCKSDB_FOUND, if false, do not try to link with rocksdb
 #  ROCKSDB_LIBRARIES, Library path and libs
 #  ROCKSDB_INCLUDE_DIR, where to find the rocksdb headers
+if (NOT DEFINED ROCKSDB_FOUND)
+    include(PlatformIntrospection)
 
-SET(_rocksdb_exploded ${CMAKE_BINARY_DIR}/tlm/deps/rocksdb.exploded)
+    cb_get_supported_platform(_supported_platform)
+    if (_supported_platform)
+        # Supported platforms should only use the provided hints and pick it up
+        # from cbdeps
+        set(_rocksdb_no_default_path NO_DEFAULT_PATH)
+    endif ()
 
-FIND_PATH(ROCKSDB_INCLUDE_DIR rocksdb/db.h
-          PATH_SUFFIXES include
-          PATHS ${_rocksdb_exploded}
-          NO_DEFAULT_PATH)
+    set(_rocksdb_exploded ${CMAKE_BINARY_DIR}/tlm/deps/rocksdb.exploded)
 
-FIND_LIBRARY(ROCKSDB_LIBRARIES
-             NAMES rocksdb)
+    find_path(ROCKSDB_INCLUDE_DIR rocksdb/db.h
+              PATH_SUFFIXES include
+              PATHS ${_rocksdb_exploded}
+              ${_rocksdb_no_default_path})
 
-IF (ROCKSDB_INCLUDE_DIR AND ROCKSDB_LIBRARIES)
-  MESSAGE(STATUS "Found RocksDB in ${ROCKSDB_INCLUDE_DIR} : ${ROCKSDB_LIBRARIES}")
-ELSE ()
-  MESSAGE(STATUS "RocksDB not found")
-ENDIF ()
+    find_library(ROCKSDB_LIBRARIES
+                 NAMES rocksdb
+                 HINTS ${CMAKE_INSTALL_PREFIX}/lib
+                 ${_rocksdb_no_default_path})
 
-MARK_AS_ADVANCED(ROCKSDB_INCLUDE_DIR ROCKSDB_LIBRARIES)
+    if (ROCKSDB_INCLUDE_DIR AND ROCKSDB_LIBRARIES)
+        message(STATUS "Found RocksDB headers in: ${ROCKSDB_INCLUDE_DIR}")
+        message(STATUS "               libraries: ${ROCKSDB_LIBRARIES}")
+        set(ROCKSDB_FOUND true CACHE BOOL "Found RocksDB" FORCE)
+    else ()
+        message(STATUS "RocksDB not found")
+        set(ROCKSDB_FOUND false CACHE BOOL "RocksDB not found" FORCE)
+    endif ()
+
+    mark_as_advanced(ROCKSDB_FOUND ROCKSDB_INCLUDE_DIR ROCKSDB_LIBRARIES)
+endif (NOT DEFINED ROCKSDB_FOUND)
