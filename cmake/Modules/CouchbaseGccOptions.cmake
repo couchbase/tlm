dc88c7f487f0ce132c04f9ed5c33666e77efcd4c
Trond Norbye <trond.norbye@gmail.com>
2018-06-12 16:52:37 +0200

    Refactor: Clean up CouchbaseGccOptions
    Change-Id: I2439debf7116a292969f11c5b9f6b1f27708d5d8
    Reviewed-on: http://review.couchbase.org/95493
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Tim Bradgate <tim.bradgate@couchbase.com>

--- cmake/Modules/CouchbaseGccOptions.cmake
+++ cmake/Modules/CouchbaseGccOptions.cmake
@@ -1,84 +1,92 @@
-SET(CB_GNU_DEBUG "-g")
-SET(CB_GNU_WARNINGS "-Wall -pedantic -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls -fno-strict-aliasing -Wno-overlength-strings")
-SET(CB_GNU_VISIBILITY "-fvisibility=hidden")
-SET(CB_GNU_THREAD "-pthread")
-SET(CB_GNU_LANG_VER "-std=gnu99")
+#
+#     Copyright 2018 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# Set the compiler flags for gcc and g++ compilator
+
+# Add common flags for C and C++
+foreach (dir ${CB_SYSTEM_HEADER_DIRS})
+   list(APPEND _cb_c_flags "-isystem ${dir}")
+endforeach (dir ${CB_SYSTEM_HEADER_DIRS})
+
+if (CB_CODE_COVERAGE)
+   list(APPEND _cb_c_flags --coverage)
+   set(CMAKE_LINK_FLAGS "${CMAKE_LINK_FLAGS} --coverage")
+endif ()
+
+list(APPEND _cb_c_flags -fvisibility=hidden -pthread)
+# Copy the flags over to C++
+set(_cb_cxx_flags ${_cb_c_flags})
+
+# Add C specific options
+list(APPEND cb_c_flags
+     -std=gnu99
+     -Wall
+     -pedantic
+     -Werror=missing-prototypes
+     -Werror=missing-declarations
+     -Wredundant-decls
+     -fno-strict-aliasing
+     -Wno-overlength-strings)
+
+# Convert the list to a string
+string(REPLACE ";" " " _cb_c_options "${_cb_c_flags}")
 
 # We want RelWithDebInfo to have the same optimization level as
 # Release, only differing in whether debugging information is enabled.
-SET(CMAKE_C_FLAGS_RELEASE        "-O3 -DNDEBUG")
-SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -DNDEBUG -g")
-SET(CMAKE_C_FLAGS_DEBUG          "-Og -g")
+set(CMAKE_C_FLAGS_RELEASE        "-O3 -DNDEBUG")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -DNDEBUG -g")
+set(CMAKE_C_FLAGS_DEBUG          "-Og -g")
+set(CB_C_FLAGS_NO_OPTIMIZE       "-O0")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_cb_c_options}")
 
-SET(CB_C_FLAGS_NO_OPTIMIZE       -O0)
+# Configure the C++ compiler
+if (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 5.1.0)
+   message (FATAL_ERROR "Couchbase Server requires GCC version 5.1.0 or newer to compile. Aborting build.")
+endif ()
 
-IF ("${ENABLE_WERROR}" STREQUAL "YES")
-   SET(CB_GNU_WERROR "-Werror")
-ENDIF()
+list(APPEND _cb_cxx_flags
+     -std=c++11
+     -pedantic
+     -Wall
+     -Wredundant-decls
+     -Werror=missing-braces
+     -fno-strict-aliasing
+     -Werror=switch)
 
-FOREACH(dir ${CB_SYSTEM_HEADER_DIRS})
-   SET(CB_GNU_IGNORE_HEADER "${CB_GNU_IGNORE_HEADER} -isystem ${dir}")
-ENDFOREACH(dir ${CB_SYSTEM_HEADER_DIRS})
 
-IF (CB_CODE_COVERAGE)
-   SET(CB_GNU_COVERAGE "--coverage")
-   SET(CB_LINK_COVERAGE "--coverage")
-ENDIF ()
+SET(COMPILER_SUPPORTS_CXX11 true)
+SET(CB_GNU_CXX11_OPTION "-std=gnu++11")
 
-SET(CMAKE_C_FLAGS "${CB_GNU_IGNORE_HEADER} ${CMAKE_C_FLAGS} ${CB_GNU_DEBUG} ${CB_GNU_LANG_VER} ${CB_GNU_WARNINGS} ${CB_GNU_VISIBILITY} ${CB_GNU_THREAD} ${CB_GNU_WERROR} ${CB_GNU_COVERAGE}")
-
-SET(CMAKE_EXE_LINKER_FLAGS "${CB_LINK_COVERAGE}")
-SET(CMAKE_SHARED_LINKER_FLAGS "${CB_LINK_COVERAGE}")
-
-SET(CB_GXX_DEBUG "-g")
-SET(CB_GXX_WARNINGS "-Wall -Wredundant-decls -Wmissing-braces -fno-strict-aliasing")
-SET(CB_GXX_VISIBILITY "-fvisibility=hidden")
-SET(CB_GXX_THREAD "-pthread")
-
-IF (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER 6.9.9)
+if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER 6.9.9)
    #Disable aligned-new warning
-   SET(CB_GXX_WARNINGS "${CB_GXX_WARNINGS} -Wno-aligned-new")
-   MESSAGE(STATUS "Disabling aligned-new warning as we don't support C++17 (yet)")
+   list(APPEND _cb_cxx_flags -Wno-aligned-new)
+   message(STATUS "Disabling aligned-new warning as we don't support C++17 (yet)")
 
    # Disable stringop-overflow warnings as there seem to be a fair few bugs in that area (GCC 7.2)
    # There are multiple bugs files regarding false positives.
    # See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=79095 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=83239
-   SET(CB_GXX_WARNINGS "${CB_GXX_WARNINGS} -Wno-stringop-overflow")
-   MESSAGE(STATUS "Disabling stringop-overflow warning as it seems to report false positives at the moment")
-   MESSAGE(STATUS "Current GXX Warnings state: ${CB_GXX_WARNINGS}")
-ENDIF ()
+   list(APPEND _cb_cxx_flags -Wno-stringop-overflow)
+   message(STATUS "Disabling stringop-overflow warning as it seems to report false positives at the moment")
+endif ()
+# Convert the list to a string
+string(REPLACE ";" " " _cb_cxx_options "${_cb_cxx_flags}")
 
 # We want RelWithDebInfo to have the same optimization level as
 # Release, only differing in whether debugging information is enabled.
-SET(CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG")
-SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -DNDEBUG -g")
-SET(CMAKE_CXX_FLAGS_DEBUG          "-Og -g")
-
-SET(CB_CXX_FLAGS_NO_OPTIMIZE       -O0)
-
-IF ("${ENABLE_WERROR}" STREQUAL "YES")
-   SET(CB_GXX_WERROR "-Werror")
-ELSE()
-   # We've fixed all occurrences for the following warnings and don't want
-   # any new ones to appear
-   SET(CB_GXX_WERROR "-Werror=switch")
-ENDIF()
-
-IF (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 5.1.0)
-   MESSAGE (FATAL_ERROR "\n\nCouchbase Server requires GCC version 5.1.0 or newer to compile. Aborting build.\n\n")
-ENDIF ()
-
-SET(COMPILER_SUPPORTS_CXX11 true)
-SET(CB_CXX_LANG_VER "-std=c++11")
-SET(CB_GNU_CXX11_OPTION "-std=gnu++11")
-SET(CB_CXX_PEDANTIC "-pedantic")
-
-FOREACH(dir ${CB_SYSTEM_HEADER_DIRS})
-   SET(CB_GNU_CXX_IGNORE_HEADER "${CB_GNU_CXX_IGNORE_HEADER} -isystem ${dir}")
-ENDFOREACH(dir ${CB_SYSTEM_HEADER_DIRS})
-
-IF (CB_CODE_COVERAGE)
-   SET(CB_GXX_COVERAGE "--coverage")
-ENDIF ()
-
-SET(CMAKE_CXX_FLAGS "${CB_GNU_CXX_IGNORE_HEADER} ${CMAKE_CXX_FLAGS} ${CB_CXX_LANG_VER} ${CB_GXX_DEBUG} ${CB_GXX_WARNINGS} ${CB_CXX_PEDANTIC} ${CB_GXX_VISIBILITY} ${CB_GXX_THREAD} ${CB_GXX_WERROR} ${CB_GXX_COVERAGE}")
+set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -DNDEBUG -g")
+set(CMAKE_CXX_FLAGS_DEBUG          "-Og -g")
+set(CB_CXX_FLAGS_NO_OPTIMIZE       "-O0")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_cb_cxx_options}")
