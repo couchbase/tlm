e21739f8789e680f25756c4385cc0772bd6fc3e7
Trond Norbye <trond.norbye@gmail.com>
2018-06-13 17:32:09 +0200

    Add support for MSVC2017
    One of the source files in forestdb currently fails with MSVC2017
    due to changes in offsetof. Add an option to get the old behavior
    until it is resolved in forestdb.
    
    Add support for ignoring warnings in system headers
    
    Change-Id: I982981d976627baf856bc07951646c6b13ecf69b
    Reviewed-on: http://review.couchbase.org/95556
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/CouchbaseMsvcOptions.cmake
+++ cmake/Modules/CouchbaseMsvcOptions.cmake
@@ -9,12 +9,15 @@ set(CB_C_FLAGS_NO_OPTIMIZE       "/Od /Ob0")
 # provided on Linux/Unix to avoid having to deal with #ifdef's
 include_directories(AFTER ${CMAKE_SOURCE_DIR}/platform/include/win32)
 
-# We want RelWithDebInfo to have the same optimization level as
-# Release, only differing in whether debugging information is enabled.
-set(CMAKE_CXX_FLAGS_RELEASE        "/MD /O2 /Ob2 /D NDEBUG")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MD /O2 /Ob2 /D NDEBUG /Zi")
-set(CMAKE_CXX_FLAGS_DEBUG          "/MDd /Od /Ob0 /Zi")
-set(CB_CXX_FLAGS_NO_OPTIMIZE       "/Od /Ob0")
+if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER 19.10)
+
+    foreach (dir ${CB_SYSTEM_HEADER_DIRS})
+        list(APPEND _cb_cxx_flags "/external:I ${dir}")
+    endforeach (dir ${CB_SYSTEM_HEADER_DIRS})
+
+    # forestdb hash needs this in MSVC 2017 (should be fixed there eventually)
+    list(APPEND _cb_cxx_flags "/Zc:offsetof-")
+endif()
 
 # Our code emits tons of warnings due to missing declspec dllimport/export
 # for standard types (std::vector, unordered_map etc).
@@ -24,9 +27,15 @@ set(CB_CXX_FLAGS_NO_OPTIMIZE       "/Od /Ob0")
 #          'identifier' : class 'type' needs to have dll-interface
 #          to be used by clients of class 'type2'
 #
+list(APPEND _cb_cxx_flags "/wd4251")
+
+
 #   4275 - https://msdn.microsoft.com/en-us/library/3tdb471s.aspx
 #         non – DLL-interface classkey 'identifier' used as base for
 #         DLL-interface classkey 'identifier'
+
+list(APPEND _cb_cxx_flags "/wd4275")
+
 #
 # Our code has a lot of align-new warnings generated, but we can't really
 # make use of these warnings until we have C++17 support, so for now we
@@ -34,6 +43,9 @@ set(CB_CXX_FLAGS_NO_OPTIMIZE       "/Od /Ob0")
 #
 #   4316 - https://msdn.microsoft.com/en-us/library/dn448573.aspx
 #          object allocated on the heap may not be aligned 128
+
+list(APPEND _cb_cxx_flags "/wd4316")
+
 #
 # We have a couple of C4800: 'type' : forcing value to bool 'true' or
 # 'false' (performance warning) warnings
@@ -42,4 +54,14 @@ set(CB_CXX_FLAGS_NO_OPTIMIZE       "/Od /Ob0")
 #   4800 - https://msdn.microsoft.com/en-us/library/b6801kcy.aspx
 #          'type' : forcing value to bool 'true' or 'false'
 #	   (performance warning) warnings
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4251 /wd4275 /wd4316 /wd4800")
+list(APPEND _cb_cxx_flags "/wd4800")
+# Convert the list to a string
+string(REPLACE ";" " " _cb_cxx_options "${_cb_cxx_flags}")
+
+# We want RelWithDebInfo to have the same optimization level as
+# Release, only differing in whether debugging information is enabled.
+set(CMAKE_CXX_FLAGS_RELEASE        "/MD /O2 /Ob2 /D NDEBUG")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MD /O2 /Ob2 /D NDEBUG /Zi")
+set(CMAKE_CXX_FLAGS_DEBUG          "/MDd /Od /Ob0 /Zi")
+set(CB_CXX_FLAGS_NO_OPTIMIZE       "/Od /Ob0")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_cb_cxx_options}")
