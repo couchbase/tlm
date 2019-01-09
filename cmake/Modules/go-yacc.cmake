8625abafc3a6907dd43cce621939b4a09335ceed
Chris Hillery <ceej@couchbase.com>
2019-01-08 16:42:26 -0800

    MB-32527: Go fixes for MacOS Mojave
    1. Remove use of "go tool yacc"; replace with building "goyacc" tool
       (added to manifest)
    2. Bump minimum version of Go used on MacOS builds to 1.11
    
    Change-Id: Ibdc193da194dcab9f4d1755b139415e2c9d040c5
    Reviewed-on: http://review.couchbase.org/103422
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cmake/Modules/go-yacc.cmake
+++ cmake/Modules/go-yacc.cmake
@@ -1,11 +1,10 @@
 # Set GOROOT environment
 SET (ENV{GOROOT} "${GOROOT}")
-SET (GO_EXECUTABLE "${GOROOT}/bin/go")
 
 # Execute "go yacc"
-MESSAGE (STATUS "Executing: ${GO_EXECUTABLE} tool yacc ${YFILE}")
+MESSAGE (STATUS "Executing: ${GOYACC_EXECUTABLE} ${YFILE}")
 EXECUTE_PROCESS (RESULT_VARIABLE _failure
-  COMMAND "${GO_EXECUTABLE}" tool yacc "${YFILE}")
+  COMMAND "${GOYACC_EXECUTABLE}" "${YFILE}")
 IF (_failure)
   MESSAGE (FATAL_ERROR "Failed running go yacc")
 ENDIF (_failure)
