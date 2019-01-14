087ab77fcbf7c6dbc59bde88664f8f69a3d3d83a
Kenneth Lareau <kenneth.lareau@couchbase.com>
2019-01-14 14:30:33 -0800

    MB-32625: Set rpath for dependent libraries for gRPC libraries
    Change-Id: If43f89e78bab6370d1ecb813523f7c4fd6fd082c
    Reviewed-on: http://review.couchbase.org/103601
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/packages/grpc/grpc_unix.sh
+++ deps/packages/grpc/grpc_unix.sh
@@ -12,9 +12,15 @@ cp -a /tmp/install/* ${INSTALL_DIR}
 cd
 rm -rf /tmp/install
 
-# For MacOS, tweak install_name
+# For MacOS, tweak install_name, along with dependent libraries
 if [ $(uname -s) = "Darwin" ]; then
     for grpc_lib in libgrpc_cronet.dylib libaddress_sorting.dylib libgrpc++_error_details.dylib libgrpc.dylib libgrpc++.dylib libgrpc_unsecure.dylib libgpr.dylib libgrpc++_cronet.dylib libgrpc++_unsecure.dylib libgrpc++_reflection.dylib libgrpcpp_channelz.dylib; do
-        install_name_tool -id @rpath/${grpc_lib} ${INSTALL_DIR}/lib/${grpc_lib}
+        grpc_lib_full_path=${INSTALL_DIR}/lib/${grpc_lib}
+        install_name_tool -id @rpath/${grpc_lib} ${grpc_lib_full_path}
+
+        # Ensure any gRPC dependent libraries are corrected as well
+        for dep_lib in $(otool -L ${grpc_lib_full_path} | awk 'NR > 3 {print $1}' | grep '^lib'); do
+            install_name_tool -change ${dep_lib} @rpath/${dep_lib} ${grpc_lib_full_path}
+        done
     done
 fi
