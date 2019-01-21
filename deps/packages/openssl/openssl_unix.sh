e67efc26eb34263dac6505824a880e075de6f14d
Trond Norbye <trond.norbye@gmail.com>
2019-01-21 17:56:01 +0100

    MB-32160: Build OpenSSL on all platforms
    In order to support TLS 1.3 in KV we need to bundle
    OpenSSL on all platforms
    
    Change-Id: Iea62d60451efb3a1597d1cd5a33feb5b61acb19e
    Reviewed-on: http://review.couchbase.org/103849
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/packages/openssl/openssl_unix.sh
+++ deps/packages/openssl/openssl_unix.sh
@@ -1,43 +1,75 @@
 #!/bin/bash
+set -e
 
 INSTALL_DIR=$1
 PLATFORM=$2
 VERSION=$3
 
-# NOTE: This script currently ONLY works on macOS and will need modifications
-#       for any form of Linux/Unix
+OS=`uname -s`
+case "$OS" in
+    Darwin)
+        target=darwin64-x86_64-cc
+        ;;
+    Linux)
+        target=linux-x86_64
+        extra_flags=-Wl,--enable-new-dtags,-rpath,'$(ORIGIN)/../lib/'
+        ;;
+    *)
+        echo "Unknown platform"
+        exit 1
+        ;;
+esac
 
-./Configure darwin64-x86_64-cc shared enable-ec_nistp_64_gcc_128 \
-  no-comp no-ssl2 no-ssl3 --openssldir=${INSTALL_DIR} || exit 1
-make depend || exit 1
+./Configure ${target} \
+            enable-ec_nistp_64_gcc_128 \
+            shared \
+            threads \
+            no-tests \
+            no-comp \
+            no-ssl \
+            no-ssl2 \
+            no-ssl3 \
+            --prefix=${INSTALL_DIR} \
+            --openssldir=${INSTALL_DIR} \
+            ${extra_flags}
+
 # Note - don't use "make -j" as OpenSSL's target dependencies are messed up.
 # There's a race which causes frequent build failures.
-make  && make install || exit 1
+make && make install
 
-rm ${INSTALL_DIR}/lib/libcrypto.a ${INSTALL_DIR}/lib/libssl.a
+if [ "$OS" == "Darwin" ]
+then
+    # NOTE: The below loop actually doesn't do anything; the libraries under
+    #       lib/engines are actually bundles on macOS and not shared libraries,
+    #       so the install_name_tool is a no-op.  Setting the install name
+    #       MAY be doable via compile time options, but this would require
+    #       further research.  Leaving the code here with a note for possible
+    #       future exploration into the issue.
+    for lib in $(ls ${INSTALL_DIR}/lib/engines); do
+        chmod u+w ${INSTALL_DIR}/lib/engines/${lib}
+        install_name_tool -id @rpath/${lib} ${INSTALL_DIR}/lib/engines/${lib}
+        chmod u-w ${INSTALL_DIR}/lib/engines/${lib}
+    done
 
-# NOTE: The below loop actually doesn't do anything; the libraries under
-#       lib/engines are actually bundles on macOS and not shared libraries,
-#       so the install_name_tool is a no-op.  Setting the install name
-#       MAY be doable via compile time options, but this would require
-#       further research.  Leaving the code here with a note for possible
-#       future exploration into the issue.
-for lib in $(ls ${INSTALL_DIR}/lib/engines); do
-    chmod u+w ${INSTALL_DIR}/lib/engines/${lib}
-    install_name_tool -id @rpath/${lib} ${INSTALL_DIR}/lib/engines/${lib}
-    chmod u-w ${INSTALL_DIR}/lib/engines/${lib}
-done
+    chmod u+w ${INSTALL_DIR}/lib/libssl.1.1.dylib \
+              ${INSTALL_DIR}/lib/libcrypto.1.1.dylib \
+              ${INSTALL_DIR}/bin/openssl
+    install_name_tool -id @rpath/libssl.1.1.dylib \
+                      ${INSTALL_DIR}/lib/libssl.1.1.dylib
+    install_name_tool -change ${INSTALL_DIR}/lib/libcrypto.1.1.dylib \
+                      @loader_path/libcrypto.1.1.dylib \
+                      ${INSTALL_DIR}/lib/libssl.1.1.dylib
+    install_name_tool -id @rpath/libcrypto.1.1.dylib \
+                      ${INSTALL_DIR}/lib/libcrypto.1.1.dylib
+    install_name_tool -change ${INSTALL_DIR}/lib/libssl.1.1.dylib \
+                      @executable_path/../lib/libssl.1.1.dylib \
+                      -change ${INSTALL_DIR}/lib/libcrypto.1.1.dylib \
+                      @executable_path/../lib/libcrypto.1.1.dylib \
+                      ${INSTALL_DIR}/bin/openssl
+    chmod u-w ${INSTALL_DIR}/lib/libssl.1.1.dylib \
+              ${INSTALL_DIR}/lib/libcrypto.1.1.dylib \
+              ${INSTALL_DIR}/bin/openssl
+fi
 
-chmod u+w ${INSTALL_DIR}/lib/libssl.1.0.0.dylib ${INSTALL_DIR}/lib/libcrypto.1.0.0.dylib ${INSTALL_DIR}/bin/openssl
-install_name_tool -id @rpath/libssl.1.0.0.dylib ${INSTALL_DIR}/lib/libssl.1.0.0.dylib
-install_name_tool -change ${INSTALL_DIR}/lib/libcrypto.1.0.0.dylib @loader_path/libcrypto.1.0.0.dylib ${INSTALL_DIR}/lib/libssl.1.0.0.dylib
-install_name_tool -id @rpath/libcrypto.1.0.0.dylib ${INSTALL_DIR}/lib/libcrypto.1.0.0.dylib
-install_name_tool -change ${INSTALL_DIR}/lib/libssl.1.0.0.dylib @executable_path/../lib/libssl.1.0.0.dylib -change ${INSTALL_DIR}/lib/libcrypto.1.0.0.dylib @executable_path/../lib/libcrypto.1.0.0.dylib ${INSTALL_DIR}/bin/openssl
-chmod u-w ${INSTALL_DIR}/lib/libssl.1.0.0.dylib ${INSTALL_DIR}/lib/libcrypto.1.0.0.dylib ${INSTALL_DIR}/bin/openssl
-
-# We don't want CMake to "find" these libraries in the exploded directory,
-# only headers, so we hide the lib directory. CMakeLists_package.txt will
-# handle copying them to the INSTALL directory where we want CMake to find
-# them.
-mkdir ${INSTALL_DIR}/package
-mv ${INSTALL_DIR}/lib ${INSTALL_DIR}/package
+# We don't want the entire manual set added to the package
+rm -rf ${INSTALL_DIR}/man
