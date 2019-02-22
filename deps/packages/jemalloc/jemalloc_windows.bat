a187603dbb5195ff32bcec779978108090c6c32c
Build Team <build-team@couchbase.com>
2019-02-22 00:29:06 -0800

    MB-30029: jemalloc: Restore Windows build steps for v4.1.0
    Restore the cbdeps build steps for Windows (using the steps prior to
    the 5.0.1 upgrade).
    
    The eventual aim is to upgrade jemalloc for Windows to the same
    version as the other platforms (5.1), this patch is a stepping stone
    to that goal; to add back build steps which work for the
    currently-used version of jemalloc on Windows (4.1.0).
    
    Note this patch creates a -cb3 build - one higher than the current
    windows version of -cb2. It /should/ be the same but given it's been
    rebuilt we will incrementally update to cb3 to ensure build steps are
    correct.
    
    Note(2) this still requires autogen et al from cygwin to build.
    
    Change-Id: I7c81e478264f2cdd026d761aba9f66881afd1496
    Reviewed-on: http://review.couchbase.org/105269
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>

--- deps/packages/jemalloc/jemalloc_windows.bat
+++ deps/packages/jemalloc/jemalloc_windows.bat
@@ -1,4 +1,12 @@
+setlocal
+set PATH=%PATH%;c:\cygwin\bin
+
 set INSTALL_DIR=%1
 
-rem Figure out rest later
-exit /B 1
+sh -c "./autogen.sh CC=cl --prefix=%INSTALL_DIR% --with-jemalloc-prefix=je_ --disable-cache-oblivious --disable-zone-allocator --enable-prof"
+
+make build_lib_shared
+make install_lib_shared install_include install_bin
+
+rem The standard install rules don't install the import library or PDB files - do it manually.
+cp lib\jemalloc.lib lib\jemalloc.pdb %INSTALL_DIR%\lib
