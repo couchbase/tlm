6ad95fd007fd6a4c916678cad7e2044aa0ea5b04
Kim Trang <kim.trang@couchbase.com>
2019-03-04 10:57:59 -0800

    CBD-2896 sync folly's dependencies with Server
    Change-Id: Id42800d6b7f2633fb6978db8badffb353a5b2c54
    Reviewed-on: http://review.couchbase.org/105599
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -27,7 +27,10 @@ DECLARE_DEP (erlang VERSION OTP-20.3.8.11-cb1 PLATFORMS amzn2 centos6 centos7 de
 DECLARE_DEP (flatbuffers VERSION 1.10.0-cb2 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11 suse12 ubuntu14.04 ubuntu16.04 windows_msvc2015 windows_msvc2017 amzn2 suse15 ubuntu18.04)
 DECLARE_DEP (flex VERSION 2.5.4a-cb1 PLATFORMS windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (flex VERSION 2.6.4-cb4 PLATFORMS amzn2 centos6 centos7 debian8 debian9 macosx suse11 suse12 suse15 ubuntu14.04 ubuntu16.04 ubuntu18.04)
-DECLARE_DEP (folly VERSION v2018.08.13.00-cb1 PLATFORMS amzn2 centos6 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2015 windows_msvc2017)
+DECLARE_DEP (folly VERSION v2018.08.13.00-cb2 PLATFORMS amzn2 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04)
+DECLARE_DEP (folly VERSION v2018.08.13.00-cb1 PLATFORMS windows_msvc2015 windows_msvc2017)
+## Define folly's gflag dependency for Escrow build purpose, gflag is not required in Server's build
+DECLARE_DEP (gflags VERSION 2.2.1-cb2 PLATFORMS amzn2 centos6 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2015 windows_msvc2017 SKIP)
 DECLARE_DEP (glog VERSION 0.3.5-cb1 PLATFORMS amzn2 centos6 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2015 windows_msvc2017)
 DECLARE_DEP (grpc VERSION 1.12.0-cb1 PLATFORMS amzn2 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (icu4c VERSION 59.1-cb1 PLATFORMS centos6 centos7 debian7 debian8 debian9 macosx suse11 suse12 ubuntu14.04 ubuntu16.04 windows_msvc2015 windows_msvc2017)
