91f67d16c5038c70f571532c517bf774819bcd20
Kenneth Lareau <kenneth.lareau@couchbase.com>
2019-03-14 16:54:18 -0700

    MB-33272: Add protoc-gen-go to manifest.cmake (cbdeps 2.0)
    Change-Id: I6cec3f29517b9379ea7c09edf0f8ed5a46376098
    Reviewed-on: http://review.couchbase.org/106249
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -51,6 +51,7 @@ DECLARE_DEP (openssl VERSION 1.0.2k-cb3 PLATFORMS macosx)
 DECLARE_DEP (opentracing-cpp VERSION v1.5.1-cb1 PLATFORMS amzn2 centos7 debian8 debian9 macos suse11 suse12 ubuntu16.04 ubuntu18.04)
 DECLARE_DEP (pcre VERSION 8.42-cb1 PLATFORMS amzn2 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04)
 DECLARE_DEP (pcre VERSION 8.42-cb2 PLATFORMS windows_msvc2017)
+DECLARE_DEP (protoc-gen-go V2 VERSION 1.3.0 BUILD 3 PLATFORMS amzn2 centos7 debian8 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (rocksdb VERSION 5.12.5-cb1 PLATFORMS amzn2 centos6 centos7 debian8 debian9 macosx suse11 suse12 suse15 ubuntu14.04 ubuntu16.04)
 # TODO: cbdeps-build is not building for ubuntu18 (issue tracked in MB-31199), so use the previous dep for now. Remove this as soon as cbdeps-build has been fixed.
 DECLARE_DEP (rocksdb VERSION 5.8.0-cb2 PLATFORMS ubuntu18.04)
