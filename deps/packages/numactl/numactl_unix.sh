014260b3cd811c57a26ef4434baf3aa98b88e178
Kenneth Lareau <kenneth.lareau@couchbase.com>
2019-04-08 13:06:04 -0700

    MB-32376: Add support for RHEL8 for cbdeps (Mad Hatter)
    Change-Id: I1f55ba799f730fa91c390e44bd1bc817a34c5b84
    Reviewed-on: http://review.couchbase.org/106966
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/packages/numactl/numactl_unix.sh
+++ deps/packages/numactl/numactl_unix.sh
@@ -4,7 +4,12 @@ INSTALL_DIR=$1
 PLATFORM=$2
 VERSION=$3
 
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+
 ./autogen.sh || exit 1
 ./configure --prefix=${INSTALL_DIR} || exit 1
+git apply ${SCRIPTPATH}/numactl_define.patch
 make -j8 && make install
 rm -rf ${INSTALL_DIR}/bin ${INSTALL_DIR}/share
