884370896a1b4fc3166f395a8b62d1fadaf06e43
Chris Hillery <ceej@couchbase.com>
2019-06-02 23:57:48 -0700

    MB-34392: grpc libs, cppplugin, and third-party static libs (Windows cbdeps)
    Also, switch build to Ninja for speed, and apply patch to support building
    with latest MSVC 2017
    
    Change-Id: I3add1d3e1af1d69474f26661194664c3aad7caa6
    Reviewed-on: http://review.couchbase.org/109554
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- deps/packages/grpc/grpc_windows.bat
+++ deps/packages/grpc/grpc_windows.bat
@@ -1,6 +1,10 @@
 set INSTALL_DIR=%1
 set DEPS=%WORKSPACE%\deps
 
+rem Apply patch for building boringssl with latest MSVC2017
+git apply %~dp0\win_msvc2017_boringssl.patch
+
+rem Download cbdep and use it to get Golang
 set SITE=https://packages.couchbase.com/cbdep/cbdep-0.8.0-windows.exe
 set FILENAME=%WORKSPACE%\cbdep.exe
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('%SITE%', '%FILENAME%') }" || goto error
@@ -11,17 +15,23 @@ set GOROOT=%DEPS%\go1.10.3
 set PATH=%GOROOT%\bin;%PATH%
 
 rem Build grpc binaries and libraries
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64
 md .build
 cd .build
-cmake .. -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX="C:\cb2\install"
-cmake --build . --target INSTALL --config Release
+cmake .. -G Ninja ^
+  -D CMAKE_C_COMPILER=cl -D CMAKE_CXX_COMPILER=cl ^
+  -D CMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -D CMAKE_INSTALL_PREFIX="%INSTALL_DIR%" || goto error
+ninja install || goto error
 
-rem Copy wholesale to output directory.
-xcopy C:\cb2\install %INSTALL_DIR% /O /X /E /H /K || goto error
-
-rem Delete install temp directory
-rmdir /q /s "C:\cb2\install" 2>NUL || goto error
+rem Copy stuff that grpc build doesn't think to include in install directory
+copy *.lib %INSTALL_DIR%\lib || goto error
+copy grpc_cpp_plugin.exe %INSTALL_DIR%\bin || goto error
+copy third_party\protobuf\libprotobuf.lib %INSTALL_DIR%\lib || goto error
+copy third_party\zlib\zlib*.lib %INSTALL_DIR%\lib || goto error
+copy third_party\boringssl\gtest.lib %INSTALL_DIR%\lib || goto error
+copy third_party\boringssl\crypto\crypto.lib %INSTALL_DIR%\lib || goto error
+copy third_party\boringssl\decrepit\decrepit.lib %INSTALL_DIR%\lib
+copy third_party\boringssl\ssl\ssl.lib %INSTALL_DIR%\lib || goto error
 
 goto :eof
 
