0bc400c3876d18cd6194c1d58b5406474db5e7a7
Chris Hillery <ceej@couchbase.com>
2019-06-18 17:10:50 -0700

    MB-32895: Rebuild PCRE on Windows as a dynamic library, using MinGW
    Change-Id: I0dc7e63cea32d4bf70d56a754025e99b5bc9c123
    Reviewed-on: http://review.couchbase.org/109157
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cmake/Modules/FindCouchbasePCRE.cmake
+++ cmake/Modules/FindCouchbasePCRE.cmake
@@ -5,9 +5,11 @@
 # standard CMake conventions so it may work in other environments as well.
 # Sets:
 # PCRE_INCLUDE_DIR
-# PCRE_LIBRARY
-# PCRE_LIBRARIES (not fully implemented, only includes libpcre.so)
 # PCRE_LIBRARY_DIR
+#
+# Important: the PCRE cbdep package is only expected to work with CGo.
+# Linking it to MSVC-compiled C/C++ code will likely not work due to the
+# absence of the .lib link library. See comment further down.
 
 IF (NOT DEFINED PCRE_FOUND)
 
@@ -21,26 +23,34 @@ IF (NOT DEFINED PCRE_FOUND)
 
   FIND_PATH (PCRE_INCLUDE_DIR
     NAMES pcre.h
-    PATHS "${_exploded_dir}"
+    HINTS "${_exploded_dir}"
     PATH_SUFFIXES include
     ${_no_default_path})
 
-  FIND_LIBRARY (PCRE_LIBRARY
-    NAMES pcre
-    PATHS "${_exploded_dir}"
-    PATH_SUFFIXES lib
+  # I am theorizing a bit here, but: On Windows I do not believe we can use
+  # FIND_LIBRARY() here. This is because PCRE was built using MinGW (due to
+  # insurmountable problems getting it to compile correctly in MSVC), but
+  # our overall Server CMake project is in "MSVC Mode" and hence is expecting
+  # to "find" a pcre.lib link library for building. No such library is produced
+  # by MinGW, and while there is some evidence that it can be convinced to do
+  # so, the doc I've been able to find online left me too confused to try.
+  # Moreover, since PCRE is only actually linked by Go code in our project
+  # (which uses MinGW under the covers anyway), we never actually NEED the
+  # .lib file; all we need is the path to the directory containing libpcre.dll.
+  # There is extensive confusing commentary about this on MB-32895.
+  FIND_PATH (PCRE_LIBRARY_DIR
+    NAMES libpcre.dll libpcre.so
+    HINTS "${CMAKE_INSTALL_PREFIX}/bin" "${CMAKE_INSTALL_PREFIX}/lib"
     ${_no_default_path})
 
   INCLUDE (FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS (PCRE DEFAULT_MSG PCRE_LIBRARY PCRE_INCLUDE_DIR)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS (PCRE DEFAULT_MSG PCRE_LIBRARY_DIR PCRE_INCLUDE_DIR)
 
   IF (PCRE_FOUND)
-	SET (PCRE_LIBRARIES ${PCRE_LIBRARY})
-    GET_FILENAME_COMPONENT (PCRE_LIBRARY_DIR "${PCRE_LIBRARY}" DIRECTORY)
+    MESSAGE (STATUS "PCRE header dir: ${PCRE_INCLUDE_DIR}")
     MESSAGE (STATUS "PCRE library dir: ${PCRE_LIBRARY_DIR}")
   ELSE (PCRE_FOUND)
-    SET (PCRE_LIBRARIES)
-    SET (PCRE_LIBRARY_DIR)
+    MESSAGE (FATAL_ERROR "PCRE is required for building Couchbase, but was not found")
   ENDIF (PCRE_FOUND)
 
   MARK_AS_ADVANCED (PCRE_INCLUDE_DIR PCRE_LIBRARY PCRE_LIBRARY_DIR PCRE_LIBRARIES)
--- deps/packages/pcre/pcre_windows.bat
+++ deps/packages/pcre/pcre_windows.bat
@@ -1,11 +1,13 @@
 set INSTALL_DIR=%1
 
 rem Build pcre libraries
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64
 md .build
 cd .build
-cmake .. -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -D CMAKE_BUILD_TYPE=RelWithDebInfo || goto error
-cmake --build . --target INSTALL --config Release || goto error
+cmake .. -G "Ninja" ^
+  -D CMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
+  -D CMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -D BUILD_SHARED_LIBS=ON || goto error
+ninja install || goto error
 
 goto :eof
 
