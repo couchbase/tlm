7431e1319c25d5e8045d165a7e638b7340b269d2
Richard de Mellow <richard.demellow@couchbase.com>
2019-08-23 14:01:38 +0100

    MB-34971 Update jemalloc_windows.bat to use msbuild.exe
    Update jemalloc_windows.bat to use msbuild.exe so we correctly build
    jemalloc.dll on Windows. As the Jemalloc project now suggest using
    Visual Studios tool chain to build on the windows platform.
    
    Change-Id: Id941ee5b7565f42be71133732a429075fff897d0
    Reviewed-on: http://review.couchbase.org/113752
    Tested-by: Richard de Mellow <richard.demellow@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- deps/packages/jemalloc/jemalloc_windows.bat
+++ deps/packages/jemalloc/jemalloc_windows.bat
@@ -4,12 +4,18 @@ set PATH=%PATH%;c:\cygwin\bin
 set INSTALL_DIR=%1
 
 sh -c "./autogen.sh CC=cl --prefix=%INSTALL_DIR% --with-jemalloc-prefix=je_ --disable-cache-oblivious --disable-zone-allocator --enable-prof"
+rem Set up install directory paths
+sh -c "mkdir -p %INSTALL_DIR% && mkdir -p %INSTALL_DIR%/bin && mkdir -p %INSTALL_DIR%/lib && mkdir -p %INSTALL_DIR%/include"
 
-make build_lib_shared
-make install_lib_shared install_include install_bin
+rem Make the header files and bin directory
+make install_include install_bin
+rem Make jemalloc.dll using msbuild.exe and jemalloc.vcxproj as this is now the supported build method on Windows
+msbuild .\msvc\projects\vc2017\jemalloc\jemalloc.vcxproj -property:Configuration=Release -maxcpucount:8
 
-rem The standard install rules don't install the import library or PDB files - do it manually.
-cp lib\jemalloc.lib lib\jemalloc.pdb %INSTALL_DIR%\lib
+rem Copy the build output to the install directory
+sh -c "cp -f msvc/projects/vc2017/jemalloc/x64/Release/jemalloc.dll %INSTALL_DIR%/lib"
+sh -c "cp -f msvc/projects/vc2017/jemalloc/x64/Release/jemalloc.lib %INSTALL_DIR%/lib"
+sh -c "cp -f msvc/projects/vc2017/jemalloc/x64/Release/jemalloc.pdb %INSTALL_DIR%/lib"
 
 rem Need strings.h and more on Windows. Jemalloc supplies it, copy manually.
-cp -r include\msvc_compat %INSTALL_DIR%\include
+cp -r include/msvc_compat %INSTALL_DIR%/include
