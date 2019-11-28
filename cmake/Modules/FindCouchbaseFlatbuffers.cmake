c2d2709ce011b43b358029e8bc33cb503e584c33
Dave Rigby <daver@couchbase.com>
2019-11-28 15:46:04 +0000

    MB-37096: Build debug variant of flatbuffers on Windows
    In addition to the current Release variant of flatbuffers, also
    build the debug variant on Windows.
    
    The debug variant is linked against the MSVC Debug CRT, which allows
    us to link against flatbuffers when the application (e.g. memcached) is
    linked with Debug CRT.
    
    Change-Id: Id8e8110e19d59a092b28441eab3ba3f7b3d18a55
    Reviewed-on: http://review.couchbase.org/119429
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- cmake/Modules/FindCouchbaseFlatbuffers.cmake
+++ cmake/Modules/FindCouchbaseFlatbuffers.cmake
@@ -15,45 +15,49 @@
 
 # Locate Google flatbuffers
 # This module defines
-#  FLATBUFFERS_FOUND, if we located flatbuffers
 #  FLATBUFFERS_INCLUDE_DIR, where to find the flatbuffer headers
 #  FLATC, the flatc binary
-if (NOT DEFINED FLATBUFFERS_FOUND)
-    include(PlatformIntrospection)
+include(PlatformIntrospection)
 
-    cb_get_supported_platform(_supported_platform)
-    if (_supported_platform)
-        # Supported platforms should only use the provided hints and pick it up
-        # from cbdeps
-        set(_flatbuffers_no_default_path NO_DEFAULT_PATH)
-    endif ()
+cb_get_supported_platform(_supported_platform)
+if (_supported_platform)
+    # Supported platforms should only use the provided hints and pick it up
+    # from cbdeps
+    set(_flatbuffers_no_default_path NO_DEFAULT_PATH)
+endif ()
 
-    set(google_flatbuffers_exploded ${CMAKE_BINARY_DIR}/tlm/deps/flatbuffers.exploded)
+set(google_flatbuffers_exploded ${CMAKE_BINARY_DIR}/tlm/deps/flatbuffers.exploded)
 
-    find_path(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
-              HINTS ${google_flatbuffers_exploded}/include
-              ${_flatbuffers_no_default_path})
-    if (NOT FLATBUFFERS_INCLUDE_DIR)
-        message(FATAL_ERROR "Failed to locate flatbuffers/flatbuffers.h")
-    endif ()
+find_path(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
+            HINTS ${google_flatbuffers_exploded}/include
+            ${_flatbuffers_no_default_path})
+if (NOT FLATBUFFERS_INCLUDE_DIR)
+    message(FATAL_ERROR "Failed to locate flatbuffers/flatbuffers.h")
+endif ()
 
-    find_program(FLATC flatc HINTS ${google_flatbuffers_exploded}/bin ${_flatbuffers_no_default_path})
-    if (NOT FLATC)
-        message(FATAL_ERROR "Failed to locate flatc")
-    endif ()
+find_program(FLATC flatc HINTS ${google_flatbuffers_exploded}/bin ${_flatbuffers_no_default_path})
+if (NOT FLATC)
+    message(FATAL_ERROR "Failed to locate flatc")
+endif ()
 
-    find_library(FLATBUFFERS_LIBRARIES
-                 NAMES flatbuffers
-                 PATHS ${google_flatbuffers_exploded}/lib/)
+find_library(FLATBUFFERS_LIBRARY_RELEASE
+             NAMES flatbuffers
+             PATHS ${google_flatbuffers_exploded}/lib/)
 
-    if (NOT FLATBUFFERS_LIBRARIES)
-        message(FATAL_ERROR "Failed to locate flatbuffers library")
-    endif ()
+find_library(FLATBUFFERS_LIBRARY_DEBUG
+             NAMES flatbuffersd
+             PATHS ${google_flatbuffers_exploded}/lib/)
 
-    message(STATUS "Found Google Flatbuffers headers in: ${FLATBUFFERS_INCLUDE_DIR}")
-    message(STATUS "                            library: ${FLATBUFFERS_LIBRARIES}")
-    message(STATUS "                           compiler: ${FLATC}")
+# Defines FLATBUFFERS_LIBRARY / LIBRARIES to the correct Debug / Release
+# lib based on the current BUILD_TYPE
+select_library_configurations(FLATBUFFERS)
 
-    set(FLATBUFFERS_FOUND true CACHE BOOL "Found Google flatbuffers" FORCE)
-    mark_as_advanced(FLATBUFFERS_FOUND FLATBUFFERS_INCLUDE_DIR FLATC FLATBUFFERS_LIBRARIES)
-endif (NOT DEFINED FLATBUFFERS_FOUND)
+if (NOT FLATBUFFERS_LIBRARIES)
+    message(FATAL_ERROR "Failed to locate flatbuffers library")
+endif ()
+
+message(STATUS "Found Google Flatbuffers headers in: ${FLATBUFFERS_INCLUDE_DIR}")
+message(STATUS "                            library: ${FLATBUFFERS_LIBRARIES}")
+message(STATUS "                           compiler: ${FLATC}")
+
+mark_as_advanced(FLATBUFFERS_INCLUDE_DIR FLATC FLATBUFFERS_LIBRARIES)
