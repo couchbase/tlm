f7131d0a4d0dc87cba2a31e2eb128c6749862125
Dave Rigby <daver@couchbase.com>
2019-11-28 15:46:04 +0000

    MB-37096: Use folly v2019.08.12.00-cb2 & glog 0.3.5-cb4
    Update to folly v2019.08.12.00, cb2. This includes the debug build of
    folly in addition to a release variant.
    
    Also upgrade glog to 0.3.5-cb4 (dependancy of folly).
    
    This requires changes to the FindCouchbaseFolly.cmake script, to search
    for both the release and debug versions of the library.
    
    Change-Id: Ibe30a87de4a0a872abb04dfa5fb215ae40c42569
    Reviewed-on: http://review.couchbase.org/119428
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- cmake/Modules/FindCouchbaseFolly.cmake
+++ cmake/Modules/FindCouchbaseFolly.cmake
@@ -9,68 +9,76 @@ INCLUDE(FindCouchbaseDoubleConversion)
 INCLUDE(FindCouchbaseGlog)
 INCLUDE(FindCouchbaseLibevent)
 
-if (NOT DEFINED FOLLY_FOUND)
-    include(PlatformIntrospection)
+include(PlatformIntrospection)
+include(SelectLibraryConfigurations)
 
-    cb_get_supported_platform(_supported_platform)
-    if (_supported_platform)
-        # Supported platforms should only use the provided hints and pick it up
-        # from cbdeps
-        set(_folly_no_default_path NO_DEFAULT_PATH)
-    endif ()
+cb_get_supported_platform(_supported_platform)
+if (_supported_platform)
+    # Supported platforms should only use the provided hints and pick it up
+    # from cbdeps
+    set(_folly_no_default_path NO_DEFAULT_PATH)
+endif ()
 
-    set(_folly_exploded ${CMAKE_BINARY_DIR}/tlm/deps/folly.exploded)
+set(_folly_exploded ${CMAKE_BINARY_DIR}/tlm/deps/folly.exploded)
 
-    find_path(FOLLY_INCLUDE_DIR folly/folly-config.h
-            PATH_SUFFIXES include
-            PATHS ${_folly_exploded}
-            ${_folly_no_default_path})
+find_path(FOLLY_INCLUDE_DIR folly/folly-config.h
+          PATH_SUFFIXES include
+          PATHS ${_folly_exploded}
+          ${_folly_no_default_path})
 
-    # Somebody working on folly decided to add a template parameter to
-    # SharedMutex that defaults to whether or not folly was compiled with or
-    # without TSan. This is a pain for us because we will need different symbols
-    # based on whether or not we are compiling with TSan. If we generally use
-    # TSan on a given platform then we should have created an additional
-    # library named 'libfollytsan.a' that we should link against instead of the
-    # normal library 'libfolly.a' if we are building with TSan enabled.
-    if(CB_THREADSANITIZER)
-        set(folly_lib follytsan)
-    else(CB_THREADSANITIZER)
-        set(folly_lib folly)
-    endif(CB_THREADSANITIZER)
+if (NOT FOLLY_INCLUDE_DIR)
+    message(FATAL_ERROR "Failed to locate folly include directory")
+endif ()
 
-    find_library(FOLLY_LIBRARIES
-            NAMES ${folly_lib}
-            HINTS ${_folly_exploded}/lib
-            ${_folly_no_default_path})
+# Somebody working on folly decided to add a template parameter to
+# SharedMutex that defaults to whether or not folly was compiled with or
+# without TSan. This is a pain for us because we will need different symbols
+# based on whether or not we are compiling with TSan. If we generally use
+# TSan on a given platform then we should have created an additional
+# library named 'libfollytsan.a' that we should link against instead of the
+# normal library 'libfolly.a' if we are building with TSan enabled.
+if(CB_THREADSANITIZER)
+    set(folly_lib follytsan)
+else(CB_THREADSANITIZER)
+    set(folly_lib folly)
+endif(CB_THREADSANITIZER)
 
-    if(FOLLY_INCLUDE_DIR AND FOLLY_LIBRARIES)
-        MESSAGE(STATUS "Found Facebook Folly headers: ${FOLLY_INCLUDE_DIR}")
-        MESSAGE(STATUS "                   libraries: ${FOLLY_LIBRARIES}")
+find_library(FOLLY_LIBRARY_RELEASE
+             NAMES ${folly_lib}
+             HINTS ${_folly_exploded}/lib
+             ${_folly_no_default_path})
 
-        if(NOT DOUBLE_CONVERSION_INCLUDE_DIR OR NOT DOUBLE_CONVERSION_LIBRARIES)
-            MESSAGE(FATAL_ERROR "Can't use Folly without double-conversion library")
-        endif()
+find_library(FOLLY_LIBRARY_DEBUG
+             NAMES follyd
+             HINTS ${_folly_exploded}/lib
+             ${_jemalloc_no_default_path})
 
-        # Append Folly's depenancies to the include / lib variables so users
-        # of Folly pickup the dependancies automatically.
-        list(APPEND FOLLY_INCLUDE_DIR ${DOUBLE_CONVERSION_INCLUDE_DIR} ${GLOG_INCLUDE_DIR})
-        set(FOLLY_INCLUDE_DIR ${FOLLY_INCLUDE_DIR} CACHE STRING "Folly include directories" FORCE)
+# Defines FOLLY_LIBRARY / LIBRARIES to the correct Debug / Release
+# lib based on the current BUILD_TYPE
+select_library_configurations(FOLLY)
 
-        list(APPEND FOLLY_LIBRARIES
-                ${DOUBLE_CONVERSION_LIBRARIES}
-                ${GLOG_LIBRARIES}
-                ${CMAKE_DL_LIBS}
-                ${Boost_SYSTEM_LIBRARY}
-                ${Boost_THREAD_LIBRARY}
-                ${LIBEVENT_LIBRARIES})
-        set(FOLLY_LIBRARIES ${FOLLY_LIBRARIES} CACHE STRING "Folly libraries" FORCE)
+if (NOT FOLLY_LIBRARIES)
+    message(FATAL_ERROR "Failed to locate folly library")
+endif ()
 
-        set(FOLLY_FOUND true CACHE BOOL "Found Facebook Folly" FORCE)
-    else()
-        set(FOLLY_FOUND false CACHE BOOL "Found Facebook Folly" FORCE)
-        MESSAGE(FATAL_ERROR "Can't build Couchbase without Facebook Folly")
-    endif()
+MESSAGE(STATUS "Found Facebook Folly headers: ${FOLLY_INCLUDE_DIR}")
+MESSAGE(STATUS "                   libraries: ${FOLLY_LIBRARIES}")
 
-    mark_as_advanced(FOLLY_FOUND FOLLY_INCLUDE_DIR FOLLY_LIBRARIES)
+if(NOT DOUBLE_CONVERSION_INCLUDE_DIR OR NOT DOUBLE_CONVERSION_LIBRARIES)
+    MESSAGE(FATAL_ERROR "Can't use Folly without double-conversion library")
 endif()
+
+# Append Folly's depenancies to the include / lib variables so users
+# of Folly pickup the dependancies automatically.
+list(APPEND FOLLY_INCLUDE_DIR ${DOUBLE_CONVERSION_INCLUDE_DIR} ${GLOG_INCLUDE_DIR})
+set(FOLLY_INCLUDE_DIR ${FOLLY_INCLUDE_DIR} CACHE STRING "Folly include directories" FORCE)
+
+list(APPEND FOLLY_LIBRARIES
+            ${DOUBLE_CONVERSION_LIBRARIES}
+            ${GLOG_LIBRARIES}
+            ${CMAKE_DL_LIBS}
+            ${Boost_SYSTEM_LIBRARY}
+            ${Boost_THREAD_LIBRARY}
+            ${LIBEVENT_LIBRARIES})
+
+mark_as_advanced(FOLLY_INCLUDE_DIR FOLLY_LIBRARIES)
