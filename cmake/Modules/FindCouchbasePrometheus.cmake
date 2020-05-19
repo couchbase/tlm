6c8ca7377a2cf6d82ece5dd30e77905e4714474c
James Harrison <james.harrison@couchbase.com>
2020-05-19 12:12:03 +0100

    MB-39041: Add prometheus-cpp to cbdeps
    KV-Engine will use prometheus-cpp to export metrics.
    
    Change-Id: Ib6ddc286e1f64c8ad3c9eff979acc24ad68580ab
    Reviewed-on: http://review.couchbase.org/c/tlm/+/128467
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ cmake/Modules/FindCouchbasePrometheus.cmake
@@ -0,0 +1,40 @@
+# Locate prometheus-cpp
+# This module defines
+#  PROMETHEUS_LIBRARIES, Library path and libs
+#  PROMETHEUS_INCLUDE_DIR, where to find the headers
+
+set(_prometheus_exploded ${CMAKE_BINARY_DIR}/tlm/deps/prometheus-cpp.exploded)
+
+include(PlatformIntrospection)
+
+cb_get_supported_platform(_supported_platform)
+if (_supported_platform)
+    set(_prometheus_no_default_path NO_DEFAULT_PATH)
+endif ()
+
+find_path(PROMETHEUS_INCLUDE_DIR prometheus/registry.h
+        PATH_SUFFIXES include
+        PATHS ${_prometheus_exploded}
+        ${_prometheus_no_default_path})
+
+find_library(PROMETHEUS_CORE_LIBRARY
+        NAMES prometheus-cpp-core
+        HINTS ${_prometheus_exploded}/lib
+        ${_prometheus_no_default_path})
+
+find_library(PROMETHEUS_PULL_LIBRARY
+        NAMES prometheus-cpp-pull
+        HINTS ${_prometheus_exploded}/lib
+        ${_prometheus_no_default_path})
+
+set(PROMETHEUS_LIBRARIES ${PROMETHEUS_CORE_LIBRARY} ${PROMETHEUS_PULL_LIBRARY})
+
+if (NOT PROMETHEUS_INCLUDE_DIR)
+    message(FATAL_ERROR "Failed to locate prometheus-cpp include directory")
+endif ()
+
+IF (PROMETHEUS_LIBRARIES AND PROMETHEUS_INCLUDE_DIR)
+    MESSAGE(STATUS "Found prometheus-cpp in ${PROMETHEUS_INCLUDE_DIR} : ${PROMETHEUS_LIBRARIES}")
+endif ()
+
+mark_as_advanced(PROMETHEUS_INCLUDE_DIR PROMETHEUS_LIBRARIES)
