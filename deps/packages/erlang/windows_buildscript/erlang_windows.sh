d0640265cc0c3f99296eebfb2a63409b8ba2805d
Chris Hillery <ceej@couchbase.com>
2020-09-27 23:56:07 -0700

    MB-40090: Update to OpenSSL 1.1.1h
    Since Erlang on Windows links OpenSSL statically, we also update to a
    new build number for Erlang. Did so on all platforms to keep things
    consistent, even though on Linux/Mac it's not strictly necessary.
    
    Improved Erlang Windows build to dynamically install the right version
    of OpenSSL rather than expecting it to exist on the build VM.
    
    Updated mentions of OpenSSL in folly and libevent build scripts, but
    didn't actually rebuild since OpenSSL is not actually included in
    those packages.
    
    Change-Id: Ic2f6b3c285c926d4521ddabc417598716cb9235c
    Reviewed-on: http://review.couchbase.org/c/tlm/+/136952
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- deps/packages/erlang/windows_buildscript/erlang_windows.sh
+++ deps/packages/erlang/windows_buildscript/erlang_windows.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 set -ex
 
+################## OPENSSL #####################
+# Chocolatey OpenSSL versions are eg. "1.1.1.800", where "800" == "h"
+# because "h" is the eighth letter of the alphabet.
+OPENSSL_VER=1.1.1.800
+################################################
+
+
 echo start build at `date`
 
 thisdir=`pwd`
 
 otp_version=$1
 cb_buildnumber=$2
+
+# start by ensuring right version of OpenSSL is installed.
+# This isn't a very 'friendly' way of doing it, but since we already know
+# this VM only exists for building Erlang on Windows, it's OK.
+/cygdrive/c/ProgramData/chocolatey/bin/choco install -y \
+    --allow-downgrade openssl --version=${OPENSSL_VER} --no-progress
+# otp_build and configure don't like paths with spaces, so copy the OpenSSL
+# installation. Also note that this path matches LIB and LIBPATH in
+# ./cygwin.bash_profile.
+OPENSSL_DIR=/cygdrive/c/OpenSSL
+rm -rf ${OPENSSL_DIR}
+cp -a "/cygdrive/c/Program Files/OpenSSL-Win64" ${OPENSSL_DIR}
+
+# Convert dos2unix
+echo "Converting line endings"
+find . -type f | xargs dos2unix &>/dev/null
 
 # Parse erts_version out of erts/vsn.mk
 eval $(grep '^VSN' erts/vsn.mk | sed -e 's/ //g')
@@ -18,25 +41,27 @@ package_name="${otp_version}-${cb_buildnumber}"
 package_name_tgz="erlang-windows_msvc2017-amd64-$package_name.tgz"
 package_name_md5="erlang-windows_msvc2017-amd64-$package_name.md5"
 
-# Ensure build uses correct libraries with OpenSSL 1.1.1d
+# Ensure build uses correct libraries with OpenSSL 1.1.1
 patch_ssl() {
     sed -i "s/SSL_CRYPTO_LIBNAME = @SSL_CRYPTO_LIBNAME@/SSL_CRYPTO_LIBNAME = libcrypto64MD/g" ./lib/crypto/c_src/Makefile.in
     sed -i "s/SSL_SSL_LIBNAME = @SSL_SSL_LIBNAME@/SSL_SSL_LIBNAME = libssl64MD/g" ./lib/crypto/c_src/Makefile.in
 }
 
-# Convert dos2unix
-echo "Converting line endings"
-find . -type f |xargs dos2unix &>/dev/null
-
 installdir="/cygdrive/c/Program Files/erl${erts_version}"
 mkdir -p "${installdir}"
 
 ## build the source, as per instructions
-eval `./otp_build env_win32 x64`
+eval $(./otp_build env_win64)
 ./otp_build autoconf 2>&1 | tee autoconf.out
-# Hardcode library names in Makefile template to allow building against OpenSSL 1.1.1d
 patch_ssl
-./otp_build configure  --with-ssl=/cygdrive/c/OpenSSL 2>&1 | tee configure.out
+./otp_build configure \
+      --without-javac \
+      --without-wx \
+      --without-et \
+      --without-debugger \
+      --without-megaco \
+      --without-observer \
+      --with-ssl=${OPENSSL_DIR} 2>&1 | tee configure.out
 ./otp_build boot -a 2>&1 | tee boot.out
 ./otp_build release -a 2>&1 | tee release.out
 #####./otp_build debuginfo_win32 -a 2>&1 | tee dbginfo.out
