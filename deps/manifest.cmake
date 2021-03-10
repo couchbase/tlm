058cdf997b926d1da534bddb1b0cbdb100b61ede
Chris Hillery <ceej@couchbase.com>
2021-03-10 02:54:05 -0800

    MB-43626: Add rhel8 for all deps
    Change-Id: I771e9d176515ff7d7f6aee843ba06a768f4f39e2
    Reviewed-on: http://review.couchbase.org/c/tlm/+/148162
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -16,12 +16,12 @@
 # version, you must also update couchbase-server-black-duck-manifest.json
 # in this same directory!
 #
-DECLARE_DEP (breakpad V2 VERSION 20200430 BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
+DECLARE_DEP (breakpad V2 VERSION 20200430 BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (boost VERSION 1.74.0-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (curl V2 VERSION 7.66.0 BUILD 8 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (double-conversion VERSION 3.0.0-cb2 PLATFORMS amzn2 centos7 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (double-conversion VERSION 3.0.0-cb4 PLATFORMS centos8 debian10 rhel8 ubuntu20.04)
-DECLARE_DEP (erlang VERSION cheshirecat-cb5 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
+DECLARE_DEP (erlang VERSION cheshirecat-cb5 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (flatbuffers VERSION 1.10.0-cb5 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (folly VERSION v2020.08.24.00-couchbase-cb1 PLATFORMS ubuntu16.04)
 DECLARE_DEP (folly VERSION v2020.08.24.00-couchbase-cb4 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
@@ -33,17 +33,17 @@ DECLARE_DEP (json VERSION 3.5.0-cb2 PLATFORMS centos8 debian10 rhel8 ubuntu20.04
 DECLARE_DEP (libevent VERSION 2.1.11-cb6 PLATFORMS amzn2 centos7 centos8 debian10 debian9 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (libuv V2 VERSION 1.20.3 BUILD 17 PLATFORMS amzn2 centos7 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (libuv V2 VERSION 1.20.3 BUILD 19 PLATFORMS centos8 debian10 rhel8 ubuntu20.04)
-DECLARE_DEP (lz4 VERSION 1.9.2-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04)
+DECLARE_DEP (lz4 VERSION 1.9.2-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04)
 DECLARE_DEP (maven VERSION 3.5.2-cb5 PLATFORMS amzn2 centos7 debian9 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (maven VERSION 3.5.2-cb6 PLATFORMS centos8 debian10 rhel8 ubuntu20.04)
 DECLARE_DEP (numactl VERSION 2.0.11-cb1 PLATFORMS amzn2 centos7 debian9 suse12 suse15 ubuntu16.04 ubuntu18.04)
 DECLARE_DEP (numactl VERSION 2.0.11-cb3 PLATFORMS centos8 debian10 rhel8 ubuntu20.04)
-DECLARE_DEP (openssl V2 VERSION 1.1.1h BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
+DECLARE_DEP (openssl V2 VERSION 1.1.1h BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (pcre VERSION 8.43-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (pcre VERSION 8.43-cb2 PLATFORMS macosx ubuntu20.04)
-DECLARE_DEP (prometheus V2 VERSION 2.22 BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
+DECLARE_DEP (prometheus V2 VERSION 2.22 BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (prometheus-cpp VERSION v0.10.0-couchbase-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
-DECLARE_DEP (protoc-gen-go V2 VERSION 1.2.5 BUILD 3 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
+DECLARE_DEP (protoc-gen-go V2 VERSION 1.2.5 BUILD 3 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 # We don't want RocksDB to end up in shipped production builds.
 # NB: I don't indent this IF() block just in case, because I know that some
 # scripts (such as escrow) parse this file manually.
