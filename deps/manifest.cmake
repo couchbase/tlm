76f6180a8f00ca142c9fcc85fbe3fb6a875df247
James Harrison <james.harrison@couchbase.com>
2021-03-15 11:14:42 +0000

    MB-43901: Update prometheus-cpp dep
    prometheus-cpp relies on CivetWeb, which has a known issue:
    
    https://github.com/civetweb/civetweb/issues/954
    
    This lead to memcached hanging on shutdown as the prometheus Exposer
    never finished destruction, instead waiting forever to deregister a
    response handler.
    
    Bump the CivetWeb submodule forward to the fix noted in that issue.
    
    Change-Id: I308257b7f0cbe59aef2709a9dc0f687e960e1106
    Reviewed-on: http://review.couchbase.org/c/tlm/+/148542
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -42,7 +42,7 @@ DECLARE_DEP (openssl V2 VERSION 1.1.1h BUILD 1 PLATFORMS amzn2 centos7 centos8 d
 DECLARE_DEP (pcre VERSION 8.43-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 windows_msvc2017)
 DECLARE_DEP (pcre VERSION 8.43-cb2 PLATFORMS macosx ubuntu20.04)
 DECLARE_DEP (prometheus V2 VERSION 2.22 BUILD 1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
-DECLARE_DEP (prometheus-cpp VERSION v0.10.0-couchbase-cb1 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
+DECLARE_DEP (prometheus-cpp VERSION v0.10.0-couchbase-cb2 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (protoc-gen-go V2 VERSION 1.2.5 BUILD 3 PLATFORMS amzn2 centos7 centos8 debian9 debian10 macosx rhel8 suse12 suse15 ubuntu16.04 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 # We don't want RocksDB to end up in shipped production builds.
 # NB: I don't indent this IF() block just in case, because I know that some
