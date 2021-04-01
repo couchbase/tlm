a44396723afecbd9f0b6814d9c7a38d89860fa5b
Chris Hillery <ceej@couchbase.com>
2021-04-01 01:46:33 -0700

    MB-45044: Allow overriding gcc location, and set build RPATH accordingly
    Ensure that the BUILD RPATH contains the path to GCC's libstdc++.so.
    
    Also introduces CB_GCC_PATH, with a default value currently of
    /opt/gcc-10.2.0. If that path exists, the build will prefer to use gcc
    from that location.
    
    Change-Id: I44aa5ff02d169e3ed5293a3cfaa4ba3552a6af64
    Reviewed-on: http://review.couchbase.org/c/tlm/+/149975
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/CouchbaseRpath.cmake
+++ cmake/Modules/CouchbaseRpath.cmake
@@ -3,12 +3,12 @@ SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 
 # If the system supports it we want to add in $ORIGIN
 IF (UNIX)
-   IF(APPLE)
-      SET (ORIGIN @executable_path)
-   ELSE(APPLE)
-      SET (ORIGIN \$ORIGIN)
-   ENDIF(APPLE)
-   SET(RPATH ${ORIGIN}/../lib)
+  IF(APPLE)
+    SET (ORIGIN @executable_path)
+  ELSE(APPLE)
+    SET (ORIGIN \$ORIGIN)
+  ENDIF(APPLE)
+  SET(RPATH ${ORIGIN}/../lib)
 ENDIF()
 
 SET(RPATH "${RPATH};${CMAKE_INSTALL_PREFIX}/lib")
@@ -19,3 +19,18 @@ ENDIF()
 SET(CMAKE_INSTALL_RPATH "${RPATH}")
 
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+# If using GCC, also ensure that we set a BUILD RPATH that includes
+# that gcc's libs such as libstdc++.so.
+IF ("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
+  EXECUTE_PROCESS(
+    COMMAND "${CMAKE_CXX_COMPILER}" -print-file-name=libstdc++.so
+    OUTPUT_VARIABLE _gccfile OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_VARIABLE _errormsg
+    RESULT_VARIABLE _failure)
+  IF (_failure)
+    MESSAGE (FATAL_ERROR "Error (${_failure}) determining path to libstdc++.so: ${_errormsg}")
+  ENDIF ()
+  GET_FILENAME_COMPONENT (_gccdir "${_gccfile}" DIRECTORY)
+  LIST (APPEND CMAKE_BUILD_RPATH "${_gccdir}")
+ENDIF()
