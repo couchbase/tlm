5ab2c9b08b98f1eae82c66c4bae2a83deadc5797
Chris Hillery <ceej@couchbase.com>
2021-04-05 01:29:17 -0700

    MB-45044: No longer need to set RPATH by hand; gcc handles it
    Change-Id: I44aa5ff02d169e3ed5293a3cfaa4ba3552a6af6f
    Reviewed-on: http://review.couchbase.org/c/tlm/+/150386
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/CouchbaseRpath.cmake
+++ cmake/Modules/CouchbaseRpath.cmake
@@ -19,18 +19,3 @@ ENDIF()
 SET(CMAKE_INSTALL_RPATH "${RPATH}")
 
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-
-# If using GCC, also ensure that we set a BUILD RPATH that includes
-# that gcc's libs such as libstdc++.so.
-IF ("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
-  EXECUTE_PROCESS(
-    COMMAND "${CMAKE_CXX_COMPILER}" -print-file-name=libstdc++.so
-    OUTPUT_VARIABLE _gccfile OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_VARIABLE _errormsg
-    RESULT_VARIABLE _failure)
-  IF (_failure)
-    MESSAGE (FATAL_ERROR "Error (${_failure}) determining path to libstdc++.so: ${_errormsg}")
-  ENDIF ()
-  GET_FILENAME_COMPONENT (_gccdir "${_gccfile}" DIRECTORY)
-  LIST (APPEND CMAKE_BUILD_RPATH "${_gccdir}")
-ENDIF()
