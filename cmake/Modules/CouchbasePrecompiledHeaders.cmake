7ba063c282af16aeffd5e55e07d4fd8cec007732
Ben Huddleston <ben.huddleston@couchbase.com>
2021-06-04 14:48:26 +0100

    MB-43677: Add reuse_pch function and option to enable/disable PCHs
    Change-Id: I685705c967311f32b5c3c0099cae8896308f9054
    Reviewed-on: http://review.couchbase.org/c/tlm/+/155177
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ cmake/Modules/CouchbasePrecompiledHeaders.cmake
@@ -0,0 +1,11 @@
+# reuse_pch will reuse the precompiled header of the given target for the given target.
+# The function can be globally 'disabled' with CB_PCH=OFF
+
+option(CB_PCH "CB_PCH defines if targets will use pre-compiled headers where configured." ON)
+
+function(reuse_pch target pch_target)
+    if (CB_PCH)
+        target_precompile_headers(${target} REUSE_FROM ${pch_target})
+        add_dependencies(${target} ${pch_target})
+    endif()
+endfunction()
