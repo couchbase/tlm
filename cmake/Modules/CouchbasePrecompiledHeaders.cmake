cd48d8b8d0a567fe8da7c204c51afba98a7afdd5
Dave Rigby <daver@couchbase.com>
2021-07-02 13:58:18 +0100

    MB-46988: Fix Windows LNK4221 warning after adding PCH support
    After adding precompiled header support, specifically
    "MB-46988: Fix PCH headers causing ccache cache misses"(1da8a18),
    we see the following linker error when building with MSVC:
    
        cmake_pch.cxx.obj : warning LNK4221: This object file does not
        define any previously undefined public symbols, so it will not
        be used by any link operation that consumes this library
    
    This is due to the fact we end up with the PCH object file
    (cmake_pch.cxx.obj) on the link line twice.
    
    This is a result of the aforementioned patch adding the following line
    to the reuse_pch() function:
    
        target_link_libraries(${target} PRIVATE ${pch_target})
    
    Note that 'pch_target' is already linked to 'target' via
    the target_precompile_headers() call, and for whatever reason CMake
    doesn't de-duplicate them.
    
    Fix this by solving the original ccache miss issue in a slightly
    different way - instead of linking to the entire PCH library, just
    lookup it's INCLUDE_DIRECTORIES (and those of it's dependancies)
    and add those to the target's INCLUDE_DIRECTORIES.
    
    Change-Id: Ibbcbeb757f532d56e35fcb042572076ab622efa8
    Reviewed-on: http://review.couchbase.org/c/tlm/+/156873
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- cmake/Modules/CouchbasePrecompiledHeaders.cmake
+++ cmake/Modules/CouchbasePrecompiledHeaders.cmake
@@ -7,12 +7,42 @@ function(reuse_pch target pch_target)
     if (CB_PCH)
         target_precompile_headers(${target} REUSE_FROM ${pch_target})
 
-        # Also need to link to pch_target so the target picks up include paths
-        # for the preprocessed headers. This isn't _strictly_ needed for a
-        # straight CMake build; but ccache wants to preprocess each source
-        # file by taking CMake-generated compile command and adding '-E' or
-        # similar, which requires the actual target source files can locate
-        # all "raw" precompiled headers themselves.
-        target_link_libraries(${target} PRIVATE ${pch_target})
+        # Also need to propogate any include directories from the pch_target,
+        # so the target picks up include paths for the preprocessed headers.
+        # This isn't _strictly_ needed for a straight CMake build; but ccache
+        # wants to preprocess each source file by taking CMake-generated
+        # compile command and adding '-E' or similar, which requires the actual
+        # target source files can locate all "raw" precompiled headers
+        # themselves.
+        # Note: We don't want to propagate _all_ dependancies (libraries etc),
+        # just the include paths - if we propogate libraries say using:
+        #
+        #     target_link_libraries(${target} PRIVATE ${pch_target})
+        #
+        # then we can end up lnking the PCH "library" twice - once via
+        # target_precompile_headers() above and once via target_link_libraries()
+        # which results in linker warnings with MSVC.
+
+        # Propagate INTERFACE_INCLUDE_DIRECTORIES properties set directly on
+        # the PCH target to the target.
+        get_target_property(include_dirs ${pch_target} INTERFACE_INCLUDE_DIRECTORIES)
+        set_property(TARGET ${target} APPEND PROPERTY INCLUDE_DIRECTORIES ${include_dirs})
+
+        # Propagate any INTERFACE_INCLUDE_DIRECTORIES which come via
+        # LINK_LIBRARIES on the PCH target to the target.
+        get_target_property(target_type "${pch_target}" TYPE)
+        if (${target_type} STREQUAL "INTERFACE_LIBRARY")
+            get_target_property(libs "${pch_target}" INTERFACE_LINK_LIBRARIES)
+        else()
+            get_target_property(libs "${pch_target}" LINK_LIBRARIES)
+        endif()
+        foreach(lib IN LISTS libs)
+            if(NOT TARGET "${lib}")
+                continue()
+            endif()
+
+            get_target_property(include_dirs ${lib} INTERFACE_INCLUDE_DIRECTORIES)
+            set_property(TARGET ${target} APPEND PROPERTY INCLUDE_DIRECTORIES ${include_dirs})
+        endforeach()
     endif()
 endfunction()
