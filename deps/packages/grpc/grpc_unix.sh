36d05fbdc4c57afd9baec4bac0e6536d468a41d5
Chris Hillery <ceej@couchbase.com>
2021-07-09 18:16:13 -0700

    CBD-3564: Restore grpc as a v1 cbdep.
    This was originally removed when converting grpc to v2 cbdeps and
    creating a new v2 grpc 1.31.1 package for SDK. However, Server still
    uses the v1 grpc 1.28.1 package, so this code should remain here until
    it upgrades. (In particular we want to update it to build for aarch64.)
    
    This partially reverts commit e343252f797c1eab97667bb834c90ac22e689ce4.
    
    Change-Id: I584378571611ce872a9ab2a9e67483fd67d4f6ba
    Reviewed-on: http://review.couchbase.org/c/tlm/+/157276
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

---
+++ deps/packages/grpc/grpc_unix.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -ex
+
+INSTALL_DIR=$1
+PLATFORM=$2
+VERSION=$3
+CBDEPS_DIR=$4
+
+# Build and install cares and protobuf from third_party
+cd third_party/protobuf/cmake
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+  -Dprotobuf_BUILD_TESTS=OFF \
+  -D CMAKE_PREFIX_PATH="${CBDEPS_DIR}/zlib.exploded" \
+  ..
+make -j8 install
+cd ../../../..
+
+cd third_party/cares/cares
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+  -DCARES_STATIC=ON -DCARES_STATIC_PIC=ON -DCARES_SHARED=OFF \
+  ..
+make -j8 install
+cd ../../../..
+
+# Build grpc binaries and libraries
+mkdir .build
+cd .build
+cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+  -D CMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+  -D CMAKE_PREFIX_PATH="${CBDEPS_DIR}/zlib.exploded;${CBDEPS_DIR}/openssl.exploded;${INSTALL_DIR}" \
+  -DgRPC_INSTALL=ON \
+  -DgRPC_BUILD_TESTS=OFF \
+  -DgRPC_PROTOBUF_PROVIDER=package \
+  -DgRPC_ZLIB_PROVIDER=package \
+  -DgRPC_CARES_PROVIDER=package \
+  -DgRPC_SSL_PROVIDER=package \
+  ..
+make -j8 install
+
+exit 0
