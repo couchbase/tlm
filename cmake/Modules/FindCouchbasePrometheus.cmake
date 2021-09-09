f78b56039aa938dbf741ed93df8d4f682cfe9ff3
Trond Norbye <trond.norbye@gmail.com>
2021-09-09 06:10:06 +0200

    Use modern cmake for prometheus-cpp
    Locate the package with find_package which installs the
    cmake targets
    
    Change-Id: Ic2a0f5f3b21bc6fb9d0c04c16e2253df644e01f4
    Reviewed-on: http://review.couchbase.org/c/tlm/+/161131
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cmake/Modules/FindCouchbasePrometheus.cmake
+++ cmake/Modules/FindCouchbasePrometheus.cmake
@@ -1,40 +1,22 @@
-# Locate prometheus-cpp
-# This module defines
-#  PROMETHEUS_LIBRARIES, Library path and libs
-#  PROMETHEUS_INCLUDE_DIR, where to find the headers
+#
+#     Copyright 2021 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 set(_prometheus_exploded ${CMAKE_BINARY_DIR}/tlm/deps/prometheus-cpp.exploded)
 
-include(PlatformIntrospection)
-
-cb_get_supported_platform(_is_supported_platform)
-if (_is_supported_platform)
-    set(_prometheus_no_default_path NO_DEFAULT_PATH)
+if (NOT DEFINED prometheus-cpp_ROOT AND EXISTS ${_prometheus_exploded})
+    set(prometheus-cpp_ROOT ${_prometheus_exploded})
 endif ()
 
-find_path(PROMETHEUS_INCLUDE_DIR prometheus/registry.h
-        PATH_SUFFIXES include
-        PATHS ${_prometheus_exploded}
-        ${_prometheus_no_default_path})
-
-find_library(PROMETHEUS_CORE_LIBRARY
-        NAMES prometheus-cpp-core
-        HINTS ${_prometheus_exploded}/lib
-        ${_prometheus_no_default_path})
-
-find_library(PROMETHEUS_PULL_LIBRARY
-        NAMES prometheus-cpp-pull
-        HINTS ${_prometheus_exploded}/lib
-        ${_prometheus_no_default_path})
-
-set(PROMETHEUS_LIBRARIES ${PROMETHEUS_PULL_LIBRARY} ${PROMETHEUS_CORE_LIBRARY})
-
-if (NOT PROMETHEUS_INCLUDE_DIR)
-    message(FATAL_ERROR "Failed to locate prometheus-cpp include directory")
-endif ()
-
-IF (PROMETHEUS_LIBRARIES AND PROMETHEUS_INCLUDE_DIR)
-    MESSAGE(STATUS "Found prometheus-cpp in ${PROMETHEUS_INCLUDE_DIR} : ${PROMETHEUS_LIBRARIES}")
-endif ()
-
-mark_as_advanced(PROMETHEUS_INCLUDE_DIR PROMETHEUS_LIBRARIES)
+find_package(prometheus-cpp REQUIRED)
