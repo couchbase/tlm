8e4720b923602a6f6d913adec55d438b621ce027
Dave Rigby <daver@couchbase.com>
2022-03-28 13:28:00 +0100

    MB-51532: jemalloc cbdep: Enable debug assertions on Debug build (unix)
    Compile jemalloc an additional time for Debug builds - enable runtime
    assertion checks and with a lower level of optimisation.
    
    This should allow us to catch any invalid usages of jemalloc APIs and
    things like double-free; while still using the same memory allocator
    as we use in production - ASan requires the use of system malloc.
    
    Not yet enabled for Windows as Debug builds there require linking a
    different C runtime library.
    
    Change-Id: I1616237348ec9eaa29c050cbb2b9e15c81efe052
    Reviewed-on: https://review.couchbase.org/c/tlm/+/172898
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- deps/packages/jemalloc/jemalloc_unix.sh
+++ deps/packages/jemalloc/jemalloc_unix.sh
@@ -12,23 +12,35 @@ set -e
 
 INSTALL_DIR=$1
 
+configure_args="--prefix=${INSTALL_DIR} \
+   --with-jemalloc-prefix=je_ \
+   --disable-cache-oblivious \
+   --disable-zone-allocator \
+   --disable-initial-exec-tls"
+
 # Profiling only supported on non-Darwin.
 if [ $(uname -s) != "Darwin" ]; then
-    extra_configure_args="--enable-prof"
+    configure_args+=" --enable-prof"
 fi
 
-./autogen.sh
-CPPFLAGS=-I/usr/local/include ./configure \
-    --prefix=${INSTALL_DIR} \
-    --with-jemalloc-prefix=je_ \
-    --disable-cache-oblivious \
-    --disable-zone-allocator \
-    --disable-initial-exec-tls \
-    ${extra_configure_args}
+# Re configure and build twice:
+# - once for Release build (fully optimized, no runtime assertions)
+# - once for Debug build (optimisation disable, runtime asserts enabled.
+./autogen.sh ${configure_args}
 make -j8 build_lib_shared
 make -j8 check
 make install_lib_shared install_include install_bin
 
+# Note that jemalloc --with-debug by default disables all optimisations (-O0).
+# This has a significant performance hit; we mostly want --with-debug for
+# runtime assertions. As such, turn back on "optimise for debug" (-Og).
+CFLAGS=-Og ./autogen.sh ${configure_args} --enable-debug \
+    --with-install-suffix=d
+make -j8 build_lib_shared
+make -j8 check
+make install_lib_shared
+
 if [ $(uname -s) = "Darwin" ]; then
     install_name_tool -id @rpath/libjemalloc.2.dylib ${INSTALL_DIR}/lib/libjemalloc.2.dylib
+    install_name_tool -id @rpath/libjemallocd.2.dylib ${INSTALL_DIR}/lib/libjemallocd.2.dylib
 fi
