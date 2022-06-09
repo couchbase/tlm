7a6cf56d29a1f7dc5b2d1b8bddcd10f225688f29
Ming Ho <ming.ho@couchbase.com>
2022-06-08 17:40:56 -0700

    MB-52314, add libsodium to tlm.
    -Ming Ho
    
    Change-Id: I0eb99df8f2dfb95955083460ae5402c0590a8c8a
    Reviewed-on: https://review.couchbase.org/c/tlm/+/175830
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ cmake/Modules/FindCouchbaseLibsodium.cmake
@@ -0,0 +1,53 @@
+#
+#     Copyright 2022 Couchbase, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# Locate libsodium library
+# This module defines
+#  LIBSODIUM_FOUND, if libsodium was found
+#  LIBSODIUM_LIBRARIES, Library path and libs
+#  LIBSODIUM_INCLUDE_DIR, where to find the libsodium headers
+if (NOT DEFINED LIBSODIUM_FOUND)
+    include(PlatformIntrospection)
+
+    cb_get_supported_platform(_is_supported_platform)
+    if (_is_supported_platform)
+        # Supported platforms should only use the provided hints and pick it up
+        # from cbdeps
+        set(_libsodium_no_default_path NO_DEFAULT_PATH)
+    endif ()
+
+    set(_libsodium_exploded ${CMAKE_BINARY_DIR}/tlm/deps/libsodium.exploded)
+    find_path(LIBSODIUM_INCLUDE_DIR sodium.h
+              HINTS ${_libsodium_exploded}/include
+              ${_libsodium_no_default_path})
+
+    if (NOT LIBSODIUM_INCLUDE_DIR)
+        message(FATAL_ERROR "Failed to locate sodium.h")
+    endif ()
+
+    find_library(LIBSODIUM_LIBRARIES
+                 NAMES sodium libsodium
+                 HINTS ${CMAKE_INSTALL_PREFIX}/lib
+                 ${_libsodium_no_default_path})
+    if (NOT LIBSODIUM_LIBRARIES)
+        message(FATAL_ERROR "Failed to locate libsodium")
+    endif ()
+
+    message(STATUS "Found libsodium headers in: ${LIBSODIUM_INCLUDE_DIR}")
+    message(STATUS "             libraries: ${LIBSODIUM_LIBRARIES}")
+
+    set(LIBSODIUM_FOUND true CACHE BOOL "Found libsodium" FORCE)
+    mark_as_advanced(LIBSODIUM_FOUND LIBSODIUM_INCLUDE_DIR LIBSODIUM_LIBRARIES)
+endif (NOT DEFINED LIBSODIUM_FOUND)
