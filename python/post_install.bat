b87fed64d6c80847d6bc71f0347dc72975968645
Chris Hillery <ceej@couchbase.com>
2022-06-11 01:04:49 -0700

    CBD-4875: Utilize cbpy from cbdeps rather than building every time
    Change-Id: Iac8594cded2a57af6b4856c1a9c3a5684747f741
    Reviewed-on: https://review.couchbase.org/c/tlm/+/176065
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- python/post_install.bat
+++
@@ -1,2 +0,0 @@
-xcopy site-packages\* Lib\site-packages\* /s /e /i /Y  >NUL
-rmdir site-packages /S /Q >NUL
--- python/post_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in site-packages/*
-do
-    dir=$(basename $f)
-    rm -rf lib/python*/site-packages/"${dir}"
-    mv "site-packages/${dir}" lib/python*/"site-packages"
-done
