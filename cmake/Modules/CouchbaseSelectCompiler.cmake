eb551dfdf36bceb58d77efd17dc56baf451e9b75
Dave Rigby <daver@couchbase.com>
2022-10-18 14:25:21 +0000

    MB-54162: cbdeps: Set default Linux compiler as per Server
    cbdeps currently uses whatever the default system compiler is - GCC
    7.3.0 on our Linux server Docker images.
    
    For Folly (and potentially other cbdeps) we need to use the same
    compiler as used by the main server build, otherwise we run into
    linker issues where libfolly.a has been compiled to use
    std::experimental::filesystem (pre-C++ 17 namespace) and CB Server has
    been compiled to use std::filesystem.
    
    As such, move the logic to select the default compiler (if one not
    explicitly specified) from CMakeLists.txt to
    CouchbaseSelectCompiler.cmake; and have both CMakeLists.txt and
    deps/packages/CMakeLists.txt include that file.
    
    *** NOTE ***
    
    This patch does not change the actual compiler used by cbdeps built
    via ExternalProject_Add() - that command spawns a new sub-CMake (or
    other custom command) and hence will continue to pickup whichever
    compiler it did previously (typically 'cc' and 'c++' in the path).
    
    It does however set the CMake vars CMAKE_{C,CXX}_COMPILER in the scope
    where ExternalProject_Add() is called, and hence allows future patches
    to pass that variable down so selected cbdeps can use the same
    compiler - e.g.:
    
        ExternalProject_Add(folly
            CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
            ...
        )
    
    See also: MB-45044, which introduced the automatic selection of
    GCC-10.2 where appropriate.
    
    Change-Id: I9d0eaa9b569d0585d8255a32f418ab6abc641278
    Reviewed-on: https://review.couchbase.org/c/tlm/+/181470
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

---
+++ cmake/Modules/CouchbaseSelectCompiler.cmake
@@ -0,0 +1,18 @@
+# Select the compiler to use by default, if not specified by user.
+# Sets CMAKE_C_COMPILER and CMAKE_CXX_COMPILER - note this file should
+# be include()'d before the project() command as CMAKE_C_COMPILER
+# cannot be changed after project().
+
+# Use gcc from an alternate location if available (and the user didn't
+# ask for something else using the standard CMAKE_C/CXX_COMPILER
+# variables or CC / CXX env vars).
+set (CB_GCC_PATH /opt/gcc-10.2.0 CACHE PATH "Preferred location for GCC, if available")
+if(NOT DEFINED CMAKE_C_COMPILER AND
+   NOT DEFINED CMAKE_CXX_COMPILER AND
+   NOT DEFINED ENV{CC} AND
+   NOT DEFINED ENV{CXX})
+  if(EXISTS "${CB_GCC_PATH}")
+    set(CMAKE_C_COMPILER "${CB_GCC_PATH}/bin/gcc" CACHE PATH "Path to C Compiler" FORCE)
+    set(CMAKE_CXX_COMPILER "${CB_GCC_PATH}/bin/g++" CACHE PATH "Path to C++ Compiler" FORCE)
+  endif()
+endif()
