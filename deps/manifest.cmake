b11069d7f492cdb52018f49788ebb59fa2efd0f3
Chris Hillery <ceej@couchbase.com>
2023-06-02 02:51:30 -0700

    CBD-5406: Corrected Erlang package for OpenSSL 1.1.1u
    Also update BD manifest.
    
    Change-Id: Ib06ae73e436256b777795e2dc67d2fab8459e701
    Reviewed-on: https://review.couchbase.org/c/tlm/+/191972
    Well-Formed: Restriction Checker
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -22,7 +22,7 @@ DECLARE_DEP (boost VERSION 1.74.0-cb1 PLATFORMS amzn2 centos7 debian9 debian10 m
 DECLARE_DEP (cbpy VERSION 7.5.0-cb2 PLATFORMS linux macosx windows DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/python/interp")
 DECLARE_DEP (curl V2 VERSION 7.88.1 BUILD 4_openssl111 PLATFORMS linux macosx windows)
 DECLARE_DEP (double-conversion VERSION 3.0.0-cb4 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
-DECLARE_DEP (erlang V2 VERSION 24.3.4.4 BUILD 2 PLATFORMS linux macosx windows)
+DECLARE_DEP (erlang V2 VERSION 24.3.4.4 BUILD 7 PLATFORMS linux macosx windows)
 DECLARE_DEP (flatbuffers VERSION 1.10.0-cb5 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (fmt VERSION 7.1.3-cb2 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (folly VERSION v2020.09.07.00-couchbase-cb1 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
