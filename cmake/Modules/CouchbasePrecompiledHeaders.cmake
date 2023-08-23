75d6ec12021cf02099157ef986a080c3dca9eebe
Dave Rigby <daver@couchbase.com>
2023-08-23 12:05:33 +0100

    PCH: Instantiate templates if supported under AppleClang
    Clang-11 (and at least AppleClang 12.0.5) upwards support
    instantiating templates as part of precompiled headers; this can
    significantly speed up (1.3x) building as we only need to instantiate
    templates from precompiled headers once, instead of every time the
    templates is needed.
    
    While CMake 3.19+ enables this automatically for Clang when supported,
    it does not for AppleClang - see
    https://gitlab.kitware.com/cmake/cmake/-/issues/21133 Given the boost
    this gives, manually enable it for AppleClang here if supported.
    
    Example numbers before and after when building one of the larger
    KV-Engine unit test binaries:
    
    Before - 160s
    
        $ CCACHE_DISABLE=1 hyperfine --prepare "ninja -t clean" --runs 3 "ninja ep-engine_ep_unit_tests"
        Benchmark 1: ninja ep-engine_ep_unit_tests
          Time (mean ± σ):     160.301 s ± 26.053 s    [User: 1035.765 s, System: 72.557 s]
          Range (min … max):   142.190 s … 190.159 s    3 runs
    
    After - 125s
    
        $ CCACHE_DISABLE=1 hyperfine --prepare "ninja -t clean" --runs 3 "ninja ep-engine_ep_unit_tests"
        Benchmark 1: ninja ep-engine_ep_unit_tests
          Time (mean ± σ):     124.560 s ±  0.711 s    [User: 1038.355 s, System: 69.938 s]
          Range (min … max):   123.867 s … 125.288 s    3 runs
    
    Change-Id: I55c559990d6d585fd241f5c3320202f4e0f5e6ff
    Reviewed-on: https://review.couchbase.org/c/tlm/+/195878
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- cmake/Modules/CouchbasePrecompiledHeaders.cmake
+++ cmake/Modules/CouchbasePrecompiledHeaders.cmake
@@ -3,6 +3,28 @@
 
 option(CB_PCH "CB_PCH defines if targets will use pre-compiled headers where configured." ON)
 
+# Clang-11 (and at least AppleClang 12.0.5) upwards support instantiating
+# templates as part of precompiled headers; this can significantly speed up
+# (1.3x) building as we only need to instantiate templates from precompiled
+# headers once, instead of every time the templates are needed.
+# While CMake 3.19 enables this automatically for Clang when supported, it does
+# not for AppleClang - see
+# https://gitlab.kitware.com/cmake/cmake/-/issues/21133
+# Given the boost this gives, manually enable it for AppleClang here if
+# compiler supports it.
+if (CB_PCH AND APPLE)
+    check_cxx_compiler_flag(-fpch-instantiate-templates
+            HAVE_PCH_INSTANTIATE_TEMPLATES)
+    if (HAVE_PCH_INSTANTIATE_TEMPLATES)
+        # Note: Technically we only need this to be added to the compilation
+        # of the precompiled header itself, not all targets, however CMake
+        # doesn't expose an easy way to set compile arguments for PCH building,
+        # and (Apple)Clang ignores it if not compiling a header, so just
+        # add to the global CXX flags variable.
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fpch-instantiate-templates")
+    endif()
+endif()
+
 function(reuse_pch target pch_target)
     if (CB_PCH)
         target_precompile_headers(${target} REUSE_FROM ${pch_target})
