2296baa8348038b639adaeb51ef5b4d4a5f883a9
Ming Ho <ming.ho@couchbase.com>
2024-01-24 12:27:50 -0800

    MB-60481, use newer cbpy to pick up pycryptodome to 3.20.0.
    -Ming
    
    Change-Id: Ie5ed270a6a473a7d065a7fab1c3e160ec1566fc8
    Reviewed-on: https://review.couchbase.org/c/tlm/+/204372
    Tested-by: Build Bot <build@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -19,7 +19,7 @@
 DECLARE_DEP (benchmark VERSION v1.6.0-cb1 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (breakpad V2 VERSION 20200430 BUILD 1 PLATFORMS amzn2 centos7 debian9 debian10 suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (boost VERSION 1.74.0-cb1 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
-DECLARE_DEP (cbpy VERSION 7.6.0-cb4 PLATFORMS linux macosx windows DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/python/interp")
+DECLARE_DEP (cbpy VERSION 7.6.0-cb5 PLATFORMS linux macosx windows DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/python/interp")
 DECLARE_DEP (curl V2 VERSION 8.4.0 BUILD 1_openssl31x PLATFORMS linux macosx windows)
 DECLARE_DEP (double-conversion VERSION 3.0.0-cb4 PLATFORMS amzn2 centos7 debian9 debian10 macosx suse12 suse15 ubuntu18.04 ubuntu20.04 windows_msvc2017)
 DECLARE_DEP (erlang V2 VERSION 25.3 BUILD 11 PLATFORMS linux macosx windows)
