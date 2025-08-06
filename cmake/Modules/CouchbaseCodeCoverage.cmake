8fe02b24b7e479095520093684a9a69bc401f62a
Vesko Karaganev <vesko.karaganev@couchbase.com>
2025-08-06 10:57:00 +0100

    MB-67965: Add support for coverage on Clang with llvm-cov
    Clang generates coverage data which can be parsed using llvm-cov.
    llvm-cov has a compatilibity mode which can be invoked using llvm-cov
    gcov.
    
    Change-Id: I8d3a97867d89bb31d7fa2e86f71a300eb5f1d26f
    Reviewed-on: https://review.couchbase.org/c/tlm/+/231900
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- cmake/Modules/CouchbaseCodeCoverage.cmake
+++ cmake/Modules/CouchbaseCodeCoverage.cmake
@@ -15,35 +15,51 @@
 # Note 2: If you followed all the steps above but the coverage report is empty,
 # try to use "touch CMakeCache.txt" in your build directory, then rebuild the project and rerun the coverage report
 
-OPTION(CB_CODE_COVERAGE "Enable code coverage testing."
+option(CB_CODE_COVERAGE "Enable code coverage testing."
        OFF)
 
-IF (CB_CODE_COVERAGE)
-    FIND_PROGRAM(GCOV_PATH gcov)
-    FIND_PROGRAM(GCOVR_PATH gcovr)
+if (CB_CODE_COVERAGE)
+    # GCC uses gcov, Clang uses llvm-cov.
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        find_program(GCOV_PATH gcov)
 
-    IF (GCOV_PATH)
-        EXECUTE_PROCESS(COMMAND ${GCOV_PATH} --version
-                        OUTPUT_VARIABLE GCOV_VERSION
-                        OUTPUT_STRIP_TRAILING_WHITESPACE)
-        MESSAGE(STATUS "gcov found: ${GCOV_PATH} version: ${GCOV_VERSION}")
-    ELSE (GCOV_PATH)
-        MESSAGE(STATUS "gcov not found.")
-    ENDIF (GCOV_PATH)
+        if (GCOV_PATH)
+            execute_process(COMMAND ${GCOV_PATH} --version
+                            OUTPUT_VARIABLE GCOV_VERSION
+                            OUTPUT_STRIP_TRAILING_WHITESPACE)
+            message(STATUS "gcov found: ${GCOV_PATH} version: ${GCOV_VERSION}")
+        else()
+            message(FATAL_ERROR "CB_CODE_COVERAGE enabled but a required tool (gcov) was not found - cannot continue.")
+        endif()
 
-    IF (GCOVR_PATH)
-        EXECUTE_PROCESS(COMMAND ${GCOVR_PATH} --version
+        set(GCOV_EXECUTABLE ${GCOV_PATH})
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+        find_program(LLVM_COV_PATH llvm-cov)
+
+        if (LLVM_COV_PATH)
+            execute_process(COMMAND ${LLVM_COV_PATH} --version
+                            OUTPUT_VARIABLE LLVM_COV_VERSION
+                            OUTPUT_STRIP_TRAILING_WHITESPACE)
+            message(STATUS "llvm-cov found: ${LLVM_COV_PATH} version: ${LLVM_COV_VERSION}")
+        else()
+            message(FATAL_ERROR "CB_CODE_COVERAGE enabled but a required tool (llvm-cov) was not found - cannot continue.")
+        endif()
+
+        set(GCOV_EXECUTABLE "${LLVM_COV_PATH}" gcov)
+    else()
+        message(FATAL_ERROR "CB_CODE_COVERAGE enabled but ${CMAKE_CXX_COMPILER_ID} is not supported.")
+    endif()
+
+    find_program(GCOVR_PATH gcovr)
+    if (GCOVR_PATH)
+        execute_process(COMMAND ${GCOVR_PATH} --version
                         OUTPUT_VARIABLE GCOVR_VERSION
                         OUTPUT_STRIP_TRAILING_WHITESPACE)
-        MESSAGE(STATUS "gcovr found: ${GCOVR_PATH} version: ${GCOVR_VERSION}")
-    ELSE (GCOVR_PATH)
-        MESSAGE(STATUS "gcovr [www.gcovr.com] not found. HTML / XML coverage report rules will not be available")
-    ENDIF (GCOVR_PATH)
-
-    IF (NOT GCOV_PATH)
-       MESSAGE(FATAL_ERROR "CB_CODE_COVERAGE enabled but a required tool (gcov) was not found - cannot continue.")
-    ENDIF()
-ENDIF(CB_CODE_COVERAGE)
+        message(STATUS "gcovr found: ${GCOVR_PATH} version: ${GCOVR_VERSION}")
+    else()
+        message(STATUS "gcovr [www.gcovr.com] not found. HTML / XML coverage report rules will not be available")
+    endif()
+endif()
 
 # Defines a coverage report for the current module. If CB_CODE_COVERAGE is enabled,
 # adds three new targets to that module:
@@ -59,30 +75,39 @@ ENDIF(CB_CODE_COVERAGE)
 # 2) Run whatever programs to excercise the code (unit tests, etc).
 # 3) `make <project>-coverage-report-{html,xml}` to generate a report.
 #
-FUNCTION(ENABLE_CODE_COVERAGE_REPORT)
-   GET_FILENAME_COMPONENT(_cc_project ${CMAKE_CURRENT_BINARY_DIR} NAME)
+function(ENABLE_CODE_COVERAGE_REPORT)
+   get_filename_component(_cc_project ${CMAKE_CURRENT_BINARY_DIR} NAME)
 
-   IF (CB_CODE_COVERAGE)
-      MESSAGE(STATUS "Setting up code coverage for ${PROJECT_NAME}")
+   if (CB_CODE_COVERAGE)
+      message(STATUS "Setting up code coverage for ${PROJECT_NAME}")
 
-      ADD_CUSTOM_TARGET(${_cc_project}-coverage-zero-counters
+      add_custom_target(${_cc_project}-coverage-zero-counters
                         COMMAND find . -name *.gcda -exec rm {} \;
                         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                         COMMENT "Zeroing coverage counters for objects in ${CMAKE_CURRENT_BINARY_DIR}"
                         VERBATIM)
 
-      IF (GCOVR_PATH)
-          ADD_CUSTOM_TARGET(${_cc_project}-coverage-report-html
+      if (GCOVR_PATH)
+          set(GCOVR_ARGS
+            --gcov-executable ${GCOV_EXECUTABLE}
+            --root=${CMAKE_SOURCE_DIR}
+            "--filter=${CMAKE_CURRENT_SOURCE_DIR}/.*"
+            # Allow same function on different lines. Instead of merging keep
+            # the functions separate. The default "abort" fails for our fork
+            # of folly/AtomicBitSet.h, which exists in folly and in kv_engine.
+            --merge-mode-functions=separate
+          )
+          add_custom_target(${_cc_project}-coverage-report-html
                             COMMAND ${CMAKE_COMMAND} -E remove_directory coverage
                             COMMAND ${CMAKE_COMMAND} -E make_directory coverage
-                            COMMAND ${GCOVR_PATH} --root=${CMAKE_SOURCE_DIR} --filter="${CMAKE_CURRENT_SOURCE_DIR}/.*" --html --html-details -o coverage/index.html
+                            COMMAND ${GCOVR_PATH} ${GCOVR_ARGS} --html --html-details -o coverage/index.html
                             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                             COMMENT "Generating code coverage report for ${PROJECT_NAME} to ${CMAKE_CURRENT_BINARY_DIR}/coverage/index.html")
 
-          ADD_CUSTOM_TARGET(${_cc_project}-coverage-report-xml
-                            COMMAND ${GCOVR_PATH} --root=${CMAKE_SOURCE_DIR} --filter="${CMAKE_CURRENT_SOURCE_DIR}/.*" --xml -o coverage.xml
+          add_custom_target(${_cc_project}-coverage-report-xml
+                            COMMAND ${GCOVR_PATH} ${GCOVR_ARGS} --xml -o coverage.xml
                             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                             COMMENT "Generating code coverage report for ${PROJECT_NAME} to ${CMAKE_CURRENT_BINARY_DIR}/coverage.xml")
-      ENDIF (GCOVR_PATH)
-   ENDIF (CB_CODE_COVERAGE)
-ENDFUNCTION()
+      endif()
+   endif()
+endfunction()
