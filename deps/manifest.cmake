d242856b7a73b406234f76f5b907f378b2306877
Ananth Krishnaswamy <ananth.krishnaswamy@couchbase.com>
2026-08-21 15:48:58 +0530

    CBD-6812: Change build for erlang to use 28.5.0.5
    Change-Id: I125098e07a678f5034917e7d3da8a2e1c79d97b5
    Reviewed-on: https://review.couchbase.org/c/tlm/+/251413
    Tested-by: Ananth Krishnaswamy <ananth.krishnaswamy@couchbase.com>
    Reviewed-by: <yash.dodderi@couchbase.com>

--- deps/manifest.cmake
+++ deps/manifest.cmake
@@ -23,7 +23,7 @@ DECLARE_DEP (boost VERSION 1.86.0-cb3 PLATFORMS linux macosx windows)
 DECLARE_DEP (cbpy V2 VERSION 3.14.2 BUILD 4 PLATFORMS linux macosx windows DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/python/interp")
 DECLARE_DEP (curl V2 VERSION 8.21.0 BUILD 2 PLATFORMS linux macosx windows)
 DECLARE_DEP (double-conversion VERSION 3.3.0-cb1 PLATFORMS linux macosx windows)
-DECLARE_DEP (erlang V2 VERSION 28.5 BUILD 1 PLATFORMS linux macosx windows)
+DECLARE_DEP (erlang V2 VERSION 28.5 BUILD 2 PLATFORMS linux macosx windows)
 # CUDA GPU support is only available on Linux
 IF(ENABLE_CUDA_GPU AND NOT WIN32 AND NOT APPLE)
   SET(FAISS_BUILD "30_avx2-gpu")
